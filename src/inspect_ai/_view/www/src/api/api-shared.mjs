23e68cc0760a94b7085bed9af32ecb64309b78f1
Charles Teague <cteague@gmail.com>
2024-07-18 11:49:49 -0400

    Hostable Viewer
    The viewer may now be hosted alongside deployed log files, which will be loaded individually using:
    
    `?log_file=<path>`
    
    So, for example, you can serve the log viewer and then display a log file using:
    
    `http://localhost:8000/?log_file=logs/2024-07-18T09-00-04-04-00_bash_QuRMsu7hRUWrJjxe8xePLa.json`

---
+++ src/inspect_ai/_view/www/src/api/api-shared.mjs
@@ -0,0 +1,10 @@
+// Function that uses the dom to download the contents provided in filecontents
+export async function download_file(_logfile, filename, filecontents) {
+  const blob = new Blob([filecontents], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
