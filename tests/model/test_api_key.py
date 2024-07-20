8be1ed3e05788951859ca1a658da058482e9f65a
J.J. Allaire <jj.allaire@gmail.com>
2024-07-20 16:47:48 -0400

    groq: typing, tools tests, docs

--- tests/model/test_api_key.py
+++ tests/model/test_api_key.py
@@ -4,10 +4,10 @@ import pytest
 from test_helpers.utils import (
     skip_if_no_anthropic,
     skip_if_no_google,
+    skip_if_no_groq,
     skip_if_no_mistral,
     skip_if_no_openai,
     skip_if_no_together,
-    skip_if_no_groq,
 )
 
 from inspect_ai.model import get_model
