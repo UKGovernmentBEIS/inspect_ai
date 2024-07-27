9936d8cf98aab24389796fd5bde3f0c4676e2175
Charles Teague <cteague@gmail.com>
2024-07-26 20:38:29 -0400

    Display reduced name in navbar scores

--- src/inspect_ai/_view/www/src/navbar/Navbar.mjs
+++ src/inspect_ai/_view/www/src/navbar/Navbar.mjs
@@ -184,14 +184,18 @@ const RunningPanel = () => {
 
 const ResultsPanel = ({ results }) => {
   // Map the scores into a list of key/values
-  const scorers = {};
-  results.scores.map((score) => {
-    scorers[score.name] = Object.keys(score.metrics).map((key) => {
-      return { name: key, value: score.metrics[key].value };
-    });
-  });
-
   if (results.scores.length === 1) {
+    const scorers = {};
+    results.scores.map((score) => {
+      scorers[score.name] = Object.keys(score.metrics).map((key) => {
+        return {
+          name: key,
+          value: score.metrics[key].value,
+          reducer: score.reducer,
+        };
+      });
+    });
+
     const metrics = Object.values(scorers)[0];
     return html`<div
       style=${{
@@ -232,18 +236,36 @@ const ResultsPanel = ({ results }) => {
 };
 
 const VerticalMetric = ({ metric, isFirst }) => {
+  const reducer_component = metric.reducer
+    ? html` <div
+        style=${{
+          fontSize: "0.7rem",
+          fontWeight: "200",
+          textAlign: "center",
+          textTransform: "uppercase",
+          paddingTop: "0.3rem",
+          marginBottom: "-0.3rem",
+        }}
+      >
+        ${metric.reducer}
+      </div>`
+    : "";
+
   return html`<div style=${{ paddingLeft: isFirst ? "0" : "1em" }}>
     <div
       style=${{
         fontSize: "0.8rem",
         fontWeight: "200",
         textAlign: "center",
-        marginBottom: "-0.3rem",
         paddingTop: "0.3rem",
+        marginBottom: "-0.3rem",
+        textTransform: "uppercase",
+        borderBottom: "solid var(--bs-border-color) 1px",
       }}
     >
       ${metric.name}
     </div>
+    ${reducer_component}
     <div
       style=${{
         fontSize: "1.5rem",
@@ -258,6 +280,20 @@ const VerticalMetric = ({ metric, isFirst }) => {
 
 const MultiScorerMetric = ({ scorer, isFirst }) => {
   const baseFontSize = Object.keys(scorer.metrics).length === 1 ? 0.9 : 0.7;
+
+  const reducer_component = scorer.reducer
+    ? html`<div
+        style=${{
+          fontSize: `${baseFontSize - 0.1}rem`,
+          fontWeight: "200",
+          textAlign: "center",
+          textTransform: "uppercase",
+        }}
+      >
+        ${scorer.reducer}
+      </div>`
+    : "";
+
   return html`<div style=${{ paddingLeft: isFirst ? "0" : "1.5em" }}>
     <div
       style=${{
@@ -271,6 +307,7 @@ const MultiScorerMetric = ({ scorer, isFirst }) => {
     >
       ${scorer.name}
     </div>
+    ${reducer_component}
     <div
       style=${{
         display: "grid",
