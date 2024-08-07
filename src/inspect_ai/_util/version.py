c7c312d55a0cb8e722c844cffe2b6575be1a4962
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-08-07 17:24:04 -0400

    pin to mistal 0.4.2 for now (#212)
    Co-authored-by: J.J. Allaire <jj.allaire@gmail.com>

--- src/inspect_ai/_util/version.py
+++ src/inspect_ai/_util/version.py
@@ -2,12 +2,17 @@ from importlib.metadata import version
 
 import semver
 
-from .error import module_version_error
+from .error import module_max_version_error, module_version_error
 
 
 def verify_required_version(feature: str, package: str, version: str) -> None:
     if not has_required_version(package, version):
         raise module_version_error(feature, package, version)
+
+
+def verify_max_version(feature: str, package: str, max_version: str) -> None:
+    if semver.Version.parse(version(package)).compare(max_version) > 0:
+        raise module_max_version_error(feature, package, max_version)
 
 
 def has_required_version(package: str, required_version: str) -> bool:
