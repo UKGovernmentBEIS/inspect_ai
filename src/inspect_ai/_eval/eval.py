7c0ffbabd9d39df391d9843431441ad201130843
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-08-19 14:45:28 +0000

    update comment to not reference eval_gather

--- src/inspect_ai/_eval/eval.py
+++ src/inspect_ai/_eval/eval.py
@@ -199,12 +199,11 @@ async def eval_async(
         List of EvalLog (one for each task)
     """
     # only a single call to eval_async can be active at a time, this is
-    # because when running a task a chdir to the task's directory (and a
+    # because when running a task a chdir to the task's directory (and
     # similar mutation of the Python sys.path) occurs. since this is a
     # change to global process state it cannot occur in parallel. for
-    # task parallelism, use eval_gather, which enforces the appropriate
-    # constraints on task parallelism and schedules multiple tasks for
-    # optimal concurrency
+    # task parallelism, pass multiple tasks to eval or eval_async (which
+    # will enforce the appropriate constraints on task parallelism)
     global _eval_async_running
     if _eval_async_running:
         raise RuntimeError("Multiple concurrent calls to eval_async are not allowed.")
