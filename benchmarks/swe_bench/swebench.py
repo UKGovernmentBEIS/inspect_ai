61d4feb3becf6f369fdd61c0a60e555ff6a1607c
max-kaufmann <max.a.kaufmann@gmail.com>
2024-08-19 18:32:39 +0100

    intermediate changing

--- benchmarks/swe_bench/swebench.py
+++ benchmarks/swe_bench/swebench.py
@@ -15,7 +15,10 @@ from inspect_ai.util import sandbox
 from datasets import load_dataset
 from agent_framework.experimental.agents.base_aisi_agent import base_aisi_agent
 from agent_framework.tools import submit_answer_tool, bash_tool
-from swebench.harness.constants import
+from swebench.harness.constants import MAP_REPO_VERSION_TO_SPECS
+from swebench.harness.utils import get_test_directives
+import shlex
+import re
 
 INPUT_PROMPT = """
 Please solve the following issue:
@@ -28,22 +31,26 @@ def swe_bench(
     split: str | None = "dev",
     dataset="princeton-nlp/SWE-bench_Lite",
 ) -> Task:
-    hf_dataset = load_dataset(
+    
+    swebench_dataset = hf_dataset(
         dataset,
         split=split,
+        sample_fields=FieldSpec(
+            input="problem_statement",
+            id="instance_id",
+            metadata=["base_commit","patch","test_patch","version","repo","environment_setup_commit","PASS_TO_PASS","FAIL_TO_PASS"],
+        )
     )
+
+    swebench_dataset = [sample for sample in swebench_dataset if sample.id == instance_id]
+    assert len(swebench_dataset) == 1, f"There should be exactly one sample matching the input instance_id, found {instance_id}"
+
+    # Put the input in context 
+    swebench_dataset[0].input = INPUT_PROMPT.format(swebench_dataset[0].input)
     load_environment_images(hf_dataset)
 
-    instance = hf_dataset.filter(lambda record: instance_id == record["instance_id"])[0]
-    sample = Sample(input=INPUT_PROMPT.format(issue_text=instance["problem_statement"]), id=instance_id,metadata={
-        "base_commit":instance["base_commit"],
-        "test_patch": instance["test_patch"],
-        "patch": instance["patch"]
-
-    })
-
     return Task(
-        dataset=[sample],
+        dataset=swebench_dataset,
         plan=base_aisi_agent(
             tools=[submit_answer_tool(), bash_tool()], max_iterations=5
         ),
@@ -68,154 +75,60 @@ cat model.patch"""
 def swebench_scorer() -> Scorer:
 
     async def scorer(state: TaskState, target: Target) -> Score:
-        base_commit = state.metadata["base_commit"]
-        test_patch = state.metadata["test_patch"]
 
-        sandbox_output = await sandbox().exec(["bash","-c",CREATE_MODEL_PATCH.format(base_commit=base_commit)])
+        # Get the changes the model made, for logging
+        await sandbox().exec(["bash","-c",CREATE_MODEL_PATCH.format(base_commit=state.metadata["base_commit"])])
         agent_patch = await sandbox().exec(["bash","-c",GET_AGENT_PATCH])
 
-        print("")
+        # Run the evaluation script
+        eval_script = get_eval_script(test_patch=state.metadata["test_patch"], repo=state.metadata["repo"],version=state.metadata["version"],base_commit=state.metadata["base_commit"])
+        eval_output = await sandbox().exec(["bash","-c",eval_script])
 
-        return Score(value=0.0,metadata={"model_patch":agent_patch.stdout})
+        return Score(value=0.0,explanation=eval_output.stdout,metadata={"model_patch":agent_patch.stdout})
     
     return scorer
 
 
-def run_instance(
-        test_spec: TestSpec,
-        pred: dict,
-        rm_image: bool,
-        force_rebuild: bool,
-        client: docker.DockerClient,
-        run_id: str,
-        timeout: int | None = None,
-    ):
-    """
-    Run a single instance with the given prediction.
+def get_eval_script(test_patch :str , repo :str, version : str, base_commit : str) -> str:
 
-    Args:
-        test_spec (TestSpec): TestSpec instance
-        pred (dict): Prediction w/ model_name_or_path, model_patch, instance_id
-        rm_image (bool): Whether to remove the image after running
-        force_rebuild (bool): Whether to force rebuild the image
-        client (docker.DockerClient): Docker client
-        run_id (str): Run ID
-        timeout (int): Timeout for running tests
-    """
-    # Set up logging directory
-    instance_id = test_spec.instance_id
+    #First we fetch the repository-specific 'specification' which SWE-bench provides 
+    conda_env="testbed"
+    repo_directory=f"/testbed"
 
-    # Get git diff before running eval script
-    git_diff_output_before = (
-        sandbox.exec_run("git diff", workdir="/testbed").output.decode("utf-8").strip()
-    )
-    logger.info(f"Git diff before:\n{git_diff_output_before}")
-
-    eval_file = Path(log_dir / "eval.sh")
-    eval_file.write_text(test_spec.eval_script)
-    logger.info(
-        f"Eval script for {instance_id} written to {eval_file}; copying to container..."
-    )
-    copy_to_container(sandbox, eval_file, Path("/eval.sh"))
-
-    # Run eval script, write output to logs
-    test_output, timed_out, total_runtime = exec_run_with_timeout(sandbox, "/bin/bash /eval.sh", timeout)
-    test_output_path = log_dir / "test_output.txt"
-    logger.info(f'Test runtime: {total_runtime:_.2f} seconds')
-    with open(test_output_path, "w") as f:
-        f.write(test_output)
-        logger.info(f"Test output for {instance_id} written to {test_output_path}")
-        if timed_out:
-            f.write(f"\n\nTimeout error: {timeout} seconds exceeded.")
-            raise EvaluationError(
-                instance_id,
-                f"Test timed out after {timeout} seconds.",
-                logger,
-            )
-
-    # Get git diff after running eval script
-    git_diff_output_after = (
-        sandbox.exec_run("git diff", workdir="/testbed").output.decode("utf-8").strip()
-    )
-
-    # Check if git diff changed after running eval script
-    logger.info(f"Git diff after:\n{git_diff_output_after}")
-    if git_diff_output_after != git_diff_output_before:
-        logger.info(f"Git diff changed after running eval script")
-
-    # Get report from test output
-    logger.info(f"Grading answer for {instance_id}...")
-    report = get_eval_report(
-        test_spec=test_spec,
-        prediction=pred,
-        log_path=test_output_path,
-        include_tests_status=True,
-    )
-    logger.info(
-        f"report: {report}\n"
-        f"Result for {instance_id}: resolved: {report[instance_id]['resolved']}"
-    )
-
-
-def get_eval_script(instance, repo, version, base_commit,env_name):
-
-    """
-    Applies the test patch and runs the tests.
-    """
-
-    spec = MAP_EVAL_VERSION_TO_SPECS[repo][version]
-    env_name="testbed"
-    repo_directory=f"/{env_name}"
-    eval_instance = {"repo":repo,"version":version}
+    # Fetch the command which runs the test. Often simply the string 'pytest'
+    test_command = MAP_REPO_VERSION_TO_SPECS[repo][version]["test_cmd"]
     
-
+    # Fetch any repo-specific setup commands, e.g. any environment variables
+    eval_setup_commands = MAP_REPO_VERSION_TO_SPECS[repo][version].get("eval_commands",[])
+    
     # Find all the files which have been modified by the test patch
-    test_files = re.findall(r"--- a/(.*)", test_patch)
+    test_patch_files = re.findall(r"--- a/(.*)", test_patch)
+
+    #Find all the files which contain tests. Ugly interface is due to swebench
+    test_files = get_test_directives({"repo":repo,"test_patch":test_patch}) #type: ignore
 
     # Reset test files to the state they should be in before the patch.
-    reset_tests_command = f"git checkout {base_commit} {' '.join(test_files)}"
-    sandbox().exec(["bash","-c",reset_tests_command])
+    eval_script = f"""set -euox pipefail
+#We switch to the repository and activate the environment needed to run the tests
+cd {repo_directory}
+source /opt/miniconda3/bin/activate
+conda activate {conda_env}
 
-    #We write the test_patch to the container, and then apply it
-    TEST_PATCH_LOCATION = "/tmp/test_patch.diff"
-    sandbox().write_file(TEST_PATCH_LOCATION,test_patch)
-    sandbox().exec(["bash","-c",f"git apply {TEST_PATCH_LOCATION}"])
+#We run all of the repo-specific setup commands (If they)
+{"\n".join(eval_setup_commands)}
 
-    #We get a
-    test_command = " ".join(
-        [
-            MAP_REPO_VERSION_TO_SPECS[repo][version]["test_cmd"],
-            *get_test_directives(instance),
-        ]
-    )
+#We make sure we're back in the correct environment, in case the repo setup created issues
+cd {repo_directory}
+source /opt/miniconda3/bin/activate
+conda activate {conda_env}
 
-    eval_commands = [
-        f"source /opt/miniconda3/bin/activate",
-        f"conda activate {env_name}",
-        f"cd {repo_directory}",
-    ]
+#First we reset all of the files which out test patch touches
+git checkout {base_commit} {' '.join(test_patch_files)}
 
-    if "eval_commands" in specs:
-        eval_commands += specs["eval_commands"]
-    
-    eval_commands += [
-        f"git config --global --add safe.directory {repo_directory}",  # for nonroot user
-        f"cd {repo_directory}",
-        # This is just informational, so we have a record
-        f"git status",
-        f"git show",
-        f"git diff {base_commit}",
-        "source /opt/miniconda3/bin/activate",
-        f"conda activate {env_name}",
-    ]
+#Then we apply the test patch given to us by SWE-bench, setting up the test we need to run
+git apply {shlex.quote(test_patch)}
 
-    if "install" in specs:
-        eval_commands.append(specs["install"])
-    
-    eval_commands += [
-        reset_tests_command,
-        apply_test_patch_command,
-        test_command,
-        reset_tests_command,  # Revert tests after done, leave the repo in the same state as before
-    ]
-    return eval_commands
+#Then we run all the tests in the repository.
+{test_command} {" ".join(test_files)}"""
+
+    return eval_script
