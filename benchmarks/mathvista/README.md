c582380a1bedb8fc3d4a3e5f5ff9d4941af79fcf
Shiv Munagala <shiv@shivm.xyz>
2024-09-03 22:28:37 +0100

    initial commit

---
+++ benchmarks/mathvista/README.md
@@ -0,0 +1,21 @@
+# MathVista: Evaluating Mathematical Reasoning in Visual Contexts
+
+[MathVista](https://arxiv.org/pdf/2310.02255) to evaluate model performance in answering mathematics problems that have a visual component. Each problem has an associated image which is required to solve the problem. The dataset is made up of 6,141 questions from 28 existing multimodal datasets as well as 3 new datasets created for MathVista, where each dataset contains different styles of questions. There are both multiple-choice questions and free-form questions, where the free-form questions ask for a response in the form of an integer, floating-point number, or list.
+
+## Execution
+Here is an example from the dataset:
+
+**Question:** The derivative of f(x) at x=2 is ____ that at x=5
+
+**Choices:** "larger than", "equal to", "smaller than"
+
+**Image:**
+
+![Image for example question](resources/example_image.png)
+
+**Correct answer:** "equal to"
+
+## Evaluation
+For multiple choice questions the model is prompted to answer with a single letter answer, it is given the question, the image, and the choices and asked to answer. For free-form questions the model is given the question, the image, and told the format in which to give the answer and is asked to answer.
+
+N.B. It is expected that the images required for solving the questions are available in a subdirectory called `images` from which the file `mathvista.py` is being run. The images can be downloaded from [HuggingFace](https://huggingface.co/datasets/AI4Math/MathVista) where it is available in a zipped format `images.zip` - it must be unzipped before being used.
---
+++ benchmarks/mathvista/mathvista.py
@@ -0,0 +1,160 @@
+import re
+
+from inspect_ai import Task, task
+from inspect_ai._util.content import ContentImage, ContentText
+from inspect_ai._util.pattern import ANSWER_PATTERN_LETTER, ANSWER_PATTERN_LINE
+from inspect_ai.dataset import Sample, hf_dataset
+from inspect_ai.model import ChatMessageUser
+from inspect_ai.scorer import (
+    CORRECT,
+    INCORRECT,
+    Score,
+    Target,
+    accuracy,
+    scorer,
+    stderr,
+)
+from inspect_ai.scorer._pattern import match_first
+from inspect_ai.solver import Generate, Solver, TaskState, solver
+from inspect_ai.solver._multiple_choice import prompt
+
+SINGLE_ANSWER_TEMPLATE = r"""
+Answer the following multiple choice question. The entire content of your response should be of the following format: 'ANSWER: $LETTER' (without quotes) where LETTER is one of {letters}.
+
+{question}
+
+{choices}
+""".strip()
+
+FREEFORM_TEMPLATE = r"""
+Answer the following question. The entire content of your response should be of the following format: 'ANSWER: $ANSWER' (without quotes) where $ANSWER is one your answer.
+
+{question}
+"""
+
+
+@task
+def MathVistaMultipleChoice() -> Task:
+    dataset = hf_dataset(
+        path="AI4Math/MathVista",
+        split="testmini",
+        sample_fields=record_to_sample,
+        trust=True,
+        shuffle=True,
+    )
+
+    return Task(
+        dataset=dataset,
+        plan=[multi_choice_or_free_form_solver()],
+        scorer=multi_choice_or_free_form_scorer(),
+    )
+
+
+def get_multi_choice_as_letter(record: dict) -> chr:
+    choices = record["choices"]
+    labels = [chr(i) for i in range(ord("a"), ord("a") + len(choices))]
+
+    choices = dict(zip(labels, choices))
+
+    answer = record["answer"]
+    target = list(choices.values()).index(answer)
+    target = chr(ord("A") + int(target))
+
+    return target
+
+
+def record_to_sample(record: dict) -> Sample:
+    message = [
+        ChatMessageUser(
+            content=[
+                ContentText(text=record["question"])
+                if record["question_type"] == "multi_choice"
+                else ContentText(text=record["query"]),
+                ContentImage(image=record["image"]),
+            ]
+        )
+    ]
+    if record["question_type"] == "multi_choice":
+        return Sample(
+            input=message,
+            choices=record["choices"],
+            target=get_multi_choice_as_letter(record),
+            id=record["pid"],
+            metadata={
+                "question_type": record["question_type"],
+                "answer_type": record["answer_type"],
+                **record["metadata"],
+            },
+            files={f"image:{record['image']}": record["image"]},
+        )
+
+    elif record["question_type"] == "free_form":
+        return Sample(
+            input=message,
+            target=record["answer"],
+            id=record["pid"],
+            metadata={
+                "precision": record["precision"],
+                "question_type": record["question_type"],
+                "answer_type": record["answer_type"],
+                **record["metadata"],
+            },
+            files={f"image:{record['image']}": record["image"]},
+        )
+
+
+@solver
+def multi_choice_or_free_form_solver() -> Solver:
+    async def solve(state: TaskState, generate: Generate) -> TaskState:
+        if state.metadata["question_type"] == "multi_choice":
+            state.user_prompt.text = prompt(
+                question=state.user_prompt.text,
+                choices=state.choices,
+                template=SINGLE_ANSWER_TEMPLATE,
+            )
+
+            return await generate(state)
+
+        elif state.metadata["question_type"] == "free_form":
+            state.user_prompt.text = FREEFORM_TEMPLATE.format(
+                question=state.user_prompt.text
+            )
+            return await generate(state)
+
+    return solve
+
+
+@scorer(metrics=[accuracy(), stderr()])
+def multi_choice_or_free_form_scorer():
+    async def score(state: TaskState, target: Target) -> Score:
+        match = re.search(
+            ANSWER_PATTERN_LETTER
+            if state.metadata["question_type"] == "multi_choice"
+            else ANSWER_PATTERN_LINE,
+            state.output.completion,
+            re.IGNORECASE,
+        )
+        if match:
+            groups = match.groups()
+            found_match = match_first(matches=groups, target=target, ignore_case=True)
+
+            if found_match is None and len(groups) == 1:
+                answer = groups[0]
+            else:
+                answer = found_match
+
+            return Score(
+                value=CORRECT if found_match else INCORRECT,
+                answer=answer,
+                explanation=state.output.completion,
+            )
+
+        else:
+            # didn't find the scoring pattern
+            return Score(
+                value=INCORRECT,
+                explanation="Scoring pattern not matched in output: "
+                + f"{state.output.completion}",
+            )
+
+    return score
