5dcaea21247d7d459030671e690ea325f5bfc322
Charles Teague <cteague@gmail.com>
2024-09-04 19:51:40 -0400

    properly process the header only parameter (#326)
    * properly process the header only parameter
    
    The parameter is supposed to always be an int (older version of inspect do not pass it).
    
    * if not specified, there is no limit
    
    ---------
    
    Co-authored-by: jjallaire <jj.allaire@gmail.com>

--- src/inspect_ai/_view/view.py
+++ src/inspect_ai/_view/view.py
@@ -150,7 +150,8 @@ class ViewHTTPRequestHandler(InspectHTTPRequestHandler):
 
         # read query parameters from the URL
         query_params = parse_qs(parsed.query)
-        header_only = query_params.get("header-only", None) is not None
+        header_param = query_params.get("header-only", None)
+        header_only = int(header_param[0]) if header_param is not None else sys.maxsize
 
         # reconstruct the path
         path = urlunparse(
