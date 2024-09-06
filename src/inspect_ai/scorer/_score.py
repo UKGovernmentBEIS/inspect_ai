f78f7d763397bc7a7d4e0e101538bb5e39c9d5ce
J.J. Allaire <jj.allaire@gmail.com>
2024-09-06 12:51:06 -0400

    docs for score function

--- src/inspect_ai/scorer/_score.py
+++ src/inspect_ai/scorer/_score.py
@@ -8,11 +8,26 @@ from ._target import Target
 
 
 async def score(state: TaskState) -> list[Score]:
+    """Score a TaskState.
+
+    Score a task state from within a solver.
+
+    Args:
+      state (TaskState): `TaskState` to submit for scoring
+
+    Returns:
+      List of scores (one for each task scorer)
+
+    Raises:
+      RuntimerError: If called from outside a task or within
+        a task that does not have a scorer.
+
+    """
     scorers = _scorers.get(None)
     target = _target.get(None)
     if scorers is None or target is None:
         raise RuntimeError(
-            "The score() function can only be called while executing a scored task."
+            "The score() function can only be called while executing a task with a scorer."
         )
 
     return [await scorer(state, target) for scorer in scorers]
