32d380edb89396d57c756c3ffc27a3e1f7356ea7
Charles Teague <cteague@gmail.com>
2024-09-06 12:50:40 -0400

    Revert "scoring card tweaks"
    This reverts commit 09c39937f81c89941e936f460d030199d8170e68.
    
    # Conflicts:
    #	src/inspect_ai/_view/www/dist/assets/index.js
    #	src/inspect_ai/_view/www/src/samples/transcript/EventPanel.mjs

--- src/inspect_ai/_view/www/src/samples/SampleScoreView.mjs
+++ src/inspect_ai/_view/www/src/samples/SampleScoreView.mjs
@@ -7,7 +7,6 @@ import {
 import { MarkdownDiv } from "../components/MarkdownDiv.mjs";
 import { SampleScores } from "./SampleScores.mjs";
 import { FontSize, TextStyle } from "../appearance/Fonts.mjs";
-import { Card, CardBody, CardHeader } from "../components/Card.mjs";
 
 const labelStyle = {
   paddingRight: "2em",
@@ -41,68 +40,105 @@ export const SampleScoreView = ({
   const answer = scorerDescriptor.answer();
 
   return html`
-    <${Card} style=${{ marginTop: "0.5em" }}>
-      <${CardHeader} label="Result"/>
-      <${CardBody}>
-      <div
-        style=${{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, minmax(100px, 1fr))",
-          justifyContent: "space-between",
-          columnGap: "0.5em",
-          fontSize: FontSize.small,
-          paddingBottom: "1em",
-          ...style,
-        }}>
-        <div style=${{ gridColumn: "1/-1" }}>
-          <div style=${{ ...labelStyle }}>Input</div>
-          <div>
-            <${MarkdownDiv}
-              markdown=${scoreInput.join("\n")}
-              style=${{ wordBreak: "break-all" }}
-            />
-          </div>
-        </div>
-
+    <div
+      class="container-fluid"
+      style=${{
+        paddingTop: "1em",
+        paddingLeft: "0",
+        fontSize: FontSize.base,
+        ...style,
+      }}
+    >
+      <div>
+        <div style=${{ ...labelStyle }}>Input</div>
         <div>
-          <div style=${{ ...labelStyle }}>Target</div>
           <${MarkdownDiv}
-            markdown=${arrayToString(arrayToString(sample?.target || "none"))}
-            style=${{ paddingLeft: "0" }}
-            class="no-last-para-padding"
-          />
-        </div>
-
-        <div>
-          <div style=${{ ...labelStyle }}>Answer</div>
-          <${MarkdownDiv}
-            class="no-last-para-padding"
-            markdown=${shortenCompletion(answer)}
-            style=${{ paddingLeft: "0" }}
-          />
-        </div>
-
-        <div>
-          <div style=${{ ...labelStyle }}>Score</div>
-          <${SampleScores}
-            sample=${sample}
-            sampleDescriptor=${sampleDescriptor}
-            scorer=${scorer}
+            markdown=${scoreInput.join("\n")}
+            style=${{ wordBreak: "break-all" }}
           />
         </div>
       </div>
-      </${CardBody}>
 
-      ${
-        explanation && explanation !== answer
-          ? html`<${CardBody}>
-        <div style=${{ fontSize: FontSize.small }}>
-          <div style=${{ ...labelStyle }}>Explanation</div>
-            <${MarkdownDiv} markdown=${arrayToString(explanation)} style=${{ paddingLeft: "0" }} class="no-last-para-padding"/>
-        </div>
-      </${CardBody}>`
-          : ""
-      }
-    </${Card}>
+      <table
+        class="table"
+        style=${{ width: "100%", marginBottom: "0", marginTop: "1em" }}
+      >
+        <thead style=${{ borderBottomColor: "#00000000" }}>
+          <tr>
+            <th style=${{ ...labelStyle, fontWeight: "400" }}>Target</th>
+            <th
+              style=${{ ...labelStyle, paddingBottom: "0", fontWeight: "400" }}
+            >
+              Answer
+            </th>
+            <th
+              style=${{
+                ...labelStyle,
+                paddingLeft: "2em",
+                paddingBottom: "0",
+                fontWeight: "400",
+              }}
+            >
+              Score
+            </th>
+          </tr>
+        </thead>
+        <tbody style=${{ borderBottomColor: "#00000000" }}>
+          <tr>
+            <td
+              style=${{
+                paddingRight: "2em",
+                paddingLeft: "0",
+                paddingTop: "0",
+              }}
+            >
+              <${MarkdownDiv}
+                markdown=${arrayToString(
+                  arrayToString(sample?.target || "none"),
+                )}
+                style=${{ paddingLeft: "0" }}
+                class="no-last-para-padding"
+              />
+            </td>
+            <td style=${{ paddingTop: "0", paddingLeft: "0" }}>
+              <${MarkdownDiv}
+                class="no-last-para-padding"
+                markdown=${shortenCompletion(answer)}
+                style=${{ paddingLeft: "0" }}
+              />
+            </td>
+            <td style=${{ paddingLeft: "2em", paddingTop: "0" }}>
+              <${SampleScores}
+                sample=${sample}
+                sampleDescriptor=${sampleDescriptor}
+                scorer=${scorer}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      ${explanation && explanation !== answer
+        ? html`
+        <table class="table" style=${{ width: "100%", marginBottom: "0" }}>
+              <thead>
+                <tr>
+                  <th style=${{
+                    paddingBottom: "0",
+                    paddingLeft: "0",
+                    ...labelStyle,
+                    fontWeight: "400",
+                  }}>Explanation</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td style=${{ paddingLeft: "0" }}>
+                  <${MarkdownDiv} markdown=${arrayToString(explanation)} style=${{ paddingLeft: "0" }} class="no-last-para-padding"/>
+                </td>
+              </tbody>
+            </table
+          `
+        : ""}
+    </div>
   `;
 };
