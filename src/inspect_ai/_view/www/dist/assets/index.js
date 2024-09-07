c10aadb2490fb88e5a6b3154168d1c4f814f8f67
Charles Teague <cteague@gmail.com>
2024-09-07 16:37:50 -0400

    Fine tuning in transcript view (#345)
    * Ensure find appears on top of modals in vscode
    
    * Provide a fresh state manage for store events
    
    * Make card background more subtle
    
    * Display tool errors
    
    * Ensure expandable panel truncates
    
    * Improve theming in vscode
    
    (especially works better in darker themes)
    
    ---------
    
    Co-authored-by: jjallaire <jj.allaire@gmail.com>

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -8530,7 +8530,7 @@ const Card = ({ id, classes, style, children }) => {
       class="${classes || ""}"
       ...${{ id }}
       style=${{
-    backgroundColor: "var(--bs-light)",
+    backgroundColor: "var(--bs-light-bg-subtle)",
     border: "solid 1px var(--bs-light-border-subtle)",
     borderRadius: "var(--bs-border-radius)",
     marginBottom: "1.5em",
@@ -17716,11 +17716,22 @@ const ToolEventView = ({ id, event, style, stateManager, depth }) => {
   const title = `Tool: ${event.function}`;
   return m$1`
   <${EventPanel} id=${id} title="${title}" icon=${ApplicationIcons.solvers.use_tools} style=${style}>
-  <div name="Summary">
-    <${ExpandablePanel}>
-      ${event.result ? m$1`<${ToolOutput} output=${event.result} style=${{ margin: "1em 0" }} />` : m$1`<div style=${{ margin: "1em 0", fontSize: FontSize.small }}>No output</div>`}
-    </${ExpandablePanel}>
-  </div>
+    <div name="Summary">
+    ${event.result ? m$1`
+          <${ExpandablePanel} collapse=${true} border=${true} lines=10>
+          <${ToolOutput}
+            output=${event.result}
+            style=${{ margin: "1em 0" }}
+          />
+          </${ExpandablePanel}>
+          ` : event.error ? m$1`<div style=${{ margin: "1em 0", fontSize: FontSize.small }}>
+              ${event.error.type}: ${event.error.message}
+            </div>` : m$1`<div style=${{ margin: "1em 0", fontSize: FontSize.small }}>
+              No output
+            </div>`}
+    </div>
+    
+  
   <div name="Transcript" style=${{ margin: "1em 0" }}>
     <${ToolCallView}
       functionCall=${functionCall}
@@ -17757,194 +17768,6 @@ class EventNode {
     this.children = [];
     this.depth = depth;
   }
-}
-const TranscriptView = ({ id, events, stateManager, depth = 0 }) => {
-  const resolvedEvents = fixupEventStream(events);
-  const eventNodes = treeifyEvents(resolvedEvents, depth);
-  return m$1`
-    <${TranscriptComponent}
-      id=${id}
-      eventNodes=${eventNodes}
-      stateManager=${stateManager}
-    />
-  `;
-};
-const TranscriptComponent = ({
-  id,
-  eventNodes,
-  style,
-  stateManager
-}) => {
-  const rows = eventNodes.map((eventNode, index) => {
-    const toggleStyle = {};
-    if (eventNode.depth % 2 == 0) {
-      toggleStyle.backgroundColor = "var(--bs-light)";
-    } else {
-      toggleStyle.backgroundColor = "var(--bs-body-bg)";
-    }
-    if (index === eventNodes.length - 1) {
-      toggleStyle.marginBottom = "0";
-    } else if (eventNode.depth === 0) {
-      toggleStyle.marginBottom = "1.5em";
-    }
-    const row = m$1`
-      <${RenderedEventNode}
-        id=${`${id}-event${index}`}
-        node=${eventNode}
-        stateManager=${stateManager}
-        style=${{
-      ...toggleStyle,
-      ...style
-    }}
-      />
-    `;
-    return row;
-  });
-  return m$1`<div
-    id=${id}
-    key=${id}
-    style=${{
-    fontSize: FontSize.small,
-    display: "grid",
-    margin: "0.5em 0 0 0",
-    width: "100%"
-  }}
-  >
-    ${rows}
-  </div>`;
-};
-const RenderedEventNode = ({ id, node, style, stateManager }) => {
-  switch (node.event.event) {
-    case "sample_init":
-      return m$1`<${SampleInitEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-      />`;
-    case "info":
-      return m$1`<${InfoEventView}
-        id=${id}
-        event=${node.event}
-        style=${style}
-      />`;
-    case "logger":
-      return m$1`<${LoggerEventView}
-        id=${id}
-        event=${node.event}
-        style=${style}
-      />`;
-    case "model":
-      return m$1`<${ModelEventView}
-        id=${id}
-        event=${node.event}
-        style=${style}
-      />`;
-    case "score":
-      return m$1`<${ScoreEventView}
-        id=${id}
-        event=${node.event}
-        style=${style}
-      />`;
-    case "state":
-      return m$1`<${StateEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-      />`;
-    case "step":
-      return m$1`<${StepEventView}
-        id=${id}
-        event=${node.event}
-        children=${node.children}
-        stateManager=${stateManager}
-        style=${style}
-      />`;
-    case "store":
-      return m$1`<${StateEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-      />`;
-    case "subtask":
-      return m$1`<${SubtaskEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-        depth=${node.depth}
-      />`;
-    case "tool":
-      return m$1`<${ToolEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-        depth=${node.depth}
-      />`;
-    case "error":
-      return m$1`<${ErrorEventView}
-        id=${id}
-        event=${node.event}
-        stateManager=${stateManager}
-        style=${style}
-      />`;
-    default:
-      return m$1``;
-  }
-};
-const fixupEventStream = (events) => {
-  const initEventIndex = events.findIndex((e2) => {
-    return e2.event === "sample_init";
-  });
-  const initEvent = events[initEventIndex];
-  const fixedUp = [...events];
-  if (initEvent) {
-    fixedUp.splice(initEventIndex, 0, {
-      timestamp: initEvent.timestamp,
-      event: "step",
-      action: "begin",
-      type: null,
-      name: "sample_init"
-    });
-    fixedUp.splice(initEventIndex + 2, 0, {
-      timestamp: initEvent.timestamp,
-      event: "step",
-      action: "end",
-      type: null,
-      name: "sample_init"
-    });
-  }
-  return fixedUp;
-};
-function treeifyEvents(events, depth) {
-  const rootNodes = [];
-  const stack2 = [];
-  const pushNode = (event) => {
-    const node = new EventNode(event, stack2.length + depth);
-    if (stack2.length > 0) {
-      const parentNode = stack2[stack2.length - 1];
-      parentNode.children.push(node);
-    } else {
-      rootNodes.push(node);
-    }
-    return node;
-  };
-  events.forEach((event) => {
-    if (event.event === "step" && event.action === "begin") {
-      const node = pushNode(event);
-      stack2.push(node);
-    } else if (event.event === "step" && event.action === "end") {
-      if (stack2.length > 0) {
-        stack2.pop();
-      }
-    } else {
-      pushNode(event);
-    }
-  });
-  return rootNodes;
 }
 /*!
  * https://github.com/Starcounter-Jack/JSON-Patch
@@ -18613,6 +18436,194 @@ const initStateManager = () => {
     }
   };
 };
+const TranscriptView = ({ id, events, stateManager, depth = 0 }) => {
+  const resolvedEvents = fixupEventStream(events);
+  const eventNodes = treeifyEvents(resolvedEvents, depth);
+  return m$1`
+    <${TranscriptComponent}
+      id=${id}
+      eventNodes=${eventNodes}
+      stateManager=${stateManager}
+    />
+  `;
+};
+const TranscriptComponent = ({
+  id,
+  eventNodes,
+  style,
+  stateManager
+}) => {
+  const rows = eventNodes.map((eventNode, index) => {
+    const toggleStyle = {};
+    if (eventNode.depth % 2 == 0) {
+      toggleStyle.backgroundColor = "var(--bs-light-bg-subtle)";
+    } else {
+      toggleStyle.backgroundColor = "var(--bs-body-bg)";
+    }
+    if (index === eventNodes.length - 1) {
+      toggleStyle.marginBottom = "0";
+    } else if (eventNode.depth === 0) {
+      toggleStyle.marginBottom = "1.5em";
+    }
+    const row = m$1`
+      <${RenderedEventNode}
+        id=${`${id}-event${index}`}
+        node=${eventNode}
+        stateManager=${stateManager}
+        style=${{
+      ...toggleStyle,
+      ...style
+    }}
+      />
+    `;
+    return row;
+  });
+  return m$1`<div
+    id=${id}
+    key=${id}
+    style=${{
+    fontSize: FontSize.small,
+    display: "grid",
+    margin: "0.5em 0 0 0",
+    width: "100%"
+  }}
+  >
+    ${rows}
+  </div>`;
+};
+const RenderedEventNode = ({ id, node, style, stateManager }) => {
+  switch (node.event.event) {
+    case "sample_init":
+      return m$1`<${SampleInitEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${stateManager}
+        style=${style}
+      />`;
+    case "info":
+      return m$1`<${InfoEventView}
+        id=${id}
+        event=${node.event}
+        style=${style}
+      />`;
+    case "logger":
+      return m$1`<${LoggerEventView}
+        id=${id}
+        event=${node.event}
+        style=${style}
+      />`;
+    case "model":
+      return m$1`<${ModelEventView}
+        id=${id}
+        event=${node.event}
+        style=${style}
+      />`;
+    case "score":
+      return m$1`<${ScoreEventView}
+        id=${id}
+        event=${node.event}
+        style=${style}
+      />`;
+    case "state":
+      return m$1`<${StateEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${stateManager}
+        style=${style}
+      />`;
+    case "step":
+      return m$1`<${StepEventView}
+        id=${id}
+        event=${node.event}
+        children=${node.children}
+        stateManager=${stateManager}
+        style=${style}
+      />`;
+    case "store":
+      return m$1`<${StateEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${initStateManager()}
+        style=${style}
+      />`;
+    case "subtask":
+      return m$1`<${SubtaskEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${stateManager}
+        style=${style}
+        depth=${node.depth}
+      />`;
+    case "tool":
+      return m$1`<${ToolEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${stateManager}
+        style=${style}
+        depth=${node.depth}
+      />`;
+    case "error":
+      return m$1`<${ErrorEventView}
+        id=${id}
+        event=${node.event}
+        stateManager=${stateManager}
+        style=${style}
+      />`;
+    default:
+      return m$1``;
+  }
+};
+const fixupEventStream = (events) => {
+  const initEventIndex = events.findIndex((e2) => {
+    return e2.event === "sample_init";
+  });
+  const initEvent = events[initEventIndex];
+  const fixedUp = [...events];
+  if (initEvent) {
+    fixedUp.splice(initEventIndex, 0, {
+      timestamp: initEvent.timestamp,
+      event: "step",
+      action: "begin",
+      type: null,
+      name: "sample_init"
+    });
+    fixedUp.splice(initEventIndex + 2, 0, {
+      timestamp: initEvent.timestamp,
+      event: "step",
+      action: "end",
+      type: null,
+      name: "sample_init"
+    });
+  }
+  return fixedUp;
+};
+function treeifyEvents(events, depth) {
+  const rootNodes = [];
+  const stack2 = [];
+  const pushNode = (event) => {
+    const node = new EventNode(event, stack2.length + depth);
+    if (stack2.length > 0) {
+      const parentNode = stack2[stack2.length - 1];
+      parentNode.children.push(node);
+    } else {
+      rootNodes.push(node);
+    }
+    return node;
+  };
+  events.forEach((event) => {
+    if (event.event === "step" && event.action === "begin") {
+      const node = pushNode(event);
+      stack2.push(node);
+    } else if (event.event === "step" && event.action === "end") {
+      if (stack2.length > 0) {
+        stack2.pop();
+      }
+    } else {
+      pushNode(event);
+    }
+  });
+  return rootNodes;
+}
 const kContentProtocol = "tc://";
 const SampleTranscript = ({ id, evalEvents }) => {
   const stateManager = initStateManager();
@@ -22435,7 +22446,7 @@ const FindBand = ({ hideBand }) => {
     top: 0,
     right: 0,
     marginRight: "20%",
-    zIndex: "1050",
+    zIndex: "1060",
     color: "var(--inspect-find-foreground)",
     backgroundColor: "var(--inspect-find-background)",
     fontSize: "0.9rem",
