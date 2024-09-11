8db2e58f55ebdbc51be584d8775a63971b7619a1
Charles Teague <cteague@gmail.com>
2024-09-11 15:22:21 -0400

    Add terminal output

--- src/inspect_ai/_view/bundle.py
+++ src/inspect_ai/_view/bundle.py
@@ -1,10 +1,14 @@
 import logging
+import math
 import os
 import shutil
 import tempfile
+from contextlib import contextmanager
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Iterator
 
+from inspect_ai._display import display
+from inspect_ai._display._display import Progress
 from inspect_ai._util.error import PrerequisiteError
 from inspect_ai._util.file import filesystem
 from inspect_ai.log._file import (
@@ -52,29 +56,41 @@ def bundle(
             f"The output directory {output_dir} already exists. Choose another output directory or use `overwrite` to overwrite the directory and contents"
         )
 
-    # Work in a temporary working directory
-    with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as working_dir:
-        # copy dist to output_dir
-        copy_dir_contents(WWW_DIR, working_dir)
+    display().print(f"Bundling log directory: {log_dir}")
+    with display().progress(total=100) as p:
+        # Work in a temporary working directory
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as working_dir:
+            # copy dist to output_dir
+            display().print("\u2022 Adding viewer")
+            copy_dir_contents(WWW_DIR, working_dir)
+            p.update(5)
 
-        # create a logs dir
-        log_dir_name = "logs"
-        view_logs_dir = os.path.join(working_dir, log_dir_name)
-        os.makedirs(view_logs_dir)
+            # create a logs dir
+            display().print("\u2022 Copying logs")
+            log_dir_name = "logs"
+            view_logs_dir = os.path.join(working_dir, log_dir_name)
+            os.makedirs(view_logs_dir)
+            p.update(5)
 
-        # Copy the logs to the log dir
-        copy_log_files(log_dir, view_logs_dir, fs_options)
+            # Copy the logs to the log dir
+            copy_log_files(log_dir, view_logs_dir, p, fs_options)
 
-        # Always regenerate the manifest
-        write_log_dir_manifest(view_logs_dir)
+            # Always regenerate the manifest
+            display().print("\u2022 Updating manifest")
+            write_log_dir_manifest(view_logs_dir)
+            p.update(5)
 
-        # update the index html to embed the log_dir
-        inject_configuration(
-            os.path.join(working_dir, "index.html"), log_dir=log_dir_name
-        )
+            # update the index html to embed the log_dir
+            inject_configuration(
+                os.path.join(working_dir, "index.html"), log_dir=log_dir_name
+            )
+            p.update(5)
 
-        # Now move the contents of the working directory to the output directory
-        move_output(working_dir, output_dir, fs_options)
+            # Now move the contents of the working directory to the output directory
+            display().print("\u2022 Copying to output directory")
+            move_output(working_dir, output_dir, p, fs_options)
+            p.complete()
+    display().print(f"Bundle Directory: {output_dir}")
 
 
 def copy_dir_contents(source_dir: str, dest_dir: str) -> None:
@@ -112,7 +128,7 @@ def inject_configuration(html_file: str, log_dir: str) -> None:
 
 
 def copy_log_files(
-    log_dir: str, target_dir: str, fs_options: dict[str, Any] = {}
+    log_dir: str, target_dir: str, p: Progress, fs_options: dict[str, Any] = {}
 ) -> None:
     log_fs = filesystem(log_dir, fs_options)
     if log_fs.exists(log_dir):
@@ -121,14 +137,19 @@ def copy_log_files(
         )
 
         base_log_dir = log_fs.info(log_dir).name
-        for eval_log in eval_logs:
-            relative_path = os.path.relpath(eval_log.name, base_log_dir)
-            log_fs.get_file(eval_log.name, os.path.join(target_dir, relative_path))
+        with progress_adapter(p, 40, len(eval_logs)) as tick:
+            for eval_log in eval_logs:
+                relative_path = os.path.relpath(eval_log.name, base_log_dir)
+                log_fs.get_file(eval_log.name, os.path.join(target_dir, relative_path))
+                tick()
+
     else:
         raise PrerequisiteError(f"The log directory {log_dir} doesn't exist.")
 
 
-def move_output(from_dir: str, to_dir: str, fs_options: dict[str, Any] = {}) -> None:
+def move_output(
+    from_dir: str, to_dir: str, p: Progress, fs_options: dict[str, Any] = {}
+) -> None:
     output_fs = filesystem(to_dir, fs_options)
     for root, _dirs, files in os.walk(from_dir):
         # The relative path of the file to move
@@ -140,8 +161,28 @@ def move_output(from_dir: str, to_dir: str, fs_options: dict[str, Any] = {}) -> 
             output_fs.mkdir(dir_path)
 
         # Copy the files
-        for working_file in files:
-            target_path = os.path.join(relative_dir, working_file)
-            src = os.path.join(root, working_file)
-            dest = os.path.join(to_dir, target_path)
-            output_fs.put_file(src, dest)
+        with progress_adapter(p, 40, len(files)) as tick:
+            for working_file in files:
+                target_path = os.path.join(relative_dir, working_file)
+                src = os.path.join(root, working_file)
+                dest = os.path.join(to_dir, target_path)
+                output_fs.put_file(src, dest)
+                tick()
+
+
+@contextmanager
+def progress_adapter(
+    p: Progress, units: int, total_ticks: int
+) -> Iterator[Callable[[], None]]:
+    # Allocate 'units' ticks to represent the progress in
+    # in 'total_ticks', adjusting the size of the increments based
+    # upon the total ticks
+    ticks = 0.0
+    increment = units / total_ticks
+
+    def tick() -> None:
+        nonlocal ticks
+        ticks = ticks + increment
+        p.update(math.floor(ticks))
+
+    yield tick
