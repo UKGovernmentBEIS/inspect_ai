c2d832e9914398f368145ed775b6a8bc90a9ab35
max-kaufmann <max.a.kaufmann@gmail.com>
2024-09-16 11:09:36 +0000

    GAIA addition

--- evals/gaia/gaia.py
+++ evals/gaia/gaia.py
@@ -23,26 +23,27 @@ Any files mentioned in the question can be found in your home directory. Here is
 
 {question}"""
 
-#TODO: This is a little awkward, since initalizing the web search tool requires you to have an API key, but I don't want to force people to have to download that
+# TODO: This is a little awkward, since initalizing the web search tool requires you to have an API key, but I don't want to force people to have to download that
 DEFAULT_PLAN = basic_agent(
     init=system_message(
         "Please solve the coding task below. Once you are done, use your submit tool."
     ),
-    tools=[ bash()], # + [web_search()] 
+    tools=[bash()],  # + [web_search()]
 )
 
 GAIA_DATASET_LOCATION = Path(__file__).parent / "resources" / "GAIA"
-
+GAIA_SUBSETS = ["2023_all", "2023_level1", "2023_level2", "2023_level3"]
 
 @task
 def gaia(
     plan=DEFAULT_PLAN,
     split: Literal["test", "validation"] = "validation",
+    subset: Literal[GAIA_SUBSETS] = "2023_all",
     filter: Callable[Sample, bool] = lambda x: True,
 ) -> Task:
     # TODO: Is this the right interface?
     dataset = hf_dataset(
-        GAIA_DATASET_LOCATION,
+        str(GAIA_DATASET_LOCATION),
         split=split,
         sample_fields=FieldSpec(
             input="Question",
@@ -50,6 +51,8 @@ def gaia(
             id="task_id",
             metadata=["file_name", "Annotator Metadata"],
         ),
+        trust=True,  # Trust GAIA's remote code execution during dataset loading
+        name=subset,
     )
 
     dataset = dataset.filter(filter)
@@ -60,21 +63,22 @@ def gaia(
 
 
 def add_gaia_files(dataset: list[Sample], split: str) -> None:
-    files = GAIA_DATASET_LOCATION / "2024" / split
+    files = GAIA_DATASET_LOCATION / "2023" / split
     for sample in dataset:
         sample.input = INPUT_PROMPT.format(question=sample.input)
 
         # Setup files into the sample
-        file = next([file for file in os.listdir(files) if sample.id in file], None)
+        file = next((file for file in os.listdir(files) if sample.id in file), None)
         if file:
             sample.files = {file: str((files / file))}
 
 
-if not GAIA_DATASET_LOCATION.exists():
+if not Path(GAIA_DATASET_LOCATION).exists():
+    GAIA_DATASET_LOCATION.mkdir(parents=True, exist_ok=True)
     snapshot_download(
         repo_id="gaia-benchmark/GAIA",
         repo_type="dataset",
-        local_dir=GAIA_DATASET_LOCATION.parent,
+        local_dir=GAIA_DATASET_LOCATION,
     )
 
 gaia()
