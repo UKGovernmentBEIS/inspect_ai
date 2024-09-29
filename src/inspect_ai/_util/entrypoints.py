3f9181e4581df70cbb15f51b6e3ae81554ed08d4
J.J. Allaire <jj.allaire@gmail.com>
2024-09-29 09:18:38 +0100

    load setuptools entry points per-package

--- src/inspect_ai/_util/entrypoints.py
+++ src/inspect_ai/_util/entrypoints.py
@@ -1,23 +1,49 @@
-from importlib.metadata import EntryPoints, entry_points
+from importlib.metadata import entry_points
 from logging import getLogger
 
 logger = getLogger(__name__)
 
 
-def ensure_entry_points() -> None:
-    # ensure that inspect model provider extensions are loaded if
-    # they haven't been already
-    global _inspect_ai_eps
-    if not _inspect_ai_eps:
-        _inspect_ai_eps = entry_points(group="inspect_ai")
-        for ep in _inspect_ai_eps:
-            try:
+def ensure_entry_points(package: str | None = None) -> None:
+    # have we already loaded all entry points?
+    global _inspect_ai_eps_loaded_all
+    if _inspect_ai_eps_loaded_all:
+        return None
+
+    # have we already loaded entry points for this package?
+    if package in _inspect_ai_eps_loaded:
+        return None
+
+    # inspect_evals is special b/c it's in _this_ package,
+    # load it manually
+    if package == "inspect_evals":
+        from inspect_evals import _registry  # noqa: F401
+
+        _inspect_ai_eps_loaded.append(package)
+        return None
+
+    # enumerate entry points
+    eps = entry_points(group="inspect_ai")
+    for ep in eps:
+        try:
+            # if there is a package filter then condition on that
+            if package is not None:
+                if ep.dist and ep.dist.name == package:
+                    ep.load()
+                    _inspect_ai_eps_loaded.append(package)
+
+            # if there is no package filter then load unconditionally
+            # and mark us as fully loaded
+            else:
                 ep.load()
-            except Exception as ex:
-                logger.warning(
-                    f"Unexpected exception loading entrypoints from '{ep.value}': {ex}"
-                )
+                _inspect_ai_eps_loaded_all = True
+
+        except Exception as ex:
+            logger.warning(
+                f"Unexpected exception loading entrypoints from '{ep.value}': {ex}"
+            )
 
 
 # inspect extension entry points
-_inspect_ai_eps: EntryPoints | None = None
+_inspect_ai_eps_loaded: list[str] = []
+_inspect_ai_eps_loaded_all: bool = False
