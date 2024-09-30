d8b52771e277ea36edb9b4be16af62de9631a5c4
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-09-30 17:37:13 +0000

    more web_browser tests

--- tests/tools/test_web_browser.py
+++ tests/tools/test_web_browser.py
@@ -3,25 +3,23 @@ from typing import Literal
 
 import pytest
 from test_helpers.tool_call_utils import get_tool_call, get_tool_response
-from test_helpers.utils import skip_if_no_docker
+from test_helpers.utils import skip_if_no_docker, skip_if_no_openai
 
 from inspect_ai import Task, eval
 from inspect_ai.dataset import Sample
 from inspect_ai.model import ModelOutput, get_model
 from inspect_ai.solver import generate, use_tools
 from inspect_ai.tool import web_browser_tools
+from inspect_ai.util import SandboxEnvironmentSpec
 
 
 @skip_if_no_docker
 @pytest.mark.slow
-def test_web_browser():
+def test_web_browser_navigation():
     task = Task(
         dataset=[Sample(input="Please use the web_browser tool")],
         solver=[use_tools(web_browser_tools()), generate()],
-        sandbox=(
-            "docker",
-            (Path(__file__).parent / "test_web_browser_compose.yaml").as_posix(),
-        ),
+        sandbox=test_sandbox(),
     )
 
     log = eval(
@@ -103,3 +101,53 @@ def test_web_browser():
     check_tool_call_page("web_browser_back", "repo")
     check_tool_call_page("web_browser_forward", "website")
     check_tool_call_page("web_browser_refresh", "website")
+
+
+@skip_if_no_docker
+@skip_if_no_openai
+@pytest.mark.slow
+def test_web_browser_click():
+    task = Task(
+        dataset=[
+            Sample(
+                input="Please use the web browser tool to navigate to https://inspect.ai-safety-institute.org.uk/. Then, once there, use the web_browser_click tool to click the link to the documentation on Solvers."
+            )
+        ],
+        solver=[use_tools(web_browser_tools()), generate()],
+        sandbox=test_sandbox(),
+    )
+
+    log = eval(task, model="openai/gpt-4o")[0]
+
+    click_call = get_tool_call(log.samples[0].messages, "web_browser_click")
+    assert click_call
+    click_response = get_tool_response(log.samples[0].messages, click_call)
+    assert click_response
+    assert "defines an execution plan" in click_response.text
+
+
+@skip_if_no_docker
+@skip_if_no_openai
+@pytest.mark.slow
+def test_web_browser_input():
+    task = Task(
+        dataset=[
+            Sample(
+                input="Please use the web browser tool to navigate to https://inspect.ai-safety-institute.org.uk/. Then, once there, use the page's search interface to search for 'solvers'"
+            )
+        ],
+        solver=[use_tools(web_browser_tools()), generate()],
+        sandbox=test_sandbox(),
+    )
+
+    log = eval(task, model="openai/gpt-4o")[0]
+
+    type_call = get_tool_call(log.samples[0].messages, "web_browser_type_submit")
+    assert type_call
+
+
+def test_sandbox() -> SandboxEnvironmentSpec:
+    return (
+        "docker",
+        (Path(__file__).parent / "test_web_browser_compose.yaml").as_posix(),
+    )
