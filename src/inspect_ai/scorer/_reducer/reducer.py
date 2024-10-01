5629d78045bfeefe56012cc39bb35aecd3e36d4f
Charles Teague <cteague@gmail.com>
2024-10-01 08:29:11 -0400

    Always preserve first metadata value when reducing scores
    Fixes #587
    
    See conversation on PR #591
    
    The simplest approach which is deterministic is to simply preserve the first metadata value when reducing (without any comparison). This will avoid the complicated comparison of metadata objects which have all the various traps we're running into and keep metadata around for common cases.
    
    The alternative would be to never preserve score metadata, but this would have the effect of clearing the metadata even when a single score is being ‘reduced’ since we run the reducer pass no matter the number of epochs being run.

--- src/inspect_ai/scorer/_reducer/reducer.py
+++ src/inspect_ai/scorer/_reducer/reducer.py
@@ -1,6 +1,6 @@
 import statistics
 from collections import Counter
-from typing import Any, Callable, cast
+from typing import Callable, cast
 
 import numpy as np
 
@@ -331,33 +331,5 @@ def _reduced_score(value: Value, scores: list[Score]) -> Score:
         explanation=scores[0].explanation
         if len(set(score.explanation for score in scores)) == 1
         else None,
-        metadata=scores[0].metadata
-        if len(
-            set(
-                tuple(
-                    (key, _make_hashable(value))
-                    for key, value in score.metadata.items()
-                )
-                for score in scores
-                if score.metadata
-            )
-        )
-        == 1
-        else None,
+        metadata=scores[0].metadata,
     )
-
-
-def _make_hashable(item: Any) -> Any:
-    r"""Recursively convert dictionaries to tuples of tuples to make them hashable.
-
-    Args:
-        item: a dictionary or list of dictionaries
-    """
-    if isinstance(item, dict):
-        return tuple(
-            (key, _make_hashable(value)) for key, value in sorted(item.items())
-        )
-    elif isinstance(item, list):
-        return tuple(_make_hashable(x) for x in item)
-    else:
-        return item
