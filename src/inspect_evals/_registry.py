cbd82f854a382b77236a89aa003842652999d282
Charles Teague <cteague@gmail.com>
2024-10-01 17:15:11 -0400

    Migrate AGIEval to Inspect Evals (#617)
    Co-authored-by: jjallaire <jj.allaire@gmail.com>

--- src/inspect_evals/_registry.py
+++ src/inspect_evals/_registry.py
@@ -1,6 +1,16 @@
 # ruff: noqa: F401
 
-
+from .agieval import (
+    agie_aqua_rat,
+    agie_logiqa_en,
+    agie_lsat_ar,
+    agie_lsat_lr,
+    agie_lsat_rc,
+    agie_math,
+    agie_sat_en,
+    agie_sat_en_without_passage,
+    agie_sat_math,
+)
 from .arc import arc_challenge, arc_easy
 from .boolq import boolq
 from .commonsense_qa import commonsense_qa
---
+++ src/inspect_evals/agieval/__init__.py
@@ -0,0 +1,23 @@
+from .agieval import (
+    agie_aqua_rat,
+    agie_logiqa_en,
+    agie_lsat_ar,
+    agie_lsat_lr,
+    agie_lsat_rc,
+    agie_sat_en,
+    agie_sat_en_without_passage,
+    agie_sat_math,
+)
+from .agieval_cloze import agie_math
+
+__all__ = [
+    "agie_math",
+    "agie_aqua_rat",
+    "agie_logiqa_en",
+    "agie_lsat_ar",
+    "agie_lsat_lr",
+    "agie_lsat_rc",
+    "agie_sat_en",
+    "agie_sat_en_without_passage",
+    "agie_sat_math",
+]
