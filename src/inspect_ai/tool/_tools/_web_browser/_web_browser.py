741918a27007a552c4adfeb1130176f528960a43
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-01 09:07:37 +0000

    more flexible container binding for web browser

--- src/inspect_ai/tool/_tools/_web_browser/_web_browser.py
+++ src/inspect_ai/tool/_tools/_web_browser/_web_browser.py
@@ -2,44 +2,34 @@ import re
 from textwrap import dedent
 
 from inspect_ai._util.error import PrerequisiteError
-from inspect_ai.util._sandbox import SandboxEnvironment, sandbox
+from inspect_ai.util._sandbox import SandboxEnvironment, sandbox_with
 from inspect_ai.util._sandbox.docker.internal import INSPECT_WEB_BROWSER_IMAGE
 
 from ..._tool import Tool, ToolError, tool
 
 
-def web_browser_tools(sandbox: str = "web_browser") -> list[Tool]:
+def web_browser_tools() -> list[Tool]:
     """Tools used for web browser navigation.
-
-    Args:
-      sandbox (str): Name of sandbox that the web
-        browser is running within. Defaults to
-        "web_browser"
 
     Returns:
        List of tools used for web browser navigation.
 
     """
     return [
-        web_browser_go(sandbox),
-        web_browser_click(sandbox),
-        web_browser_scroll(sandbox),
-        web_browser_forward(sandbox),
-        web_browser_back(sandbox),
-        web_browser_refresh(sandbox),
-        web_browser_type(sandbox),
-        web_browser_type_submit(sandbox),
+        web_browser_go(),
+        web_browser_click(),
+        web_browser_scroll(),
+        web_browser_forward(),
+        web_browser_back(),
+        web_browser_refresh(),
+        web_browser_type(),
+        web_browser_type_submit(),
     ]
 
 
 @tool
-def web_browser_go(sandbox: str = "web_browser") -> Tool:
+def web_browser_go() -> Tool:
     """Web Browser tool for navigation to a URL.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser navigation tool.
@@ -54,19 +44,14 @@ def web_browser_go(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_go", url)
+        return await web_browser_cmd("web_go", url)
 
     return execute
 
 
 @tool
-def web_browser_click(sandbox: str = "web_browser") -> Tool:
+def web_browser_click() -> Tool:
     """Web Browser tool for clicking an element on a web page.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser clicking tool.
@@ -81,19 +66,14 @@ def web_browser_click(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_click", element_id)
+        return await web_browser_cmd("web_click", element_id)
 
     return execute
 
 
 @tool
-def web_browser_scroll(sandbox: str = "web_browser") -> Tool:
+def web_browser_scroll() -> Tool:
     """Web Browser tool for scrolling up or down one page.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser scrolling tool.
@@ -108,13 +88,13 @@ def web_browser_scroll(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_scroll", direction)
+        return await web_browser_cmd("web_scroll", direction)
 
     return execute
 
 
 @tool
-def web_browser_forward(sandbox: str = "web_browser") -> Tool:
+def web_browser_forward() -> Tool:
     """Web Browser tool for navigating forward in the browser history.
 
     Args:
@@ -132,19 +112,14 @@ def web_browser_forward(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_forward")
+        return await web_browser_cmd("web_forward")
 
     return execute
 
 
 @tool
-def web_browser_back(sandbox: str = "web_browser") -> Tool:
+def web_browser_back() -> Tool:
     """Web Browser tool for navigating back in the browser history.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser back navigation tool.
@@ -156,19 +131,14 @@ def web_browser_back(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_back")
+        return await web_browser_cmd("web_back")
 
     return execute
 
 
 @tool
-def web_browser_refresh(sandbox: str = "web_browser") -> Tool:
+def web_browser_refresh() -> Tool:
     """Web Browser tool for refreshing the current page.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser page refresh tool.
@@ -180,19 +150,14 @@ def web_browser_refresh(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_refresh")
+        return await web_browser_cmd("web_refresh")
 
     return execute
 
 
 @tool
-def web_browser_type(sandbox: str = "web_browser") -> Tool:
+def web_browser_type() -> Tool:
     """Web Browser tool for typing into inputs.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser typing tool.
@@ -208,19 +173,14 @@ def web_browser_type(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_type", element_id, text)
+        return await web_browser_cmd("web_type", element_id, text)
 
     return execute
 
 
 @tool
-def web_browser_type_submit(sandbox: str = "web_browser") -> Tool:
+def web_browser_type_submit() -> Tool:
     """Web Browser tool for typing and submitting input.
-
-    Args:
-       sandbox (str): Name of sandbox that the web
-         browser is running within. Defaults to
-         "web_browser"
 
     Returns:
        Web browser type and submit tool.
@@ -236,14 +196,17 @@ def web_browser_type_submit(sandbox: str = "web_browser") -> Tool:
         Returns:
            Web accessibility tree of the visible elements of the web page. The element_id of each element is displayed in brackets at the beginning of the line.
         """
-        return await web_browser_cmd(sandbox, "web_type_submit", element_id, text)
+        return await web_browser_cmd("web_type_submit", element_id, text)
 
     return execute
 
 
-async def web_browser_cmd(sandbox: str, cmd: str, *args: str) -> str:
-    result = await web_browser_sandbox(sandbox).exec(
-        ["python3", "/app/web_browser/web_client.py", cmd] + list(args)
+WEB_CLIENT_SCRIPT = "/app/web_browser/web_client.py"
+
+
+async def web_browser_cmd(cmd: str, *args: str) -> str:
+    result = await (await web_browser_sandbox()).exec(
+        ["python3", WEB_CLIENT_SCRIPT, cmd] + list(args)
     )
     if not result.success:
         raise RuntimeError(
@@ -261,13 +224,20 @@ async def web_browser_cmd(sandbox: str, cmd: str, *args: str) -> str:
             )
 
 
-def web_browser_sandbox(name: str) -> SandboxEnvironment:
-    try:
-        return sandbox(name)
-    except ValueError as ex:
-        if name == "web_browser":
-            msg = dedent(f"""
-                The web_browser service has not been configured for this task. Please add the web_browser service to your configuration. For example, the following Docker compose file includes the web_browser service:
+async def web_browser_sandbox() -> SandboxEnvironment:
+    sb = await sandbox_with(WEB_CLIENT_SCRIPT)
+    if sb:
+        return sb
+    else:
+        msg = dedent(f"""
+                The web browser service was not found in any of the sandboxes for this sample. Please add the web browser service to your configuration. For example, the following Docker compose file uses the {INSPECT_WEB_BROWSER_IMAGE} image as its default sandbox:
+
+                services:
+                  default:
+                    image: "{INSPECT_WEB_BROWSER_IMAGE}"
+                    init: true
+
+                Alternatively, this Docker compose file creates a dedicated image for the web browser service:
 
                 services:
                   default:
@@ -279,9 +249,7 @@ def web_browser_sandbox(name: str) -> SandboxEnvironment:
                     image: "{INSPECT_WEB_BROWSER_IMAGE}"
                     init: true
                 """).strip()
-            raise PrerequisiteError(msg)
-        else:
-            raise ex
+        raise PrerequisiteError(msg)
 
 
 def parse_web_browser_output(output: str) -> dict[str, str]:
