713493d94e3367d2645206ed73e9662a2acefac2
Charles Teague <cteague@gmail.com>
2024-10-01 15:25:07 -0400

    Migrate PIQA to Inspect Evals (#611)

--- src/inspect_evals/_registry.py
+++ src/inspect_evals/_registry.py
@@ -12,5 +12,6 @@ from .ifeval import ifeval
 from .mathvista import mathvista
 from .mbpp import mbpp
 from .mmmu import mmmu_multiple_choice, mmmu_open
+from .piqa import piqa
 from .race_h import race_h
 from .swe_bench import swe_bench
