2c4c8ce5e0deea0f1dac0265e657b965cd679a9b
mi sha kilianovski <mitutee@gmail.com>
2024-10-04 07:20:56 +0100

    Add support for Tab separated values `.tsv` (#637)
    Co-authored-by: jjallaire-aisi <joseph.allaire@dsit.gov.uk>

--- src/inspect_ai/dataset/_sources/csv.py
+++ src/inspect_ai/dataset/_sources/csv.py
@@ -1,7 +1,7 @@
 import csv
 from io import TextIOWrapper
 from pathlib import Path
-from typing import Any
+from typing import Any, List
 
 from inspect_ai._util.file import file
 from inspect_ai.dataset._sources.util import resolve_sample_files
@@ -27,13 +27,14 @@ def csv_dataset(
     encoding: str = "utf-8",
     name: str | None = None,
     fs_options: dict[str, Any] = {},
+    fieldnames: list[str] | None = None,
 ) -> Dataset:
     r"""Read dataset from CSV file.
 
     Args:
-        csv_file (str): Path to CSV file. Can be a local filesystem path or
-            a path to an S3 bucket (e.g. "s3://my-bucket"). Use `fs_options`
-            to pass arguments through to the `S3FileSystem` constructor.
+        csv_file (str): Path to CSV file. Can be a local filesystem path,
+            a path to an S3 bucket (e.g. "s3://my-bucket"), or an HTTPS URL.
+            Use `fs_options` to pass arguments through to the `S3FileSystem` constructor.
         sample_fields (FieldSpec | RecordToSample): Method of mapping underlying
             fields in the data source to Sample objects. Pass `None` if the data is already
             stored in `Sample` form (i.e. has "input" and "target" columns.); Pass a
@@ -43,13 +44,16 @@ def csv_dataset(
         shuffle (bool): Randomly shuffle the dataset order.
         seed: (int | None): Seed used for random shuffle.
         limit (int | None): Limit the number of records to read.
-        dialect (str): CSV dialect ("unix" or "excel", defaults to "unix").
+        dialect (str): CSV dialect ("unix", "excel" or"excel-tab"). Defaults to "unix". See https://docs.python.org/3/library/csv.html#dialects-and-formatting-parameters for more details
         encoding (str): Text encoding for file (defaults to "utf-8").
         name (str): Optional name for dataset (for logging). If not specified,
             defaults to the stem of the filename
         fs_options (dict[str, Any]): Optional. Additional arguments to pass through
             to the filesystem provider (e.g. `S3FileSystem`). Use `{"anon": True }`
             if you are accessing a public S3 bucket with no credentials.
+        fieldnames (list[str] | None): Optional. A list of fieldnames to use for the CSV.
+            If None, the values in the first row of file f will be used as the fieldnames.
+            Useful for files without a header.
 
     Returns:
         Dataset read from CSV file.
@@ -62,7 +66,7 @@ def csv_dataset(
         # filter out rows with empty values
         valid_data = [
             data
-            for data in csv_dataset_reader(f, dialect)
+            for data in csv_dataset_reader(f, dialect, fieldnames)
             if data and any(value.strip() for value in data.values())
         ]
         name = name if name else Path(csv_file).stem
@@ -86,5 +90,7 @@ def csv_dataset(
         return dataset
 
 
-def csv_dataset_reader(file: TextIOWrapper, dialect: str = "unix") -> DatasetReader:
-    return csv.DictReader(file, dialect=dialect)
+def csv_dataset_reader(
+    file: TextIOWrapper, dialect: str = "unix", fieldnames: List[str] | None = None
+) -> DatasetReader:
+    return csv.DictReader(file, dialect=dialect, fieldnames=fieldnames)
