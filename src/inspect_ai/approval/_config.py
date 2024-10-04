10722ce89ba429213dd592a0305cff7d63f1f473
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-04 11:04:48 +0000

    approval policies

--- src/inspect_ai/approval/_config.py
+++ src/inspect_ai/approval/_config.py
@@ -1,10 +1,23 @@
 import json
-from typing import Any
+from typing import Any, cast
 
 import yaml
 from pydantic import BaseModel, Field
 
+from inspect_ai._util.registry import registry_create
 from inspect_ai.util._resource import resource
+
+from ._approver import Approver
+from ._policy import ApprovalPolicy, policy_approver
+
+"""
+approvers:
+   - name: human
+     tools: [bash, python, web_browser*]
+
+   - name: auto
+     tools: *
+"""
 
 
 class ApproverPolicyConfig(BaseModel):
@@ -17,20 +30,40 @@ class ApprovalPolicyConfig(BaseModel):
     approvers: list[ApproverPolicyConfig]
 
 
-def read_policy_config(policy: str | dict[str, Any]) -> ApprovalPolicyConfig:
+def approver_from_config(policy_config: str) -> Approver:
+    policies = approval_policies_from_config(policy_config)
+    return policy_approver(policies)
+
+
+def approval_policies_from_config(policy_config: str) -> list[ApprovalPolicy]:
+    # map config -> policy
+    def policy_from_config(config: ApproverPolicyConfig) -> ApprovalPolicy:
+        approver = cast(
+            Approver, registry_create("approver", config.name, **config.params)
+        )
+        return ApprovalPolicy(approver=approver, tools=config.tools)
+
+    # resolve into approval policies
+    return [
+        policy_from_config(config)
+        for config in read_policy_config(policy_config).approvers
+    ]
+
+
+def read_policy_config(policy_config: str) -> ApprovalPolicyConfig:
     # save specified policy for error message
-    specified_policy = policy
+    specified_policy_config = policy_config
 
-    # first resolve policy to dict
-    if isinstance(policy, str):
-        # accept string or filename
-        policy = resource(policy)
+    # accept string or filename
+    policy_config = resource(policy_config)
 
-        # detect json vs. yaml
-        is_json = policy.strip().startswith("{")
-        policy = json.loads(policy) if is_json else yaml.safe_load(policy)
-        if not isinstance(policy, dict):
-            raise ValueError(f"Invalid approval policy: {specified_policy}")
+    # detect json vs. yaml
+    is_json = policy_config.strip().startswith("{")
+    policy_config_dict = (
+        json.loads(policy_config) if is_json else yaml.safe_load(policy_config)
+    )
+    if not isinstance(policy_config_dict, dict):
+        raise ValueError(f"Invalid approval policy: {specified_policy_config}")
 
     # parse and validate config
-    return ApprovalPolicyConfig(**policy)
+    return ApprovalPolicyConfig(**policy_config_dict)
--- src/inspect_ai/approval/_policy.py
+++ src/inspect_ai/approval/_policy.py
@@ -1,37 +1,195 @@
+import fnmatch
+import re
+from dataclasses import dataclass
 from re import Pattern
+from typing import Generator
+
+from rich import print
+from rich.panel import Panel
+
+from inspect_ai._util.registry import registry_info
 from inspect_ai.solver._task_state import TaskState
 from inspect_ai.tool._tool_call import ToolCall
 
 from ._approver import Approval, Approver
-
-"""
-approvers:
-   - name: human
-     tools: [bash, python, web_browser*]
-     params:
-        ansi: false
-
-   - name: auto
-     tools: *
+from ._config import approval_policies_from_config
 
 
-"""
-
-
+@dataclass
 class ApprovalPolicy:
     approver: Approver
     tools: str | list[str]
 
 
-def policy_approver(policies: list[ApprovalPolicy]) -> Approver:
-    # compile policy into approvers and regexes for matching
-    # policy_matchers: list[list[Pattern], Approver] = []
+def policy_approver(
+    policies: str | list[ApprovalPolicy], print: bool = True, log: bool = True
+) -> Approver:
+    # if policies is a str then its a config file
+    if isinstance(policies, str):
+        policies = approval_policies_from_config(policies)
 
-    # function to find
+    # compile policy into approvers and regexes for matching
+    policy_matchers: list[tuple[list[Pattern[str]], Approver]] = []
+    for policy in policies:
+        tools = [policy.tools] if isinstance(policy.tools, str) else policy.tools
+        patterns = [re.compile(fnmatch.translate(tool)) for tool in tools]
+        policy_matchers.append((patterns, policy.approver))
+
+    # generator for policies that match a tool_call
+    def tool_approvers(tool_call: ToolCall) -> Generator[Approver, None, None]:
+        for policy_matcher in iter(policy_matchers):
+            if any(
+                [pattern.match(tool_call.function) for pattern in policy_matcher[0]]
+            ):
+                yield policy_matcher[1]
 
     async def approve(
         tool_call: ToolCall, tool_view: str, state: TaskState | None = None
     ) -> Approval:
-        return Approval(decision="approve")
+        # process approvers for this tool call (continue loop on "escalate")
+        for approver in tool_approvers(tool_call):
+            approval = await call_approver(
+                approver, tool_call, tool_view, state, print, log
+            )
+            if approval.decision != "escalate":
+                return approval
+
+        # if there are no approvers then we reject
+        reject = Approval(
+            decision="reject",
+            explanation=f"No approvers registered for tool {tool_call.function}",
+        )
+        # record and return the rejection
+        record_approval("policy", reject, tool_call, print, log)
+        return reject
 
     return approve
+
+
+async def call_approver(
+    approver: Approver,
+    tool_call: ToolCall,
+    tool_view: str,
+    state: TaskState | None,
+    print: bool = True,
+    log: bool = True,
+) -> Approval:
+    # run approver
+    approval = await approver(tool_call, tool_view, state)
+
+    # record as required
+    record_approval(
+        approver_name=registry_info(approver).name,
+        approval=approval,
+        tool_call=tool_call,
+        print=print,
+        log=log,
+    )
+
+    # return approval
+    return approval
+
+
+def record_approval(
+    approver_name: str,
+    approval: Approval,
+    tool_call: ToolCall,
+    print: bool = True,
+    log: bool = True,
+) -> None:
+    # print if requested
+    if print:
+        match approval.decision:
+            case "approve":
+                print_approve_message(
+                    approver_name=approver_name,
+                    tool_call=tool_call,
+                    explanation=approval.explanation,
+                )
+            case "reject":
+                print_reject_message(
+                    approver_name=approver_name,
+                    tool_call=tool_call,
+                    explanation=approval.explanation,
+                )
+            case "terminate":
+                print_terminate_message(
+                    approver_name=approver_name,
+                    tool_call=tool_call,
+                    explanation=approval.explanation,
+                )
+            case "escalate":
+                print_escalate_message(
+                    approver_name=approver_name,
+                    tool_call=tool_call,
+                    explanation=approval.explanation,
+                )
+
+
+def print_approve_message(
+    approver_name: str, tool_call: ToolCall, explanation: str | None
+) -> None:
+    print_tool_message(
+        approver_name=approver_name,
+        title="Tool Execution",
+        subtitle="Approved",
+        message="Tool call approved",
+        tool_call=tool_call,
+        explanation=explanation,
+    )
+
+
+def print_reject_message(
+    approver_name: str, tool_call: ToolCall, explanation: str | None
+) -> None:
+    print_tool_message(
+        approver_name=approver_name,
+        title="Tool Execution",
+        subtitle="Rejected",
+        message="Tool call rejected",
+        tool_call=tool_call,
+        explanation=explanation,
+    )
+
+
+def print_terminate_message(
+    approver_name: str, tool_call: ToolCall, explanation: str | None
+) -> None:
+    print_tool_message(
+        approver_name=approver_name,
+        title="Execution Terminated",
+        subtitle="System Shutdown",
+        message="Execution terminated",
+        tool_call=tool_call,
+        explanation=explanation,
+    )
+
+
+def print_escalate_message(
+    approver_name: str, tool_call: ToolCall, explanation: str | None
+) -> None:
+    print_tool_message(
+        approver_name=approver_name,
+        title="Tool Execution",
+        subtitle="Escalated",
+        message="Tool call escalated",
+        tool_call=tool_call,
+        explanation=explanation,
+    )
+
+
+def print_tool_message(
+    approver_name: str,
+    title: str,
+    subtitle: str,
+    message: str,
+    tool_call: ToolCall,
+    explanation: str | None,
+) -> None:
+    print(
+        Panel(
+            f"{message} ({approver_name}):\nFunction: {tool_call.function}\nArguments: {tool_call.arguments}\nReason: {explanation}",
+            title=title,
+            subtitle=subtitle,
+        )
+    )
