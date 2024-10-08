32a6c3c6e8f6998721266a2b5e14ee6ea72c70a1
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-08 08:30:15 +0000

    add negating test

--- tests/log/test_log_level.py
+++ tests/log/test_log_level.py
@@ -1,7 +1,8 @@
-from logging import getLogger
+from logging import DEBUG, getLogger
 
 from inspect_ai import Task, eval
 from inspect_ai._util.constants import SANDBOX
+from inspect_ai.log._log import EvalLog
 from inspect_ai.log._transcript import LoggerEvent
 from inspect_ai.model import get_model
 from inspect_ai.solver import Generate, TaskState, solver
@@ -11,29 +12,42 @@ logger = getLogger(__name__)
 
 def test_log_file_level() -> None:
     @solver
-    def logging_solver():
+    def logging_solver(level_no: int):
         async def solve(state: TaskState, generate: Generate):
-            logger.log(SANDBOX, "sandbox log entry")
+            logger.log(level_no, "sandbox log entry")
             return state
 
         return solve
 
+    def find_logger_event(log: EvalLog) -> LoggerEvent | None:
+        if log.samples:
+            return next(
+                (
+                    event
+                    for event in log.samples[0].transcript.events
+                    if isinstance(event, LoggerEvent)
+                ),
+                None,
+            )
+        else:
+            return None
+
     log = eval(
-        Task(solver=logging_solver()),
+        Task(solver=logging_solver(SANDBOX)),
         model=get_model("mockllm/model"),
         log_level="debug",
         log_file_level="sandbox",
     )[0]
 
-    assert log.status == "success"
-    assert log.samples
-    event: LoggerEvent | None = next(
-        (
-            event
-            for event in log.samples[0].transcript.events
-            if isinstance(event, LoggerEvent)
-        ),
-        None,
-    )
+    event = find_logger_event(log)
     assert event
     assert event.message.level == "sandbox"
+
+    log = eval(
+        Task(solver=logging_solver(DEBUG)),
+        model=get_model("mockllm/model"),
+        log_level="debug",
+        log_file_level="warning",
+    )[0]
+    event = find_logger_event(log)
+    assert event is None
