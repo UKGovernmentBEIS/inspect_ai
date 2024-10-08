d464b24e9d907ba10fe63230e181201c449514bd
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-08 07:33:00 +0000

    cleanup

--- src/inspect_ai/_cli/eval.py
+++ src/inspect_ai/_cli/eval.py
@@ -376,13 +376,14 @@ def eval_command(
     config = config_from_locals(dict(locals()))
 
     # resolve common options
-    (log_dir, log_level) = resolve_common_options(kwargs)
+    (log_dir, log_level, log_file_level) = resolve_common_options(kwargs)
 
     # exec eval
     eval_exec(
         tasks=tasks,
         solver=solver,
         log_level=log_level,
+        log_file_level=log_file_level,
         log_dir=log_dir,
         model=model,
         model_base_url=model_base_url,
@@ -515,13 +516,14 @@ def eval_set_command(
     config = config_from_locals(dict(locals()))
 
     # resolve common options
-    (log_dir, log_level) = resolve_common_options(kwargs)
+    (log_dir, log_level, log_file_level) = resolve_common_options(kwargs)
 
     # exec eval
     success = eval_exec(
         tasks=tasks,
         solver=solver,
         log_level=log_level,
+        log_file_level=log_file_level,
         log_dir=log_dir,
         model=model,
         model_base_url=model_base_url,
@@ -564,6 +566,7 @@ def eval_exec(
     tasks: tuple[str] | None,
     solver: str | None,
     log_level: str,
+    log_file_level: str,
     log_dir: str,
     model: str,
     model_base_url: str | None,
@@ -638,6 +641,7 @@ def eval_exec(
             sandbox=parse_sandbox(sandbox),
             sandbox_cleanup=sandbox_cleanup,
             log_level=log_level,
+            log_file_level=log_file_level,
             log_dir=log_dir,
             limit=eval_limit,
             epochs=eval_epochs,
@@ -792,7 +796,7 @@ def eval_retry_command(
 ) -> None:
     """Retry failed evaluation(s)"""
     # resolve common options
-    (log_dir, log_level) = resolve_common_options(kwargs)
+    (log_dir, log_level, log_file_level) = resolve_common_options(kwargs)
 
     # resolve negating options
     sandbox_cleanup = False if no_sandbox_cleanup else None
@@ -809,6 +813,7 @@ def eval_retry_command(
     eval_retry(
         retry_log_files,
         log_level=log_level,
+        log_file_level=log_file_level,
         log_dir=log_dir,
         max_samples=max_samples,
         max_tasks=max_tasks,
