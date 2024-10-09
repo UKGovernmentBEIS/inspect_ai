c54354e96e440079b917ce8674285dcb50e93580
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-09 11:15:25 +0000

    rest of aiohttp implementation

--- src/inspect_ai/_view/server.py
+++ src/inspect_ai/_view/server.py
@@ -1,4 +1,5 @@
 import os
+from logging import getLogger
 from pathlib import Path
 from typing import Any
 
@@ -7,11 +8,20 @@ from pydantic_core import to_jsonable_python
 
 from inspect_ai._display import display
 from inspect_ai._util.constants import DEFAULT_SERVER_HOST, DEFAULT_VIEW_PORT
-from inspect_ai.log._file import EvalLogInfo, list_eval_logs, read_eval_log_headers
+from inspect_ai.log._file import (
+    EvalLogInfo,
+    eval_log_json,
+    list_eval_logs,
+    read_eval_log,
+    read_eval_log_headers,
+)
 
 from .notify import view_last_eval_time
 
 WWW_DIR = os.path.abspath((Path(__file__).parent / "www" / "dist").as_posix())
+
+
+logger = getLogger(__name__)
 
 
 def view_server(
@@ -26,13 +36,18 @@ def view_server(
         logs = list_eval_logs(
             log_dir=log_dir, recursive=recursive, fs_options=fs_options
         )
-        return web.json_response(log_listing(logs, log_dir))
+        return log_listing_response(logs, log_dir)
 
     # /api/log-headers
     async def api_log_headers(request: web.Request) -> web.Response:
         files = request.query.getall("file", [])
-        headers = read_eval_log_headers(files)
-        return web.json_response(to_jsonable_python(headers, exclude_none=True))
+        return log_headers_response(files)
+
+    # /api/logs/{log}
+    async def api_log(request: web.Request) -> web.Response:
+        file = request.match_info["log"]
+        header_only = request.query.get("header-only", None) is not None
+        return log_file_response(file, header_only)
 
     # /api/events
     async def api_events(request: web.Request) -> web.Response:
@@ -46,20 +61,18 @@ def view_server(
 
     # setup app and routes
     app = web.Application()
+    app.add_routes([web.get("/api/logs/{log}", api_log)])
     app.add_routes([web.get("/api/logs", api_logs)])
     app.add_routes([web.get("/api/log-headers", api_log_headers)])
     app.add_routes([web.get("/api/events", api_events)])
     app.router.add_static("/", WWW_DIR)
 
-    # notify user
-    display().print(f"Inspect view running at http://localhost:{port}/")
-
     # run app
     web.run_app(app=app, host=host, port=port)
 
 
-def log_listing(logs: list[EvalLogInfo], log_dir: str) -> dict[str, Any]:
-    return dict(
+def log_listing_response(logs: list[EvalLogInfo], log_dir: str) -> web.Response:
+    response = dict(
         log_dir=aliased_path(log_dir),
         files=[
             dict(
@@ -72,6 +85,36 @@ def log_listing(logs: list[EvalLogInfo], log_dir: str) -> dict[str, Any]:
             for log in logs
         ],
     )
+    return web.json_response(response)
+
+
+def log_file_response(file: str, header_only: bool) -> web.Response:
+    try:
+        contents: bytes | None = None
+        if header_only:
+            try:
+                log = read_eval_log(file, header_only=True)
+                contents = eval_log_json(log).encode()
+            except ValueError as ex:
+                logger.info(
+                    f"Unable to read headers from log file {file}: {ex}. "
+                    + "The file may include a NaN or Inf value. Falling back to reading entire file."
+                )
+
+        if contents is None:  # normal read
+            log = read_eval_log(file, header_only=False)
+            contents = eval_log_json(log).encode()
+
+        return web.Response(body=contents, content_type="application/json")
+
+    except Exception as error:
+        logger.exception(error)
+        return web.Response(status=500, reason="File not found")
+
+
+def log_headers_response(files: list[str]) -> web.Response:
+    headers = read_eval_log_headers(files)
+    return web.json_response(to_jsonable_python(headers, exclude_none=True))
 
 
 def aliased_path(path: str) -> str:
