1758f5e70a606a07e8113dfad5c84628b137e41d
J.J. Allaire <jj.allaire@gmail.com>
2024-10-19 18:37:42 +0100

    async json stream parsing

--- src/inspect_ai/log/_recorders/json.py
+++ src/inspect_ai/log/_recorders/json.py
@@ -1,6 +1,7 @@
-from typing import Any, Literal, get_args
+from typing import Any, AsyncIterable, BinaryIO, Literal, get_args
 
 import ijson  # type: ignore
+from fsspec.asyn import AbstractAsyncStreamedFile, AsyncFileSystem  # type: ignore
 from ijson import IncompleteJSONError
 from pydantic import BaseModel
 from pydantic_core import from_json
@@ -10,7 +11,9 @@ from inspect_ai._util.constants import LOG_SCHEMA_VERSION
 from inspect_ai._util.error import EvalError
 from inspect_ai._util.file import (
     absolute_file_path,
+    async_fileystem,
     file,
+    filesystem,
 )
 
 from .._log import (
@@ -116,9 +119,12 @@ class JSONRecorder(FileRecorder):
     @override
     @classmethod
     async def read_log(cls, location: str, header_only: bool = False) -> EvalLog:
+        # get s3 fileystem if appropriate
+        s3_fs = async_fileystem(location) if filesystem(location).is_s3() else None
+
         if header_only:
             try:
-                return _read_header_streaming(location)
+                return await _read_header(location, s3_fs)
             # The Python JSON serializer supports NaN and Inf, however
             # this isn't technically part of the JSON spec. The json-stream
             # library shares this limitation, so if we fail with an
@@ -134,28 +140,39 @@ class JSONRecorder(FileRecorder):
                     raise ValueError(f"Unable to read log file: {location}") from ex
 
         # parse full log (also used as a fallback for header_only encountering NaN or Inf)
-        with file(location, "r") as f:
-            # parse w/ pydantic
-            raw_data = from_json(f.read())
-            log = EvalLog(**raw_data)
 
-            # fail for unknown version
-            _validate_version(log.version)
+        # read async for s3
+        if s3_fs:
+            af = await s3_fs.open_async(location)
+            try:
+                bytes = await af.read()
+            finally:
+                await af.close()
+        else:
+            with file(location, "rb") as f:
+                bytes = f.read()
 
-            # set the version to the schema version we'll be returning
-            log.version = LOG_SCHEMA_VERSION
+        # parse json
+        raw_data = from_json(bytes)
+        log = EvalLog(**raw_data)
 
-            # prune if header_only
-            if header_only:
-                # exclude samples
-                log.samples = None
+        # fail for unknown version
+        _validate_version(log.version)
 
-                # prune sample reductions
-                if log.results is not None:
-                    log.results.sample_reductions = None
+        # set the version to the schema version we'll be returning
+        log.version = LOG_SCHEMA_VERSION
 
-            # return log
-            return log
+        # prune if header_only
+        if header_only:
+            # exclude samples
+            log.samples = None
+
+            # prune sample reductions
+            if log.results is not None:
+                log.results.sample_reductions = None
+
+        # return log
+        return log
 
     @override
     @classmethod
@@ -184,55 +201,119 @@ def _validate_version(ver: int) -> None:
         raise ValueError(f"Unable to read version {ver} of log format.")
 
 
-def _read_header_streaming(log_file: str) -> EvalLog:
-    with file(log_file, "rb") as f:
-        # Do low-level parsing to get the version number and also
-        # detect the presence of results or error sections
-        version: int | None = None
-        has_results = False
-        has_error = False
+class IJsonReader:
+    def __init__(self, f: BinaryIO) -> None:
+        self.f = f
 
-        for prefix, event, value in ijson.parse(f):
-            if (prefix, event) == ("version", "number"):
-                version = value
-            elif (prefix, event) == ("results", "start_map"):
-                has_results = True
-            elif (prefix, event) == ("error", "start_map"):
-                has_error = True
-            elif prefix == "samples":
-                # Break as soon as we hit samples as that can be very large
+    async def seek(self, offset: int) -> None:
+        self.f.seek(offset)
+
+    async def parse(self) -> AsyncIterable[tuple[str, str, Any]]:
+        for prefix, event, value in ijson.parse(self.f):
+            yield prefix, event, value
+
+    async def kvitems(self) -> AsyncIterable[tuple[str, Any]]:
+        for k, v in ijson.kvitems(self.f, ""):
+            yield k, v
+
+
+class IJsonAsyncReader(IJsonReader):
+    def __init__(self, f: AbstractAsyncStreamedFile) -> None:
+        self.af = f
+
+    async def seek(self, offset: int) -> None:
+        self.af.seek(offset)
+
+    async def parse(self) -> AsyncIterable[tuple[str, str, Any]]:
+        async for prefix, event, value in ijson.parse_async(self.af):
+            yield prefix, event, value
+
+    async def kvitems(self) -> AsyncIterable[tuple[str, Any]]:
+        async for k, v in ijson.kvitems_async(self.af, ""):
+            yield k, v
+
+
+async def _read_header(log_file: str, s3_fs: AsyncFileSystem | None) -> EvalLog:
+    # read version and status
+    if s3_fs:
+        af = await s3_fs.open_async(log_file)
+        json_stream = IJsonAsyncReader(af)
+        try:
+            version, has_results, has_error = await _read_version_and_status(
+                json_stream
+            )
+        finally:
+            await af.close()
+    else:
+        with file(log_file, "rb") as f:
+            version, has_results, has_error = await _read_version_and_status(
+                IJsonReader(f)
+            )
+
+    # read the rest of the log
+    if s3_fs:
+        af = await s3_fs.open_async(log_file)
+        json_stream = IJsonAsyncReader(af)
+        try:
+            return await _read_log(json_stream, version, has_results, has_error)
+        finally:
+            await af.close()
+    else:
+        with file(log_file, "rb") as f:
+            return await _read_log(IJsonReader(f), version, has_results, has_error)
+
+
+async def _read_version_and_status(
+    json_stream: IJsonReader,
+) -> tuple[int, bool, bool]:
+    # Do low-level parsing to get the version number and also
+    # detect the presence of results or error sections
+    version: int | None = None
+    has_results = False
+    has_error = False
+
+    async for prefix, event, value in json_stream.parse():
+        if (prefix, event) == ("version", "number"):
+            version = value
+        elif (prefix, event) == ("results", "start_map"):
+            has_results = True
+        elif (prefix, event) == ("error", "start_map"):
+            has_error = True
+        elif prefix == "samples":
+            # Break as soon as we hit samples as that can be very large
+            break
+
+    if version is None:
+        raise ValueError("Unable to read version of log format.")
+
+    _validate_version(version)
+    version = LOG_SCHEMA_VERSION
+
+    return version, has_results, has_error
+
+
+async def _read_log(
+    json_stream: IJsonReader, version: int, has_results: bool, has_error: bool
+) -> EvalLog:
+    # Parse the log file, stopping before parsing samples
+    async for k, v in json_stream.kvitems():
+        if k == "status":
+            assert v in get_args(Literal["started", "success", "cancelled", "error"])
+            status: Literal["started", "success", "cancelled", "error"] = v
+        if k == "eval":
+            eval = EvalSpec(**v)
+        elif k == "plan":
+            plan = EvalPlan(**v)
+        elif k == "results":
+            results = EvalResults(**v)
+        elif k == "stats":
+            stats = EvalStats(**v)
+            if not has_error:
+                # Exit before parsing samples
                 break
-
-        if version is None:
-            raise ValueError("Unable to read version of log format.")
-
-        _validate_version(version)
-        version = LOG_SCHEMA_VERSION
-
-        # Rewind the file to the beginning to re-parse the contents of fields
-        f.seek(0)
-
-        # Parse the log file, stopping before parsing samples
-        for k, v in ijson.kvitems(f, ""):
-            if k == "status":
-                assert v in get_args(
-                    Literal["started", "success", "cancelled", "error"]
-                )
-                status: Literal["started", "success", "cancelled", "error"] = v
-            if k == "eval":
-                eval = EvalSpec(**v)
-            elif k == "plan":
-                plan = EvalPlan(**v)
-            elif k == "results":
-                results = EvalResults(**v)
-            elif k == "stats":
-                stats = EvalStats(**v)
-                if not has_error:
-                    # Exit before parsing samples
-                    break
-            elif k == "error":
-                error = EvalError(**v)
-                break
+        elif k == "error":
+            error = EvalError(**v)
+            break
 
     return EvalLog(
         eval=eval,
