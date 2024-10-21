362ce8ac7904bf26351f4dbc980371e81d23d10c
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-21 08:43:07 +0000

    add model to test

--- tests/log/test_log_tags.py
+++ tests/log/test_log_tags.py
@@ -3,5 +3,5 @@ from inspect_ai import Task, eval
 
 def test_log_tags():
     tags = ["foo,bar"]
-    log = eval(Task(), tags=tags)[0]
+    log = eval(Task(), model="mockllm/model", tags=tags)[0]
     assert log.eval.tags == tags
