6db90b3d95f8b1e7948595d2b1a7828a8f437f7a
Art.Cathain <Art.Cathain@dsit.gov.uk>
2024-10-24 09:45:15 +0000

    Informative error message when directory is not writeable

--- src/inspect_ai/_util/appdirs.py
+++ src/inspect_ai/_util/appdirs.py
@@ -5,11 +5,21 @@ from platformdirs import user_cache_path, user_runtime_path
 from inspect_ai._util.constants import PKG_NAME
 
 
+def _xdg_error(dir_type: str, dir_name: str) -> str:
+    return f"""{dir_type} directory {dir_name} is not writeable.
+        This may be because you are running Inspect without a normal login session.
+        On Linux, try setting XDG_RUNTIME_DIR to somewhere writeable.
+        See also https://github.com/UKGovernmentBEIS/inspect_ai/issues/51."""
+
+
 def inspect_runtime_dir(subdir: str | None) -> Path:
     runtime_dir = user_runtime_path(PKG_NAME)
     if subdir:
         runtime_dir = runtime_dir / subdir
-    runtime_dir.mkdir(parents=True, exist_ok=True)
+    try:
+        runtime_dir.mkdir(parents=True, exist_ok=True)
+    except PermissionError as e:
+        raise Exception(_xdg_error("Runtime", runtime_dir)) from e
     return runtime_dir
 
 
@@ -17,5 +27,9 @@ def inspect_cache_dir(subdir: str | None) -> Path:
     cache_dir = user_cache_path(PKG_NAME)
     if subdir:
         cache_dir = cache_dir / subdir
-    cache_dir.mkdir(parents=True, exist_ok=True)
+    # catch this failure, suggest setting XDG_CACHE_HOME
+    try:
+        cache_dir.mkdir(parents=True, exist_ok=True)
+    except PermissionError as e:
+        raise Exception(_xdg_error("Cache", cache_dir)) from e
     return cache_dir
