a6804e54f399daf9caca565b58744432ed3a1f1c
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-10-25 15:34:16 +0000

    use user_data_dir rather than user_runtime_dir for view notifications

--- tools/vscode/src/providers/inspect/inspect-logs-watcher.ts
+++ tools/vscode/src/providers/inspect/inspect-logs-watcher.ts
@@ -1,6 +1,6 @@
 import { Disposable, Event, EventEmitter, Uri } from "vscode";
 
-import { inspectLastEvalPath } from "../../inspect/props";
+import { inspectLastEvalPaths } from "../../inspect/props";
 import { existsSync, readFileSync, statSync } from "fs";
 import { log } from "../../core/log";
 import { WorkspaceStateManager } from "../workspace/workspace-state-provider";
@@ -20,51 +20,53 @@ export class InspectLogsWatcher implements Disposable {
     log.appendLine("Watching for evaluation logs");
     this.lastEval_ = Date.now();
 
-    const evalSignalFile = inspectLastEvalPath()?.path;
+    const evalSignalFiles = inspectLastEvalPaths().map(path => path.path);
 
     this.watchInterval_ = setInterval(() => {
-      if (evalSignalFile && existsSync(evalSignalFile)) {
-        const updated = statSync(evalSignalFile).mtime.getTime();
-        if (updated > this.lastEval_) {
-          this.lastEval_ = updated;
+      for (const evalSignalFile of evalSignalFiles) {
+        if (existsSync(evalSignalFile)) {
+          const updated = statSync(evalSignalFile).mtime.getTime();
+          if (updated > this.lastEval_) {
+            this.lastEval_ = updated;
 
-          let evalLogPath: string | undefined;
-          let workspaceId;
-          const contents = readFileSync(evalSignalFile, { encoding: "utf-8" });
+            let evalLogPath: string | undefined;
+            let workspaceId;
+            const contents = readFileSync(evalSignalFile, { encoding: "utf-8" });
 
-          // Parse the eval signal file result
-          withMinimumInspectVersion(
-            kInspectChangeEvalSignalVersion,
-            () => {
-              // 0.3.10- or later
-              const contentsObj = JSON.parse(contents) as {
-                location: string;
-                workspace_id?: string;
-              };
-              evalLogPath = contentsObj.location;
-              workspaceId = contentsObj.workspace_id;
-            },
-            () => {
-              // 0.3.8 or earlier
-              evalLogPath = contents;
+            // Parse the eval signal file result
+            withMinimumInspectVersion(
+              kInspectChangeEvalSignalVersion,
+              () => {
+                // 0.3.10- or later
+                const contentsObj = JSON.parse(contents) as {
+                  location: string;
+                  workspace_id?: string;
+                };
+                evalLogPath = contentsObj.location;
+                workspaceId = contentsObj.workspace_id;
+              },
+              () => {
+                // 0.3.8 or earlier
+                evalLogPath = contents;
+              }
+            );
+
+            if (evalLogPath !== undefined) {
+              // see if this is another instance of vscode
+              const externalWorkspace = !!workspaceId && workspaceId !== this.workspaceStateManager_.getWorkspaceInstance();
+
+              // log
+              log.appendLine(`New log: ${evalLogPath}`);
+
+              // fire event
+              try {
+                const logUri = resolveToUri(evalLogPath);
+                this.onInspectLogCreated_.fire({ log: logUri, externalWorkspace });
+              } catch (error) {
+                log.appendLine(`Unexpected error parsing URI ${evalLogPath}`);
+              }
+
             }
-          );
-
-          if (evalLogPath !== undefined) {
-            // see if this is another instance of vscode
-            const externalWorkspace = !!workspaceId && workspaceId !== this.workspaceStateManager_.getWorkspaceInstance();
-
-            // log
-            log.appendLine(`New log: ${evalLogPath}`);
-
-            // fire event
-            try {
-              const logUri = resolveToUri(evalLogPath);
-              this.onInspectLogCreated_.fire({ log: logUri, externalWorkspace });
-            } catch (error) {
-              log.appendLine(`Unexpected error parsing URI ${evalLogPath}`);
-            }
-
           }
         }
       }
