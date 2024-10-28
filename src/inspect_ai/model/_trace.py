46528ff2879f92d98d147ba4817d8acdadabfbc6
J.J. Allaire <jj.allaire@gmail.com>
2024-10-28 18:36:54 +0000

    factor out some trace rendering code

--- src/inspect_ai/model/_trace.py
+++ src/inspect_ai/model/_trace.py
@@ -1,11 +1,10 @@
 from rich.console import RenderableType
-from rich.markdown import Markdown
 from rich.text import Text
 
-from inspect_ai._util.format import format_function_call
 from inspect_ai.util._trace import trace_enabled, trace_panel
 
 from ._chat_message import ChatMessage, ChatMessageAssistant, ChatMessageTool
+from ._render import messages_preceding_assistant, render_tool_calls
 
 MESSAGE_TITLE = "Message"
 
@@ -38,13 +37,7 @@ def trace_assistant_message(
 ) -> None:
     if trace_enabled():
         # print precding messages that aren't tool or assistant
-        preceding: list[ChatMessage] = []
-        for m in reversed(input):
-            if not isinstance(m, ChatMessageTool | ChatMessageAssistant):
-                preceding.append(m)
-            else:
-                break
-        for m in reversed(preceding):
+        for m in messages_preceding_assistant(input):
             trace_panel(
                 title=m.role.capitalize(),
                 content=m.text,
@@ -55,14 +48,8 @@ def trace_assistant_message(
 
         # print tool calls
         if message.tool_calls:
-            if content:
-                content.append(Text())
-            tool_calls: list[str] = []
-            for call in message.tool_calls:
-                tool_calls.append(format_function_call(call.function, call.arguments))
-            content.append(
-                Markdown("```python\n" + "\n\n".join(tool_calls) + "\n```\n"),
-            )
+            content.append(Text())
+            content.append(render_tool_calls(message.tool_calls))
 
         # print the assistant message
         trace_panel(title="Assistant", content=content)
