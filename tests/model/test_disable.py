9c995f7e1adf2cf2cff315d881ab713a3bee1bd2
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-11-05 17:54:55 +0000

    add test checking debug_errors behavior

--- tests/model/test_disable.py
+++ tests/model/test_disable.py
@@ -1,8 +1,16 @@
+import pytest
 from test_helpers.utils import skip_if_no_openai
 
 from inspect_ai import Task, eval
 from inspect_ai._util.environ import environ_var
 from inspect_ai.model._model import get_model
+
+
+@skip_if_no_openai
+def test_disable_model_api_raise_error():
+    with environ_var("INSPECT_DISABLE_MODEL_API", "1"):
+        with pytest.raises(RuntimeError):
+            eval(Task(), model="openai/gpt-4o", debug_errors=True)
 
 
 @skip_if_no_openai
