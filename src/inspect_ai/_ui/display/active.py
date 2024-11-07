b8691df65dd17fe5a7b29615a94dbcf279a35c07
J.J. Allaire <jj.allaire@gmail.com>
2024-11-07 12:11:47 -0500

    env var for textual

--- src/inspect_ai/_ui/display/active.py
+++ src/inspect_ai/_ui/display/active.py
@@ -1,18 +1,22 @@
+import os
 import sys
 
 from inspect_ai._util.ansi import no_ansi
 
 from .display import Display
-from .display_rich import RichDisplay
 from .display_textual import TextualDisplay
+from .rich.rich import RichDisplay
 
 
 def display() -> Display:
     global _active_display
     if _active_display is None:
-        force_rich = True
         have_tty = sys.stdout.isatty()
-        if have_tty and not no_ansi() and not force_rich:
+        if (
+            have_tty
+            and not no_ansi()
+            and os.environ.get("INSPECT_TEXTUAL_UI", None) is not None
+        ):
             _active_display = TextualDisplay()
         else:
             _active_display = RichDisplay()
