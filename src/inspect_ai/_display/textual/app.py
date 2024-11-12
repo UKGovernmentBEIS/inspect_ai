a53afa70d18f2ae713a3c5e6b065e2d36ed68e4c
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-11-12 15:58:52 +0000

    underscore for private app members

--- src/inspect_ai/_display/textual/app.py
+++ src/inspect_ai/_display/textual/app.py
@@ -50,11 +50,11 @@ class TaskScreenApp(App[TR]):
         super().__init__()
 
         # initialise members
-        self.title = title
-        self.worker: Worker[TR] | None = None
-        self.error: BaseException | None = None
-        self.tasks: list[TaskWithResult] = []
-        self.output: list[str] = []
+        self._title = title
+        self._worker: Worker[TR] | None = None
+        self._error: BaseException | None = None
+        self._tasks: list[TaskWithResult] = []
+        self._output: list[str] = []
 
         # dynamically enable dark mode or light mode
         self.dark = detect_terminal_background().dark
@@ -64,7 +64,7 @@ class TaskScreenApp(App[TR]):
 
     def run_app(self, main: Coroutine[Any, Any, TR]) -> TaskScreenResult[TR]:
         # create the worker
-        self.worker = self.run_worker(main, start=False, exit_on_error=False)
+        self._worker = self.run_worker(main, start=False, exit_on_error=False)
 
         # run the app
         self.run()
@@ -72,19 +72,19 @@ class TaskScreenApp(App[TR]):
         # determine result value
         if self.return_value is not None:
             value: TR | BaseException = self.return_value
-        elif self.error is not None:
-            value = self.error
+        elif self._error is not None:
+            value = self._error
         else:
             value = RuntimeError("No application result available")
 
         # return result w/ output
-        return TaskScreenResult(value=value, tasks=self.tasks, output=self.output)
+        return TaskScreenResult(value=value, tasks=self._tasks, output=self._output)
 
     def task_screen(self, total_tasks: int, parallel: bool) -> TaskScreen:
         return TextualTaskScreen()
 
     def task_display(self, profile: TaskProfile) -> TaskDisplay:
-        return TextualTaskDisplay(profile, self.tasks)
+        return TextualTaskDisplay(profile, self._tasks)
 
     def compose(self) -> ComposeResult:
         yield TaskScreenHeader()
@@ -102,7 +102,7 @@ class TaskScreenApp(App[TR]):
         self.workers.start_all()
         self.begin_capture_print(self)
         header = self.query_one(TaskScreenHeader)
-        header.title = self.title
+        header.title = self._title
 
     def on_print(self, event: Print) -> None:
         # remove trailing newline
@@ -111,25 +111,25 @@ class TaskScreenApp(App[TR]):
             text = text[:-1]
 
         # track output (for printing at the end)
-        self.output.append(text)
+        self._output.append(text)
 
         # write to log view
         self.query_one(LogView).write_ansi(text)
 
     def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
         if event.worker.state == WorkerState.ERROR:
-            self.error = event.worker.error
+            self._error = event.worker.error
             self.exit(None, 1)
         elif event.worker.state == WorkerState.CANCELLED:
-            self.error = CancelledError()
+            self._error = CancelledError()
             self.exit(None, 1)
         elif event.worker.state == WorkerState.SUCCESS:
             self.exit(event.worker.result)
 
     @override
     async def action_quit(self) -> None:
-        if self.worker and self.worker.is_running:
-            self.worker.cancel()
+        if self._worker and self._worker.is_running:
+            self._worker.cancel()
 
     def action_toggle_dark(self) -> None:
         self.dark = not self.dark
