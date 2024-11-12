3e0d4e08bb635bad0ad2c8971a202d245771d67b
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-11-12 12:44:47 +0000

    use our own header implementation

--- src/inspect_ai/_display/textual/widgets/header.py
+++ src/inspect_ai/_display/textual/widgets/header.py
@@ -1,10 +1,112 @@
-from typing import Iterable
+from __future__ import annotations
 
+from typing import Iterator
+
+from rich.text import Text
+from textual.app import RenderResult
+from textual.dom import NoScreen
+from textual.events import Mount
+from textual.reactive import Reactive
 from textual.widget import Widget
-from textual.widgets import Header
-from textual.widgets._header import HeaderTitle
 
 
-class TaskScreenHeader(Header):
-    def compose(self) -> Iterable[Widget]:
+class HeaderTitle(Widget):
+    """Display the title / subtitle in the header."""
+
+    DEFAULT_CSS = """
+    HeaderTitle {
+        content-align: center middle;
+        width: 100%;
+    }
+    """
+
+    text: Reactive[str] = Reactive("")
+    """The main title text."""
+
+    sub_text = Reactive("")
+    """The sub-title text."""
+
+    def render(self) -> RenderResult:
+        """Render the title and sub-title.
+
+        Returns:
+            The value to render.
+        """
+        text = Text(self.text, no_wrap=True, overflow="ellipsis")
+        if self.sub_text:
+            text.append(" — ")
+            text.append(self.sub_text, "dim")
+        return text
+
+
+class TaskScreenHeader(Widget):
+    DEFAULT_CSS = """
+    TaskScreenHeader {
+        dock: top;
+        width: 100%;
+        background: $foreground 5%;
+        color: $text;
+        height: 1;
+    }
+    """
+
+    DEFAULT_CLASSES = ""
+
+    def __init__(
+        self,
+        *,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+    ):
+        """Initialise the header widget.
+
+        Args:
+            name: The name of the header widget.
+            id: The ID of the header widget in the DOM.
+            classes: The CSS classes of the header widget.
+        """
+        super().__init__(name=name, id=id, classes=classes)
+
+    def compose(self) -> Iterator[Widget]:
         yield HeaderTitle()
+
+    @property
+    def screen_title(self) -> str:
+        """The title that this header will display.
+
+        This depends on [`Screen.title`][textual.screen.Screen.title] and [`App.title`][textual.app.App.title].
+        """
+        screen_title = self.screen.title
+        title = screen_title if screen_title is not None else self.app.title
+        return title
+
+    @property
+    def screen_sub_title(self) -> str:
+        """The sub-title that this header will display.
+
+        This depends on [`Screen.sub_title`][textual.screen.Screen.sub_title] and [`App.sub_title`][textual.app.App.sub_title].
+        """
+        screen_sub_title = self.screen.sub_title
+        sub_title = (
+            screen_sub_title if screen_sub_title is not None else self.app.sub_title
+        )
+        return sub_title
+
+    def _on_mount(self, _: Mount) -> None:
+        async def set_title() -> None:
+            try:
+                self.query_one(HeaderTitle).text = self.screen_title
+            except NoScreen:
+                pass
+
+        async def set_sub_title() -> None:
+            try:
+                self.query_one(HeaderTitle).sub_text = self.screen_sub_title
+            except NoScreen:
+                pass
+
+        self.watch(self.app, "title", set_title)
+        self.watch(self.app, "sub_title", set_sub_title)
+        self.watch(self.screen, "title", set_title)
+        self.watch(self.screen, "sub_title", set_sub_title)
