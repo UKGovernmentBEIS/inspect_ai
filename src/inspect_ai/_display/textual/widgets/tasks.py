93f369ec14acd2fbd957c66b91c5edaeb013fbc5
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-11-14 13:37:39 +0000

    show time

--- src/inspect_ai/_display/textual/widgets/tasks.py
+++ src/inspect_ai/_display/textual/widgets/tasks.py
@@ -1,10 +1,12 @@
 import contextlib
+from time import monotonic
 from typing import Iterator, cast
 
 from rich.console import RenderableType
 from textual.app import ComposeResult
 from textual.containers import Container, ScrollableContainer
 from textual.reactive import reactive
+from textual.timer import Timer
 from textual.widget import Widget
 from textual.widgets import ProgressBar, Static
 from typing_extensions import override
@@ -76,8 +78,8 @@ class TaskProgressView(Widget):
         height: auto;
         width: 1fr;
         layout: grid;
-        grid-size: 4 1;
-        grid-columns: auto auto auto 1fr;
+        grid-size: 5 1;
+        grid-columns: auto auto auto 1fr auto;
         grid-gutter: 1
     }
     TaskProgressView Bar {
@@ -96,6 +98,7 @@ class TaskProgressView(Widget):
         yield Static(progress_description(self.t.profile))
         yield Static(progress_model_name(self.t.profile.model))
         yield self.progress_bar
+        yield TaskTime()
 
     @contextlib.contextmanager
     def progress(self) -> Iterator[Progress]:
@@ -104,6 +107,7 @@ class TaskProgressView(Widget):
     def complete(self, result: TaskResult) -> None:
         self.t.result = result
         self.query_one(TaskStatusIcon).result = result
+        self.query_one(TaskTime).complete()
         self.task_progress.complete()
 
 
@@ -115,6 +119,33 @@ class TaskStatusIcon(Static):
 
     def watch_result(self, new_result: TaskResult | None) -> None:
         self.update(progress_status_icon(new_result))
+
+
+class TaskTime(Static):
+    DEFAULT_CSS = """
+    TaskTime {
+        color: $accent-lighten-3;
+    }
+    """
+
+    start_time: reactive[float] = reactive(monotonic)
+    time = reactive(0.0)
+    timer: Timer | None = None
+
+    def complete(self) -> None:
+        if self.timer:
+            self.timer.stop()
+
+    def on_mount(self) -> None:
+        self.timer = self.set_interval(1, self.update_time)
+
+    def update_time(self) -> None:
+        self.time = monotonic() - self.start_time
+
+    def watch_time(self, time: float) -> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:2.0f}:{minutes:02.0f}:{seconds:02.0f}")
 
 
 class TaskProgress(Progress):
