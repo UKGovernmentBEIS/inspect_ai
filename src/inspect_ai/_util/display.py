0b826b6aee3cdeb7bfe12d5c3050a3cc18b642d8
J.J. Allaire <jj.allaire@gmail.com>
2024-11-20 06:10:46 -0800

    read default display for non shell invocation

--- src/inspect_ai/_util/display.py
+++ src/inspect_ai/_util/display.py
@@ -2,13 +2,15 @@ import os
 from logging import getLogger
 from typing import Literal
 
+from inspect_ai._util.constants import DEFAULT_DISPLAY
+
 logger = getLogger(__name__)
 
 DisplayType = Literal["full", "rich", "plain", "none"]
 
 
 def display_type() -> DisplayType:
-    display = os.environ.get("INSPECT_DISPLAY", "full").lower().strip()
+    display = os.environ.get("INSPECT_DISPLAY", DEFAULT_DISPLAY).lower().strip()
     match display:
         case "full" | "rich" | "plain" | "none":
             return display
