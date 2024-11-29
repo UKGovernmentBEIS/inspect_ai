09339b700c1d6910ea8d47bcc1a69144b0eea43d
Andrei Matveiakin <andrei@apolloresearch.ai>
2024-11-29 16:43:20 +0400

    Add prettierrc

---
+++ src/inspect_ai/_view/www/.prettierrc.js
@@ -0,0 +1,12 @@
+// Do not remove this file even if the config is empty!
+// VSCode's "Format Document" will respect this config and use the default
+// settings, which is what we want. Without prettierrc, VSCode falls back to
+// users settings, which could be different.
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {};
+
+export default config;
