cac230de08a0f88e28149ead9435ba21109b6985
aisi-inspect <166920645+aisi-inspect@users.noreply.github.com>
2024-12-04 09:46:29 +0000

    write llms-full.txt

--- docs/scripts/post-render.sh
+++ docs/scripts/post-render.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for file in _site/*.lmd; do
-    mv "$file" "${file%.lmd}.md"
+files=("index" "tutorial" "workflow" "log-viewer" "vscode" "solvers" "tools" "agents" "agents-api" "scorers" "datasets" "models" "eval-sets" "errors-and-limits" "caching" "parallelism" "interactivity" "approval" "eval-logs" "extensions")
+
+llms_full="_site/llms-full.txt"
+rm -f "${llms_full}"
+for file in "${files[@]}"; do
+    path="_site/${file}.html"
+    mv "${path}.lmd" "${path}.md"
+    cat "${path}.md" >> "${llms_full}"
+    echo "" >> "${llms_full}"
 done
+
