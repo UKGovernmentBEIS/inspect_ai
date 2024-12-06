01fd94df7e8d89a0f36616fc3444db238f0625ab
Mateusz Dziemian <mattmdjaga@gmail.com>
2024-12-06 16:26:39 +0000

    Adding tool parsing for Llama, Mistral, Qwen

--- src/inspect_ai/model/_providers/hf.py
+++ src/inspect_ai/model/_providers/hf.py
@@ -2,6 +2,7 @@ import asyncio
 import functools
 import os
 import json
+import copy
 from dataclasses import dataclass
 from queue import Empty, Queue
 from threading import Thread
@@ -32,7 +33,7 @@ from .._model_output import (
     ModelUsage,
     TopLogprob,
 )
-from .util import chat_api_input, Llama31Handler, ChatAPIHandler
+from .util import ChatAPIHandler, HFHandler
 
 HF_TOKEN = "HF_TOKEN"
 
@@ -115,7 +116,7 @@ class HuggingFaceAPI(ModelAPI):
         config: GenerateConfig,
     ) -> ModelOutput:
         # create handler
-        handler: ChatAPIHandler | None = Llama31Handler() if len(tools) > 0 else None
+        handler: ChatAPIHandler | None = HFHandler() if len(tools) > 0 else None
 
         # create chat
         chat = self.hf_chat(input, tools)
@@ -177,7 +178,9 @@ class HuggingFaceAPI(ModelAPI):
         )
 
         choice = ChatCompletionChoice(
-            message=chat_completion_assistant_message(response, tools, handler),
+            message=chat_completion_assistant_message(
+                response, tools, handler, self.model_name
+            ),
             logprobs=(
                 Logprobs(content=final_logprobs) if final_logprobs is not None else None
             ),
@@ -212,31 +215,85 @@ class HuggingFaceAPI(ModelAPI):
     def hf_chat(self, messages: list[ChatMessage], tools: list[ToolInfo]) -> str:
         # convert to hf format
         tools_list = []
+        hf_messages = copy.deepcopy(messages)
         if len(tools) > 0:
             tools_list = [
                 json.loads(tool.model_dump_json(exclude_none=True, indent=2))
                 for tool in tools
             ]
-            hf_messages = messages
-        else:
-            hf_messages = messages
+            if "mistral" in self.model_name.lower():
+                hf_messages = shorten_tool_id(hf_messages)
+                tools_list = tools_to_mistral_format(tools_list)
+            elif "qwen" in self.model_name.lower():
+                hf_messages = inspect_tools_to_string(hf_messages)
+
         # apply chat template
         chat = self.tokenizer.apply_chat_template(
             hf_messages,
             add_generation_prompt=True,
             tokenize=False,
-            chat_template=self.chat_template,
             tools=tools_list if len(tools_list) > 0 else None,
         )
         # return
         return cast(str, chat)
 
 
+def shorten_tool_id(messages: list[ChatMessage]) -> list[ChatMessage]:
+    """Shorten the tool_call_id in the messages to the last 9 characters for Mistral."""
+    for i, message in enumerate(messages):
+        if message.role == "tool":
+            # Trim tool_call_id in tool messages
+            message.tool_call_id = message.tool_call_id[-9:]
+        elif message.role == "assistant" and hasattr(message, "tool_calls"):
+            # Trim tool_call IDs inside tool_calls for assistant messages
+            for tool_call in message.tool_calls:
+                tool_call.id = tool_call.id[-9:]
+    return messages
+
+
+def tools_to_mistral_format(tools: list[dict]) -> list[dict]:
+    """Convert tools to the format required for Mistral."""
+    mistral_tools = []
+    for tool in tools:
+        mistral_tools.append(
+            {
+                "function": {
+                    "name": tool["name"],
+                    "description": tool["description"],
+                    "parameters": {
+                        "type": tool["parameters"]["type"],
+                        "properties": tool["parameters"]["properties"],
+                        "required": tool["parameters"]["required"],
+                    },
+                }
+            }
+        )
+    return mistral_tools
+
+
+def inspect_tools_to_string(messages: list[ChatMessage]) -> list[ChatMessage]:
+    """Convert tools to a string for Qwen."""
+    for message in messages:
+        if message.role == "assistant":
+            # check if the message contains a tool call
+            tool_content = ""
+            if message.tool_calls:
+                for tool_call in message.tool_calls:
+                    tool_content += f'\n```json\n{{"name": "{tool_call.function}", "arguments": {json.dumps(tool_call.arguments)}}}\n```'
+            # remove the tool call from the message
+            message.tool_calls = None
+            message.content += tool_content
+    return messages
+
+
 def chat_completion_assistant_message(
-    response: Any, tools: list[ToolInfo], handler: ChatAPIHandler | None
+    response: Any,
+    tools: list[ToolInfo],
+    handler: ChatAPIHandler | None,
+    model_name: str,
 ) -> ChatMessageAssistant:
     if handler:
-        return handler.parse_assistant_response(response.output, tools)
+        return handler.parse_assistant_response(response.output, tools, model_name)
     else:
         return ChatMessageAssistant(
             content=response.output,
