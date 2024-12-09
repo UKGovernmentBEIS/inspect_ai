1ca7aaa04a7f6ab70780511f4ccf0b863617259d
J.J. Allaire <jj.allaire@gmail.com>
2024-12-09 22:31:51 +0000

    install scripts in container

--- src/inspect_ai/solver/_human/_resources/cli/commands.py
+++ src/inspect_ai/solver/_human/_resources/cli/commands.py
@@ -1,8 +1,35 @@
+#!/usr/bin/env python3
 import argparse
+import os
+import sys
 
 
 def start():
     parser = argparse.ArgumentParser(description="Start the clock")
-    parser.parse_args()
-
+    args = parser.parse_args(sys.argv[2:])
     print("starting the clock")
+
+
+def stop():
+    print("stopping the clock")
+
+
+def note():
+    print("making a note")
+
+
+if len(sys.argv) > 0:
+    command = os.path.basename(sys.argv[1])
+    match command:
+        case "start":
+            start()
+        case "stop":
+            stop()
+        case "note":
+            note()
+        case _:
+            sys.stderr.write(f"command not recognized: {command}")
+            sys.exit(1)
+else:
+    sys.stderr.write("no command specified (usage command.py <cmd>)")
+    sys.exit(1)
--- src/inspect_ai/solver/_human/sandbox.py
+++ src/inspect_ai/solver/_human/sandbox.py
@@ -9,20 +9,20 @@ async def configure_sandbox() -> None:
 
 
 async def install_human_cli() -> None:
-    # copy cli package src
-    HUMAN_CLI_DIR = "human_cli"
-    check(await sandbox().exec(["mkdir", "-p", HUMAN_CLI_DIR]))
+    # copy cli src files and make them executable
+    INSTALL_DIR = "human_cli_install"
+    check(await sandbox().exec(["mkdir", "-p", INSTALL_DIR]))
     cli_package_dir = Path(__file__).parent / "_resources" / "cli"
     for package_file in cli_package_dir.iterdir():
         with open(package_file, "r") as f:
             contents = f.read()
-        check(
-            await write_sandbox_file(f"{HUMAN_CLI_DIR}/{package_file.name}", contents)
-        )
+        file = f"{INSTALL_DIR}/{package_file.name}"
+        check(await write_sandbox_file(file, contents))
+        check(await sandbox().exec(["chmod", "+x", file]))
 
-    # install and then remove cli package src
-    check(await sandbox().exec(["pip", "install", "."], cwd=HUMAN_CLI_DIR))
-    # check(await sandbox().exec(["rm", "-rf", HUMAN_CLI_DIR]))
+    # run install script then remove directory
+    check(await sandbox().exec(["./install.sh"], cwd=INSTALL_DIR))
+    check(await sandbox().exec(["rm", "-rf", INSTALL_DIR]))
 
 
 async def write_sandbox_file(file: str, contents: str) -> ExecResult[str]:
