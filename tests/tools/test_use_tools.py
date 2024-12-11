6006e0a388658c35a3e3377d1951e279e1d74333
Art.OCathain <Art.OCathain@dsit.gov.uk>
2024-12-11 10:22:51 +0000

    add test

--- tests/tools/test_use_tools.py
+++ tests/tools/test_use_tools.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 import pytest
-from test_helpers.tools import addition, read_file
+from test_helpers.tools import addition, list_files, read_file
 from test_helpers.utils import simple_task_state
 from typing_extensions import Unpack
 
@@ -9,9 +9,7 @@ from inspect_ai.model import CachePolicy, GenerateConfigArgs
 from inspect_ai.solver import TaskState, use_tools
 
 
-@pytest.mark.asyncio
-async def test_use_tools():
-    def generate(
+def null_generate(
         state: TaskState,
         tool_calls: Literal["loop", "single", "none"] = "loop",
         cache: bool | CachePolicy = False,
@@ -19,26 +17,47 @@ async def test_use_tools():
     ) -> TaskState:
         return state
 
+@pytest.mark.asyncio
+async def test_use_tools():
+
     state = simple_task_state()
 
     addition_tool = addition()
     read_file_tool = read_file()
 
-    state = await (use_tools([addition_tool, read_file_tool]))(state, generate)
+    state = await (use_tools([addition_tool, read_file_tool]))(state, null_generate)
     assert state.tools == [addition_tool, read_file_tool]
 
-    state = await (use_tools([addition_tool, read_file_tool]))(state, generate)
+    state = await (use_tools([addition_tool, read_file_tool]))(state, null_generate)
     assert state.tools == [addition_tool, read_file_tool]
 
-    state = await (use_tools(addition_tool, read_file_tool))(state, generate)
+    state = await (use_tools(addition_tool, read_file_tool))(state, null_generate)
     assert state.tools == [addition_tool, read_file_tool]
 
-    state = await (use_tools([addition_tool]))(state, generate)
+    state = await (use_tools([addition_tool]))(state, null_generate)
     assert state.tools == [addition_tool]
 
-    state = await (use_tools(addition_tool))(state, generate)
+    state = await (use_tools(addition_tool))(state, null_generate)
     assert state.tools == [addition_tool]
 
-    state = await (use_tools(tool_choice="auto"))(state, generate)
+    state = await (use_tools(tool_choice="auto"))(state, null_generate)
     assert state.tools == [addition_tool]
     assert state.tool_choice == "auto"
+
+@pytest.mark.asyncio
+async def test_use_tools_append():
+
+    state = simple_task_state()
+
+    addition_tool = addition()
+    read_file_tool = read_file()
+    list_files_tool = list_files()
+
+    state = await (use_tools([addition_tool, read_file_tool]))(state, null_generate)
+    assert state.tools == [addition_tool, read_file_tool]
+
+    state = await (use_tools([list_files_tool]))(state, null_generate)
+    assert state.tools == [addition_tool, read_file_tool, list_files_tool]
+
+    state = await (use_tools([addition_tool, read_file_tool]))(state, null_generate)
+    assert state.tools == [addition_tool, read_file_tool]
