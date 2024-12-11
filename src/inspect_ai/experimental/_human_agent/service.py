4da1b8db2cb5ffc04afe5a0a371de6fb88559231
jjallaire <jj.allaire@gmail.com>
2024-12-11 16:34:47 +0000

    initial agent_state work

--- src/inspect_ai/experimental/_human_agent/service.py
+++ src/inspect_ai/experimental/_human_agent/service.py
@@ -1,11 +1,35 @@
 from inspect_ai.solver._task_state import TaskState
 from inspect_ai.util._sandbox import sandbox, sandbox_service
+from inspect_ai.util._store import Store
 
 from .view import HumanAgentView
 
 
+class AgentState:
+    AGENT_STATE = "agent_state"
+
+    def __init__(self, store: Store) -> None:
+        self.store = store
+
+    @property
+    def running(self) -> bool:
+        return self.store.get(f"{self.AGENT_STATE}:running", True)
+
+    @running.setter
+    def running(self, running: bool) -> None:
+        self.store.set(f"{self.AGENT_STATE}:running", running)
+
+    @property
+    def time(self) -> float:
+        return self.store.get(f"{self.AGENT_STATE}:time", 0.0)
+
+    @time.setter
+    def time(self, time: float) -> None:
+        self.store.set(f"{self.AGENT_STATE}:time", time)
+
+
 async def run_human_agent_service(state: TaskState, view: HumanAgentView) -> None:
-    view.start_task()
+    # agent_state = AgentState(state.store)
 
     async def start() -> None:
         view.start_task()
@@ -16,9 +40,12 @@ async def run_human_agent_service(state: TaskState, view: HumanAgentView) -> Non
     async def note() -> None:
         pass
 
+    def task_is_complete() -> bool:
+        return False
+
     return await sandbox_service(
         name="human_agent",
         methods=[start, stop, note],
-        until=lambda: False,
+        until=task_is_complete,
         sandbox=sandbox(),
     )
