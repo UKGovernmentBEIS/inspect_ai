20829feed85edea0a394dffda880127ae5e7804e
Charles Teague <cteague@gmail.com>
2024-12-13 08:29:58 -0500

    Add support for rate limit detection on Bedrock (#994)
    * Add support for rate limit detection on Bedrock
    
    Add support for detecting Bedrock http rate limits. The log configuration for these errors is described here:
    
    https://boto3.amazonaws.com/v1/documentation/api/latest/guide/retries.html#validating-retry-attempts
    
    We now will inspect debug messages from botocore and respond appropriately to messages.
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: jjallaire-aisi <joseph.allaire@dsit.gov.uk>

--- src/inspect_ai/_util/logger.py
+++ src/inspect_ai/_util/logger.py
@@ -1,5 +1,6 @@
 import os
 from logging import (
+    DEBUG,
     INFO,
     WARNING,
     FileHandler,
@@ -129,7 +130,7 @@ def init_logger(
     # init logging handler on demand
     global _logHandler
     if not _logHandler:
-        _logHandler = LogHandler(min(HTTP, levelno), transcript_levelno)
+        _logHandler = LogHandler(min(DEBUG, levelno), transcript_levelno)
         getLogger().addHandler(_logHandler)
 
     # establish default capture level
@@ -139,6 +140,7 @@ def init_logger(
     getLogger().setLevel(capture_level)
     getLogger(PKG_NAME).setLevel(capture_level)
     getLogger("httpx").setLevel(capture_level)
+    getLogger("botocore").setLevel(DEBUG)
 
     # set the levelno on the global handler
     _logHandler.display_level = levelno
@@ -154,7 +156,13 @@ def notify_logger_record(record: LogRecord, write: bool) -> None:
     if write:
         transcript()._event(LoggerEvent(message=LoggingMessage.from_log_record(record)))
     global _rate_limit_count
-    if record.levelno <= INFO and "429" in record.getMessage():
+    if (record.levelno <= INFO and "429" in record.getMessage()) or (
+        record.levelno == DEBUG
+        # See https://boto3.amazonaws.com/v1/documentation/api/latest/guide/retries.html#validating-retry-attempts
+        # for boto retry logic / log messages (this is tracking standard or adapative retries)
+        and "botocore.retries.standard" in record.name
+        and "Retry needed, retrying request after delay of:" in record.getMessage()
+    ):
         _rate_limit_count = _rate_limit_count + 1
 
 
