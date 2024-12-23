76963cd99fad715b62e078d16690fb3e5f92495a
Charles Teague <cteague@gmail.com>
2024-12-23 09:30:06 -0500

    Basic trace anomoly logic

--- src/inspect_ai/_cli/trace.py
+++ src/inspect_ai/_cli/trace.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from json import dumps
 from pathlib import Path
 
@@ -5,7 +6,7 @@ import click
 from pydantic_core import to_json
 
 from inspect_ai._util.error import PrerequisiteError
-from inspect_ai._util.trace import inspect_trace_dir, read_trace_file
+from inspect_ai._util.trace import ActionTraceRecord, inspect_trace_dir, read_trace_file
 
 
 @click.group("trace")
@@ -51,7 +52,64 @@ def read_command(trace_file: str) -> None:
 @click.argument("trace-file", type=str, required=True)
 def anomolies_command(trace_file: str) -> None:
     """Look for anomolies in a trace file (never completed or cancelled actions)."""
+
     trace_file_path = resolve_trace_file_path(trace_file)
+    traces = read_trace_file(trace_file_path)
+
+    # Track started actions
+    started_actions: dict[str, ActionTraceRecord] = {}
+    error_actions: dict[str, ActionTraceRecord] = {}
+    canceled_actions: dict[str, ActionTraceRecord] = {}
+
+    def action_started(trace: ActionTraceRecord) -> None:
+        started_actions[trace.trace_id] = trace
+
+    def action_completed(trace: ActionTraceRecord) -> ActionTraceRecord:
+        start_trace = started_actions.get(trace.trace_id)
+        if start_trace:
+            del started_actions[trace.trace_id]
+            return start_trace
+        else:
+            raise RuntimeError(f"Expected {trace.trace_id} in action dictionary.")
+
+    def action_failed(trace: ActionTraceRecord) -> None:
+        error_actions[start_trace.trace_id] = trace
+
+    def action_canceled(trace: ActionTraceRecord) -> None:
+        canceled_actions[start_trace.trace_id] = trace
+
+    def print_bucket(label: str, bucket: dict[str, ActionTraceRecord]) -> None:
+        if len(bucket) > 0:
+            print(label)
+            for id, action in bucket.items():
+                start_time = (
+                    formatTime(action.start_time) if action.start_time else "None"
+                )
+                print(f"{start_time} {action.message}")
+            print("")
+
+    for trace in traces:
+        if isinstance(trace, ActionTraceRecord):
+            match trace.event:
+                case "enter":
+                    action_started(trace)
+                case "exit":
+                    action_completed(trace)
+                case "cancel":
+                    # Complete with a cancellation
+                    start_trace = action_completed(trace)
+                    action_canceled(start_trace)
+                case "error":
+                    # Capture error events
+                    start_trace = action_completed(trace)
+                    action_failed(start_trace)
+                    continue
+                case _:
+                    print(f"Unknown event type: {trace.event}")
+
+    print_bucket("Incomplete Actions", started_actions)
+    print_bucket("Error Actions", error_actions)
+    print_bucket("Canceled Actions", canceled_actions)
 
 
 def resolve_trace_file_path(trace_file: str) -> Path:
@@ -65,3 +123,9 @@ def resolve_trace_file_path(trace_file: str) -> Path:
         )
 
     return trace_file_path
+
+
+def formatTime(timestamp: float) -> str:
+    # ISO format with timezone
+    dt = datetime.fromtimestamp(timestamp)
+    return dt.isoformat()
