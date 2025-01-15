e93b3b3dc9fa4d424075f5141cc74f84f17a084c
J.J. Allaire <jj.allaire@gmail.com>
2025-01-15 12:21:31 -0500

    revise runner

--- src/inspect_ai/_eval/task/runner.py
+++ src/inspect_ai/_eval/task/runner.py
@@ -1,76 +1,42 @@
-import contextlib
-from typing import Any, AsyncIterator, Protocol, runtime_checkable
+from typing import Any, NotRequired, TypedDict
 
-from inspect_ai._util.timeouts import timeout
-from inspect_ai.model import ChatMessage, Content, ModelOutput
-from inspect_ai.scorer import Score
-from inspect_ai.solver import Generate, Solver, TaskState, solver
+from openai.types.chat import ChatCompletionMessageParam
 
-RUNNER_SCORES = "inspect_ai:runner_scores"
-
-
-@runtime_checkable
-class Runner(Protocol):
-    """Run a sample, producing either output or scores.
-
-    Runners operate at a lower level than solvers, and can
-    be used with either standard scorers (by returning output)
-    or optionally also bypass scorers by returning a dictionary
-    of scores.
-
-    Args:
-       input (str | list[ChatMessage]): The input to be evaluated.
-       metadata (dict[str,Any]): Metadata associated with the sample.
-
-    Returns:
-       Either output (to be scored) or a dictionary of scores
-       if the runner does its own scoring.
-    """
-
-    async def __call__(
-        self, input: str | list[ChatMessage], metadata: dict[str, Any]
-    ) -> str | list[Content] | dict[str, Score]: ...
+# add 'target' and 'scores' to TaskState
+# enforce token limit using exceptions
+# enforce message limit using exceptions
+# create a solver wrapper for this construct
+# filesystem: discover this the same way we discover @solver
+# sandbox case: if there is a sandbox we copy you to it
 
 
-@contextlib.asynccontextmanager
-async def runner_limits() -> AsyncIterator[None]:
-    """Async context manager for applying standard limits to task runners.
-
-    Exceptions are thrown when limits are exceeded. The following exceptions should be handled within `runner_limits()`:
-
-      - TimeoutError: Occurs if the `time_limit` is exceeded.
-
-    In the near future support for handling token limits will also be added.
-
-    Note that you only need to use this context manager in cases where
-    your runner yields scores (as opposed to output). This is required
-    because
-    """
-    # use timeout if provided
-    time_limit: int | None = 60
-    timeout_cm = (
-        timeout(time_limit) if time_limit is not None else contextlib.nullcontext()
+class Score(TypedDict):
+    value: (
+        str
+        | int
+        | float
+        | bool
+        | list[str | int | float | bool]
+        | dict[str, str | int | float | bool]
     )
+    answer: NotRequired[str]
+    explanation: NotRequired[str]
+    metadata: NotRequired[dict[str, Any]]
 
-    async with timeout_cm:
-        yield
+
+class Sample(TypedDict):
+    model: str
+    sample_id: str
+    epoch: int
+    messages: list[ChatCompletionMessageParam]
+    metadata: dict[str, Any]
+    target: list[str]
 
 
-@solver
-def runner_solver(runner: Runner) -> Solver:
-    async def solve(state: TaskState, generate: Generate) -> TaskState:
-        # run it
-        result = await runner(state.input, state.metadata)
+class Result(TypedDict):
+    output: str
+    scores: NotRequired[dict[str, Score]]
 
-        # if scores are returned then set them so we can collect
-        # them from the main loop
-        if isinstance(result, dict):
-            state.store.set(RUNNER_SCORES, result)
 
-        # otherwise just set output to whatever was returned
-        else:
-            state.output = ModelOutput.from_content(str(state.model), result)
-
-        return state
-
-    return solve
+async def solver(sample: Sample) -> Result:
+    return Result(output="yes")
