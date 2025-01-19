c40969e08ad3a90b718a7f0a3ec8afcec7111826
jjallaire <jj.allaire@gmail.com>
2025-01-19 09:54:06 -0500

    `user_message()` solver for appending parameterised user messages (#1154)

--- tests/solver/test_prompt.py
+++ tests/solver/test_prompt.py
@@ -1,6 +1,9 @@
 from inspect_ai import Task, eval
 from inspect_ai.dataset import Sample
-from inspect_ai.solver import Solver, generate, prompt_template, system_message
+from inspect_ai.solver import Solver, generate, prompt_template, solver, system_message
+from inspect_ai.solver._prompt import user_message
+from inspect_ai.solver._solver import Generate
+from inspect_ai.solver._task_state import TaskState
 
 PROMPT_TEMPLATE = """
 Please answer this question.
@@ -17,21 +20,29 @@ VARIABLE_VALUE = "variable_value"
 PROMPT_VALUE = "prompt_value"
 
 
-def check_template_variables(solver: Solver):
+def check_template_variables(template_solver: Solver, index: int = 0):
+    @solver
+    def set_store_var(name: str, value: str) -> Solver:
+        async def solve(state: TaskState, generate: Generate):
+            state.store.set(name, value)
+            return state
+
+        return solve
+
     task = Task(
         dataset=[
             Sample(
                 input="Say hello.",
                 target="Hello",
-                metadata=dict(variable=VARIABLE_VALUE, prompt=PROMPT_VALUE),
+                metadata=dict(prompt=PROMPT_VALUE),
             )
         ],
-        solver=[solver, generate()],
+        solver=[set_store_var("variable", VARIABLE_VALUE), template_solver, generate()],
     )
 
     log = eval(task, model="mockllm/model")[0]
     assert log.samples
-    message = log.samples[0].messages[0].text
+    message = log.samples[0].messages[index].text
 
     assert VARIABLE_VALUE in message
     assert PARAM_VALUE in message
@@ -48,3 +59,7 @@ def test_prompt_template_variables():
 
 def test_system_message_variables():
     check_template_variables(system_message(PROMPT_TEMPLATE, param=PARAM_VALUE))
+
+
+def test_user_message_variables():
+    check_template_variables(user_message(PROMPT_TEMPLATE, param=PARAM_VALUE), index=-2)
