65536d8fb3bc477af92015fc42cdb82c71340436
J.J. Allaire <jj.allaire@gmail.com>
2025-01-23 11:50:27 -0500

    doc comments for bridge

--- src/inspect_ai/solver/_bridge/bridge.py
+++ src/inspect_ai/solver/_bridge/bridge.py
@@ -14,6 +14,15 @@ from .._task_state import TaskState
 from .patch import openai_request_to_inspect_model
 
 
+class SampleDict(TypedDict):
+    model: str
+    sample_id: str
+    epoch: int
+    messages: list[ChatCompletionMessageParam]
+    metadata: dict[str, Any]
+    target: list[str]
+
+
 class ScoreDict(TypedDict):
     value: (
         str
@@ -28,15 +37,6 @@ class ScoreDict(TypedDict):
     metadata: NotRequired[dict[str, Any]]
 
 
-class SampleDict(TypedDict):
-    model: str
-    sample_id: str
-    epoch: int
-    messages: list[ChatCompletionMessageParam]
-    metadata: dict[str, Any]
-    target: list[str]
-
-
 class ResultDict(TypedDict):
     output: str
     messages: NotRequired[list[ChatCompletionMessageParam]]
@@ -49,12 +49,18 @@ def bridge(agent: Callable[[dict[str, Any]], Awaitable[dict[str, Any]]]) -> Solv
 
     Integrate an external agent with no Inspect dependencies by converting
     it to a Solver. The only requirements of the agent are that it use
-    the standard OpenAI API and that it return a dict with output and
-    optionally messages and scores.
+    the standard OpenAI API and that it consume and produce `dict` values
+    as described below. While the agent function calls the standard
+    OpenAI API, these calls are intercepted by Inspect and sent to the
+    requisite Inspect model provider.
 
-    The following `dict` is passed to the agent:
+    Here is the type contraxt for bridged solvers (you don't need to use
+    or import these types in your agent, your `dict` usage should
+    just conform to the protocol):
 
     ```python
+    from openai.types.chat import ChatCompletionMessageParam
+
     class SampleDict(TypedDict):
         model: str
         sample_id: str
@@ -62,8 +68,81 @@ def bridge(agent: Callable[[dict[str, Any]], Awaitable[dict[str, Any]]]) -> Solv
         messages: list[ChatCompletionMessageParam]
         metadata: dict[str, Any]
         target: list[str]
+
+    class ScoreDict(TypedDict):
+        value: (
+            str
+            | int
+            | float
+            | bool
+            | list[str | int | float | bool]
+            | dict[str, str | int | float | bool | None]
+        )
+        answer: NotRequired[str]
+        explanation: NotRequired[str]
+        metadata: NotRequired[dict[str, Any]]
+
+    class ResultDict(TypedDict):
+        output: str
+        messages: NotRequired[list[ChatCompletionMessageParam]]
+        scores: NotRequired[dict[str, ScoreDict]]
+
+    async def my_agent(sample: SampleDict) -> ResultDict: ...
     ```
 
+    The agent function must be async, and should accept and return
+    `dict` values as-per the type declarations (you aren't required
+    to use these types excactly (they merely document the requirements)
+    so long as you consume and produce `dict` values that match
+    their declarations.
+
+    Returning `messages` is not required but is highly recommended
+    so that people running the agent can see the full message history
+    in the Inspect log viewer.
+
+    Returning `scores` is entirely optional (most agents
+    will in fact rely on Inspect native scorers, this is here as an
+    escape hatch for agents that want to do their own scoring).
+
+
+    Here is the simplest possble agent definition:
+
+    ```python
+    from openai import AsyncOpenAI
+
+    async def agent(sample: dict[str, Any]) -> dict[str, Any]:
+        client = AsyncOpenAI()
+        completion = await client.chat.completions.create(
+            messages=sample["messages"],
+            model=sample["model"]
+        )
+        return {
+            "output": completion.choices[0].message.content
+        }
+    ```
+
+    Note that you should always pass the "model" along to OpenAI exactly
+    as passed in the sample. While you are calling the standard
+    OpenAI API, these calls are intercepted by Inspect and sent to the
+    requisite Inspect model provider.
+
+    Here is how you can use the `bridge()` function to use this agent
+    as a solver:
+
+    ```python
+    from inspect_ai import Task, task
+    from inspect_ai.dataset import Sample
+    from inspect_ai.scorer import includes
+    from inspect_ai.solver import bridge
+
+    @task
+    def hello():
+        return Task(
+            dataset=[Sample(input="Please print the word 'hello'?", target="hello")],
+            solver=bridge(my_agent),
+            scorer=includes(),
+        )
+    ```
     """
 
     async def solve(state: TaskState, generate: Generate) -> TaskState:
