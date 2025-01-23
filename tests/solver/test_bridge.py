148f7733bb2aba674eab637056f5839fffd04d05
J.J. Allaire <jj.allaire@gmail.com>
2025-01-23 11:29:24 -0500

    additional bridge tests

--- tests/solver/test_bridge.py
+++ tests/solver/test_bridge.py
@@ -2,13 +2,13 @@ from textwrap import dedent
 from typing import Any
 
 from openai import AsyncOpenAI
-import pytest
 from test_helpers.utils import skip_if_no_anthropic, skip_if_no_openai
 
 from inspect_ai import Task, eval, task
 from inspect_ai.dataset import Sample
 from inspect_ai.scorer import includes
-from inspect_ai.solver import bridge
+from inspect_ai.solver import bridge, solver
+from inspect_ai.solver._task_state import TaskState
 
 
 async def agent(sample: dict[str, Any]) -> dict[str, Any]:
@@ -40,6 +40,30 @@ def bridged_task():
         solver=bridge(agent),
         scorer=includes(),
     )
+
+
+@task
+def openai_api_task():
+    # solver the calls the openai api directly (so should proceed unpatched)
+    @solver
+    def openai_api_solver():
+        async def solve(state, generate):
+            client = AsyncOpenAI()
+            await client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {
+                        "role": "user",
+                        "content": "Write a haiku about recursion in programming.",
+                    },
+                ],
+            )
+            return state
+
+        return solve
+
+    return Task(solver=openai_api_solver())
 
 
 def eval_bridged_task(model: str) -> str:
@@ -75,10 +99,7 @@ def test_bridged_agent():
     check_openai_log_json(log_json)
 
 
-@skip_if_no_anthropic
-def test_anthropic_bridged_agent():
-    log_json = eval_bridged_task("anthropic/claude-3-haiku-20240307")
-
+def check_anthropic_log_json(log_json: str):
     assert r'"model": "anthropic/claude-3-haiku-20240307"' in log_json
     assert r'"temperature": 0.8' in log_json
     assert r'"top_p": 0.5' in log_json
@@ -89,6 +110,21 @@ def test_anthropic_bridged_agent():
     """)
 
 
+@skip_if_no_anthropic
+def test_anthropic_bridged_agent():
+    log_json = eval_bridged_task("anthropic/claude-3-haiku-20240307")
+    check_anthropic_log_json(log_json)
+
+
 @skip_if_no_openai
 def test_bridged_agent_context():
-    pass
+    logs = eval(
+        [bridged_task(), openai_api_task()],
+        max_tasks=2,
+        model="anthropic/claude-3-haiku-20240307",
+    )
+    for log in logs:
+        assert log.status == "success"
+        if log.eval.task == "bridged_task":
+            log_json = log.model_dump_json(exclude_none=True, indent=2)
+            check_anthropic_log_json(log_json)
