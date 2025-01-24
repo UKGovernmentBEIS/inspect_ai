c079bb3814aaf6c95d91e1034330eb052de4c4b1
J.J. Allaire <jj.allaire@gmail.com>
2025-01-23 21:19:45 -0500

    add comments to agent

--- examples/bridge/langchain/agent.py
+++ examples/bridge/langchain/agent.py
@@ -9,17 +9,26 @@ from langgraph.prebuilt import create_react_agent
 
 
 async def research_agent(sample: dict[str, Any]) -> dict[str, Any]:
-    # create the model, tools, and agent
-    model = ChatOpenAI(model_name=sample["model"])
+    # Read model (this is an Inspect model name e.g. google/gemini-1.5-pro).
+    # Use OpenAI interface (when running in the Inspect bridge this will be
+    # redirected to the appropriate Inspect model provider).
+    model_name = sample["model"]
+    model = ChatOpenAI(model_name=model_name)
+
+    # Read messages (these are in standard OpenAI format, convert to LangChain)
+    messages = convert_to_messages(sample["messages"])
+
+    # Configure web research tools/agent
     tools = [TavilySearchResults()]
     agent_executor = create_react_agent(model, tools, checkpointer=MemorySaver())
 
-    # execute the agent (convert openai messages to langchain)
+    # Execute the agent
     result = await agent_executor.ainvoke(
-        input={"messages": convert_to_messages(sample["messages"])},
+        input={"messages": messages},
         config={"configurable": {"thread_id": uuid4()}},
     )
 
-    # return result (playback messages as openai-compatible)
-    messages = convert_to_openai_messages(result["messages"])
-    return {"output": messages[-1]["content"], "messages": messages}
+    # Read and return messages and output (convert messages to OpenAI format)
+    agent_messages = convert_to_openai_messages(result["messages"])
+    agent_output = agent_messages[-1]["content"]
+    return dict(output=agent_output, messages=agent_messages)
