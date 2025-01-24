f6cae9bafc3abf56edc1f7745efa64a90382aad9
J.J. Allaire <jj.allaire@gmail.com>
2025-01-24 18:34:47 -0500

    remove standaloen code

--- examples/bridge/autogen/agent.py
+++ examples/bridge/autogen/agent.py
@@ -1,38 +1,28 @@
-import asyncio
-import json
-from pathlib import Path
 from typing import Any, cast
 
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.conditions import SourceMatchTermination
 from autogen_agentchat.messages import TextMessage
 from autogen_agentchat.teams import RoundRobinGroupChat
-from autogen_core.models import ChatCompletionClient, ModelInfo
+from autogen_core.models import ModelInfo
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 
-def web_surfer_agent(*, model: ChatCompletionClient | None = None):
+def web_surfer_agent():
     """AutoGen web surfer agent.
-
-    Can be used outside of Inspect (see __main__ handler below) or within Inspect
-    using the bridge() function (see task.py)
-
-    Args:
-       model: AutoGen model. Specify `None` (the default) when using with Inspect.
 
     Returns:
        Agent function for handling samples. May be passed to Inspect `bridge()`
        to create a standard Inspect solver.
     """
-    # Initialise model client if required for Inspect
-    if model is None:
-        model = OpenAIChatCompletionClient(
-            model="inspect",
-            model_info=ModelInfo(
-                vision=True, function_calling=True, json_output=False, family="unknown"
-            ),
-        )
+    # Use OpenAI interface (will be redirected to current Inspect model)
+    model = OpenAIChatCompletionClient(
+        model="inspect",
+        model_info=ModelInfo(
+            vision=True, function_calling=True, json_output=False, family="unknown"
+        ),
+    )
 
     # Sample handler
     async def run(sample: dict[str, Any]) -> dict[str, Any]:
@@ -58,22 +48,3 @@ def web_surfer_agent(*, model: ChatCompletionClient | None = None):
         return dict(output=message.content)
 
     return run
-
-
-# Use the agent outside of Inspect
-if __name__ == "__main__":
-
-    async def main():
-        # Read json dataset
-        dataset_path = Path(__file__).parent / "dataset.json"
-        with open(dataset_path, "r") as f:
-            dataset = json.load(f)
-
-        # Create agent and run samples in parallel
-        agent = web_surfer_agent(model=OpenAIChatCompletionClient(model="gpt-4o-mini"))
-        results = await asyncio.gather(*[agent(sample) for sample in dataset])
-
-        # Print outputs
-        print("\n\n===\n\n".join([result["output"] for result in results]))
-
-    asyncio.run(main())
--- examples/bridge/langchain/agent.py
+++ examples/bridge/langchain/agent.py
@@ -1,37 +1,31 @@
-import asyncio
-import json
-from pathlib import Path
 from typing import Any
 from uuid import uuid4
 
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import AIMessage, convert_to_messages
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 
 
-def web_research_agent(*, model: BaseChatModel | None = None, max_results: int = 5):
+def web_research_agent(*, max_results: int = 5):
     """LangChain Tavili search agent.
 
-    Can be used outside of Inspect (see __main__ handler below) or within Inspect
-    using the bridge() function (see task.py)
-
     Args:
-       model: LangChain model. Specify `None` (the default) when using with Inspect.
        max_results: Max search results to return (for use of Tavily search tool)
 
     Returns:
        Agent function for handling samples. May be passed to Inspect `bridge()`
        to create a standard Inspect solver.
     """
+    # Use OpenAI interface (will be redirected to current Inspect model)
+    model = ChatOpenAI(model="inspect")
+
     # Configure web research tools/agent
-    agent_model = ChatOpenAI(model="inspect") if model is None else model
-    agent_tools = [TavilySearchResults(max_results=max_results)]
-    agent_executor = create_react_agent(
-        model=agent_model,
-        tools=agent_tools,
+    tools = [TavilySearchResults(max_results=max_results)]
+    executor = create_react_agent(
+        model=model,
+        tools=tools,
         checkpointer=MemorySaver(),
     )
 
@@ -41,32 +35,13 @@ def web_research_agent(*, model: BaseChatModel | None = None, max_results: int =
         input = convert_to_messages(sample["input"])
 
         # Execute the agent
-        result = await agent_executor.ainvoke(
+        result = await executor.ainvoke(
             input={"messages": input},
             config={"configurable": {"thread_id": uuid4()}},
         )
 
         # Return output (content of last message)
-        ai_message: AIMessage = result["messages"][-1]
-        return dict(output=str(ai_message.content))
+        message: AIMessage = result["messages"][-1]
+        return dict(output=str(message.content))
 
     return run
-
-
-# Use the agent outside of Inspect
-if __name__ == "__main__":
-
-    async def main():
-        # Read json dataset
-        dataset_path = Path(__file__).parent / "dataset.json"
-        with open(dataset_path, "r") as f:
-            dataset = json.load(f)
-
-        # Create agent and run samples in parallel
-        agent = web_research_agent(model=ChatOpenAI(model="gpt-4o"))
-        results = await asyncio.gather(*[agent(sample) for sample in dataset])
-
-        # Print outputs
-        print("\n\n===\n\n".join([result["output"] for result in results]))
-
-    asyncio.run(main())
