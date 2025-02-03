67676a70c05b33acf0a7e3d68fc44864d1c1ecc1
J.J. Allaire <jj.allaire@gmail.com>
2025-02-03 07:03:27 -0600

    parameter rendering

--- docs/reference/filter/render.py
+++ docs/reference/filter/render.py
@@ -1,7 +1,7 @@
 
 
 import panflute as pf # type: ignore
-from parse import DocFunction, DocObject
+from parse import DocFunction, DocObject, DocParameter
 
 # render reference elements
 def render_docs(elem: pf.Element, docs: DocObject) -> list[pf.Element]:
@@ -11,7 +11,35 @@ def render_docs(elem: pf.Element, docs: DocObject) -> list[pf.Element]:
 
     # type specific rendering
     if isinstance(docs, DocFunction):
+        # declaration
         elements.append(pf.CodeBlock(docs.declaration, classes = ["python"]))
+
+        # parameters
+        elements.append(render_params(docs.parameters))
+        
     
     # return elements
     return elements
+
+
+def render_params(params: list[DocParameter]) -> pf.Table:
+    return pf.Table(
+        pf.TableBody(*[render_param(param) for param in params]),
+        head=render_params_header()      
+    )
+
+def render_params_header() -> pf.TableHead:
+    return pf.TableHead(
+        pf.TableRow(
+            pf.TableCell(pf.Plain(pf.Str("Argument"))),
+            pf.TableCell(pf.Plain(pf.Str("Type"))),
+            pf.TableCell(pf.Plain(pf.Str("Description")))
+        )
+    )
+
+def render_param(param: DocParameter) -> pf.TableRow:
+    return pf.TableRow(
+        pf.TableCell(pf.Plain(pf.Str(param.name))),
+        pf.TableCell(pf.Plain(pf.Str(param.type))),
+        pf.TableCell(pf.Plain(pf.RawInline(param.description, format="markdown")))
+    )
