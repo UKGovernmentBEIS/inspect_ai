2ee41b7ba4b74ccd9344e50d8243bbcc44ab98ad
J.J. Allaire <jj.allaire@gmail.com>
2025-02-03 06:02:01 -0600

    improve structure of filter

--- docs/reference/reference.py
+++ docs/reference/reference.py
@@ -8,41 +8,42 @@ import panflute as pf # type: ignore
 
 from parse import DocFunction, DocParseOptions, parse_docs
 
-@dataclass
-class Reference:
-    object: str
-    description: str | None = field(default=None)
-
-
 def main():
 
-    # create DocOptions
+    # create options
     module= cast(Module,griffe.load("inspect_ai"))
     sha = subprocess.run(["git", "rev-parse", "HEAD"],capture_output=True).stdout.decode().strip()
     source_url = f"https://github.com/UKGovernmentBEIS/inspect_ai/blob/{sha}/src"
-
-
     parse_options = DocParseOptions(
         module=module,
         source_url=source_url
     )
 
+    # render reference elements
+    def ref_elements(module: str, elem: pf.Element) -> list[pf.Element]:
+        # get target object
+        object = f"{module}.{pf.stringify(elem.content)}"
+
+        # render doc header
+        elements: list[pf.Element] = []
+        docs = parse_docs(object, parse_options)
+        elements.append(elem)
+        elements.append(pf.RawBlock(docs.description, "markdown"))
+
+        # type specific rendering
+        if isinstance(docs, DocFunction):
+            elements.append(pf.CodeBlock(docs.declaration, classes = ["python"]))
+        
+        # return elements
+        return elements
+
+    # convert h3 into reference
     def reference(elem: pf.Element, doc: pf.Doc):
         if isinstance(elem, pf.Header) and elem.level == 3:
             title = pf.stringify(doc.metadata["title"])
             if title.startswith("inspect_ai."):
-                # get target object
                 module = title.removeprefix("inspect_ai.")
-                object = f"{module}.{pf.stringify(elem.content)}"
-
-                # render docs
-                elements: list[pf.Element] = []
-                docs = parse_docs(object, parse_options)
-                elements.append(elem)
-                elements.append(pf.RawBlock(docs.description, "markdown"))
-                if isinstance(docs, DocFunction):
-                    elements.append(pf.CodeBlock(docs.declaration, classes = ["python"]))
-                return elements
+                return ref_elements(module, elem)
 
     return pf.run_filter(reference)
 
