4e32a9e9a6cf18566b787de289c1f04f913b50b7
J.J. Allaire <jj.allaire@gmail.com>
2025-02-04 05:48:03 -0600

    type in first column

--- docs/reference/filter/render.py
+++ docs/reference/filter/render.py
@@ -15,7 +15,6 @@ def render_docs(elem: pf.Element, docs: DocObject) -> list[pf.Element]:
         # source link
         elements.append(pf.Div(pf.Plain(pf.Link(pf.Str("Source"), url=docs.source)), classes=["source-link"]))
 
-
         # declaration
         elements.append(pf.CodeBlock(docs.declaration, classes = ["python"]))
 
@@ -30,21 +29,20 @@ def render_docs(elem: pf.Element, docs: DocObject) -> list[pf.Element]:
 def render_params(params: list[DocParameter]) -> pf.Table:
     return pf.Table(
         pf.TableBody(*[render_param(param) for param in params]),
-        head=render_params_header()      
+        head=render_params_header()      ,
+        colspec=[("AlignLeft", 0.25), ("AlignLeft", 0.75)]
     )
 
 def render_params_header() -> pf.TableHead:
     return pf.TableHead(
         pf.TableRow(
             pf.TableCell(pf.Plain(pf.Str("Argument"))),
-            pf.TableCell(pf.Plain(pf.Str("Type"))),
             pf.TableCell(pf.Plain(pf.Str("Description")))
         )
     )
 
 def render_param(param: DocParameter) -> pf.TableRow:
     return pf.TableRow(
-        pf.TableCell(pf.Plain(pf.Code(param.name))),
-        pf.TableCell(pf.Plain(pf.Str(param.type))),
+        pf.TableCell(pf.Plain(pf.Code(param.name), pf.LineBreak(), pf.Span(pf.Str(param.type), classes=["argument-type"]))),
         pf.TableCell(pf.Plain(pf.RawInline(param.description, format="markdown")))
     )
