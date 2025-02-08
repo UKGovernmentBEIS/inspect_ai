46e23238d84b0f98538dd7eff68362f71e0cd508
J.J. Allaire <jj.allaire@gmail.com>
2025-02-08 11:00:48 -0500

    add reference to llms.txt

--- docs/scripts/post-render.sh
+++ docs/scripts/post-render.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=("index" "tutorial" "options" "log-viewer" "vscode" "tasks" "datasets" "solvers" "tools" "scorers" "models" "providers" "caching" "multimodal" "reasoning" "agents" "sandboxing" "agents-api" "agent-bridge" "human-agent" "approval" "eval-logs" "eval-sets"  "errors-and-limits"  "typing" "tracing" "parallelism" "interactivity" "extensions")
+files=("index" "tutorial" "options" "log-viewer" "vscode" "tasks" "datasets" "solvers" "tools" "scorers" "models" "providers" "caching" "multimodal" "reasoning" "agents" "sandboxing" "agents-api" "agent-bridge" "human-agent" "approval" "eval-logs" "eval-sets"  "errors-and-limits"  "typing" "tracing" "parallelism" "interactivity" "extensions" "reference/inspect_ai" "reference/inspect_ai.solver" "reference/inspect_ai.tool" "reference/inspect_ai.scorer" "reference/inspect_ai.model" "reference/inspect_ai.dataset" "reference/inspect_ai.approval" "reference/inspect_ai.log" "reference/inspect_ai.util")
 
 
 if [ "$QUARTO_PROJECT_RENDER_ALL" = "1" ]; then
@@ -9,7 +9,7 @@ if [ "$QUARTO_PROJECT_RENDER_ALL" = "1" ]; then
     mv _quarto.yml _quarto.yml.bak
     for file in "${files[@]}"; do
         echo "llms: ${file}.qmd"
-        quarto render "${file}.qmd" --to gfm --quiet --no-execute
+        quarto render "${file}.qmd" --to gfm-raw_html --quiet --no-execute
         output_file="${file}.md"
         cat "${output_file}" >> "${llms_full}"
         echo "" >> "${llms_full}"
