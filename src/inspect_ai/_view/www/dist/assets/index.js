a754a3b2bf18cdc3f3416b3846e3e01d3c389de2
Charles Teague <cteague@gmail.com>
2025-02-10 08:40:33 -0500

    Remove extraneous logging

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -48845,7 +48845,6 @@ self.onmessage = function (e) {
         if (remoteZipFile.centralDirectory.has(sampleFile)) {
           return await readJSONFile(sampleFile, MAX_BYTES);
         } else {
-          console.log({ dir: remoteZipFile.centralDirectory });
           throw new Error(
             `Unable to read sample file ${sampleFile} - it is not present in the manifest.`
           );
