55bb0c036c0b7aba7eff119c61e4a39d8daa8f56
Charles Teague <cteague@gmail.com>
2025-02-14 10:00:38 -0500

    Log startup activity

--- tools/vscode/src/core/log.ts
+++ tools/vscode/src/core/log.ts
@@ -3,3 +3,11 @@
 import { window } from "vscode";
 
 export const log = window.createOutputChannel("Inspect", { log: true });
+
+export const start = (message: string) => {
+  log.info(`Start: ${message}`);
+}
+
+export const end = (message: string) => {
+  log.info(`Done:  ${message}`);
+}
