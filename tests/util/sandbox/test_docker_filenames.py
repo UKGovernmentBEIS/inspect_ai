6d8a2eefaa10ef0578357472b6acccf53a2e445e
J.J. Allaire <jj.allaire@gmail.com>
2025-02-16 22:02:26 +0000

    more tests

--- tests/util/sandbox/test_docker_filenames.py
+++ tests/util/sandbox/test_docker_filenames.py
@@ -2,16 +2,29 @@ import pytest
 from test_helpers.utils import skip_if_no_docker
 
 from inspect_ai import Task, eval, task
+from inspect_ai.util._sandbox.environment import SandboxEnvironmentSpec
 
 
 @task
-def docker_config_file(config_file: str):
-    return Task(sandbox=("docker", config_file))
+def docker_config_file(config_file: str | None):
+    return Task(sandbox=SandboxEnvironmentSpec(type="docker", config=config_file))
 
 
-def check_docker_config_file(config_file: str) -> None:
+def check_docker_config_file(config_file: str | None) -> None:
     log = eval(docker_config_file(config_file), model="mockllm/model")[0]
     assert log.status == "success"
+
+
+@pytest.mark.slow
+@skip_if_no_docker
+def test_dockerfile_none():
+    check_docker_config_file(None)
+
+
+@pytest.mark.slow
+@skip_if_no_docker
+def test_dockerfile_standard():
+    check_docker_config_file("Dockerfile")
 
 
 @pytest.mark.slow
