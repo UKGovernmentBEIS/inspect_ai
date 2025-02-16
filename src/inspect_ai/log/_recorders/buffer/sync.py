3061efb509c002c03c58b7219ee358463b2b13e0
J.J. Allaire <jj.allaire@gmail.com>
2025-02-16 12:42:54 +0000

    initial sync work

---
+++ src/inspect_ai/log/_recorders/buffer/sync.py
@@ -0,0 +1,8 @@
+from inspect_ai.log._recorders.buffer.database import SampleBufferDatabase
+from inspect_ai.log._recorders.buffer.filestore import SampleBufferFilestore
+
+
+def sync_to_filestore(
+    db: SampleBufferDatabase, filestore: SampleBufferFilestore
+) -> None:
+    pass
