2fe9d77d99015a0c5ed5489abfda3de987e89a26
J.J. Allaire <jj.allaire@gmail.com>
2025-02-16 09:59:46 +0000

    indirection on sample buffer

--- src/inspect_ai/log/_recorders/buffer/buffer.py
+++ src/inspect_ai/log/_recorders/buffer/buffer.py
@@ -1,9 +1,14 @@
 from .database import SampleBufferDatabase, cleanup_sample_buffer_databases
+from .filestore import SampleBufferFilestore
 from .types import SampleBuffer
 
 
 def sample_buffer(location: str) -> SampleBuffer:
-    return SampleBufferDatabase(location)
+    buffer = SampleBufferDatabase(location, create=False)
+    if buffer.exists():
+        return buffer
+    else:
+        return SampleBufferFilestore(location, create=False)
 
 
 def cleanup_sample_buffers(log_dir: str) -> None:
