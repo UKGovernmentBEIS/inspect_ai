e665c7d7a1e8e30aad761eddaeba9f8b775a85a0
J.J. Allaire <jj.allaire@gmail.com>
2025-02-16 09:06:26 +0000

    filestore accessors

--- src/inspect_ai/log/_recorders/buffer/filestore.py
+++ src/inspect_ai/log/_recorders/buffer/filestore.py
@@ -1,12 +1,17 @@
 import tempfile
+from logging import getLogger
+from typing import Literal
 from zipfile import ZIP_DEFLATED, ZipFile
 
 from pydantic import BaseModel
+from typing_extensions import override
 
 from inspect_ai._util.file import basename, dirname, file, filesystem
 
 from ..types import SampleSummary
-from .types import AttachmentData, EventData
+from .types import AttachmentData, EventData, SampleBuffer, SampleData, Samples
+
+logger = getLogger(__name__)
 
 
 class Segment(BaseModel):
@@ -39,7 +44,7 @@ class Manifest(BaseModel):
 MANIFEST = "manifest.json"
 
 
-class SampleBufferFilestore:
+class SampleBufferFilestore(SampleBuffer):
     def __init__(self, location: str) -> None:
         self._fs = filesystem(location)
         self._dir = f"{dirname(location)}{self._fs.sep}.buffer{self._fs.sep}{basename(location)}{self._fs.sep}"
@@ -73,9 +78,8 @@ class SampleBufferFilestore:
             self._fs.put_file(segment_file.name, f"{self._dir}{segment_name(id)}")
 
     def read_manifest(self) -> Manifest | None:
-        manifest_file = f"{self._dir}{MANIFEST}"
         try:
-            with file(manifest_file, "r") as f:
+            with file(self._manifest_file(), "r") as f:
                 contents = f.read()
                 return Manifest.model_validate_json(contents)
         except FileNotFoundError:
@@ -89,6 +93,85 @@ class SampleBufferFilestore:
             with ZipFile(f, mode="r") as zip:
                 with zip.open(segment_file_name(sample_id, epoch_id), "r") as sf:
                     return SegmentData.model_validate_json(sf.read())
+
+    def cleanup(self) -> None:
+        try:
+            self._fs.rm(self._dir, recursive=True)
+        except Exception as ex:
+            logger.warning(
+                f"Error cleaning up sample buffer database at {self._dir}: {ex}"
+            )
+
+    @override
+    def get_samples(
+        self, etag: str | None = None
+    ) -> Samples | Literal["NotModified"] | None:
+        # get the etag on the filestore
+        try:
+            info = self._fs.info(self._manifest_file())
+            fs_etag = info.etag or f"{info.mtime}{info.size}"
+        except FileNotFoundError:
+            return None
+
+        # if the etag matches then return not modified
+        if etag == fs_etag:
+            return "NotModified"
+
+        # read the manifest
+        manifest = self.read_manifest()
+        if manifest is None:
+            return None
+
+        # provide samples + etag from the manifest
+        return Samples(samples=[sm.summary for sm in manifest.samples], etag=fs_etag)
+
+    @override
+    def get_sample_data(
+        self,
+        id: str | int,
+        epoch: int,
+        after_event_id: int | None = None,
+        after_attachment_id: int | None = None,
+    ) -> SampleData | None:
+        # read the manifest
+        manifest = self.read_manifest()
+        if manifest is None:
+            return None
+
+        # find this sample in the manifest
+        sample = next(
+            (
+                sample.summary
+                for sample in manifest.samples
+                if sample.summary.id == id and sample.summary.epoch == epoch
+            ),
+            None,
+        )
+        if sample is None:
+            return None
+
+        # determine which segments we need to return in order to
+        # satisfy the after_event_id and after_attachment_id
+        after_event_id = after_event_id or -1
+        after_attachment_id = after_attachment_id or -1
+        segments = [
+            segment
+            for segment in manifest.segments
+            if segment.last_events > after_event_id
+            or segment.last_attachments > after_attachment_id
+        ]
+
+        # collect data from the segments
+        sample_data = SampleData(events=[], attachments=[])
+        for segment in segments:
+            data = self.read_segment_data(segment.id, id, epoch)
+            sample_data.events.extend(data.events)
+            sample_data.attachments.extend(data.attachments)
+
+        return sample_data
+
+    def _manifest_file(self) -> str:
+        return f"{self._dir}{MANIFEST}"
 
 
 def segment_name(id: int) -> str:
