30d0d76f8f93fcdfb23f36d27cf9cf4ece80f7bc
jjallaire <jj.allaire@gmail.com>
2025-02-17 14:23:31 +0000

    disable syncing on exception

--- src/inspect_ai/log/_recorders/buffer/database.py
+++ src/inspect_ai/log/_recorders/buffer/database.py
@@ -277,7 +277,16 @@ class SampleBufferDatabase(SampleBuffer):
         if self.log_shared is not None and self._sync_filestore is not None:
             if (time.monotonic() - self._sync_time) > self.log_shared:
                 with trace_action(logger, "Log Sync", self.location):
-                    sync_to_filestore(self, self._sync_filestore)
+                    try:
+                        sync_to_filestore(self, self._sync_filestore)
+                    except Exception as ex:
+                        logger.warning(
+                            f"Unexpected exception syncing to shared log dir: {ex}. "
+                            + "Log syncing will be disabled for this task."
+                        )
+                        self._sync_filestore.cleanup()
+                        self._sync_filestore = None
+
                 self._sync_time = time.monotonic()
 
     def _increment_version(self, conn: Connection) -> None:
