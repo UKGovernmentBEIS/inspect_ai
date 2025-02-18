2ad5e6ab5e431f00a7d85bf8e0b37f5b500aedb7
Charles Teague <cteague@gmail.com>
2025-02-18 15:06:55 -0500

    Ensure we do extraneously reset teh workspace

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -65961,8 +65961,12 @@ ${events}
         },
         [setSelectedWorkspaceTab]
       );
+      const lastSelectedIndex = reactExports.useRef(-1);
       reactExports.useEffect(() => {
         const loadSpecificLog = async () => {
+          if (lastSelectedIndex.current === selectedLogIndex) {
+            return;
+          }
           const targetLog = logs.files[selectedLogIndex];
           if (targetLog) {
             try {
@@ -65971,7 +65975,10 @@ ${events}
               if (logContents) {
                 const log2 = logContents;
                 setSelectedLogSummary(log2);
-                resetWorkspace(log2, logContents.sampleSummaries);
+                if (lastSelectedIndex.current !== -1) {
+                  resetWorkspace(log2, logContents.sampleSummaries);
+                }
+                lastSelectedIndex.current = selectedLogIndex;
                 setStatus({ loading: false, error: void 0 });
               }
             } catch (e) {
@@ -65988,13 +65995,7 @@ ${events}
           }
         };
         loadSpecificLog();
-      }, [
-        selectedLogIndex,
-        logs,
-        selectedLogIndex,
-        setSelectedLogSummary,
-        setStatus
-      ]);
+      }, [selectedLogIndex, logs, setSelectedLogSummary, setStatus]);
       const loadLogs = async () => {
         try {
           const result2 = await api2.get_log_paths();
