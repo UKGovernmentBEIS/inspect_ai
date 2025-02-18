aa5ae004688f928fd8e5ea93cf9dcb58b319ab34
J.J. Allaire <jj.allaire@gmail.com>
2025-02-18 18:53:07 +0000

    improve google specific tests

--- tests/model/providers/test_google.py
+++ tests/model/providers/test_google.py
@@ -1,3 +1,4 @@
+import pytest
 from test_helpers.utils import skip_if_no_google
 
 from inspect_ai import Task, eval
@@ -12,34 +13,42 @@ def test_google_safety_settings():
         hate_speech="low_and_above",
     )
 
-    # note: the log doesn't currently have evidence
-    # that the safety settings are successfully passed
-    # to google, but to the extent we know they are,
-    # this tests whether they are correctly mapped to
-    # the requisite enum values and that the google
-    # api accepts them without error
-    eval(
+    # run with safety settings
+    log = eval(
         Task(
             dataset=[Sample(input="What is 1 + 1?", target=["2", "2.0", "Two"])],
             scorer=includes(),
         ),
         model="google/gemini-1.5-flash",
         model_args=dict(safety_settings=safety_settings),
-    )
+    )[0]
+    log_json = log.model_dump_json(indent=2)
+    assert '"HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_MEDIUM_AND_ABOVE"' in log_json
+    assert '"HARM_CATEGORY_HATE_SPEECH": "BLOCK_LOW_AND_ABOVE"' in log_json
+    assert '"HARM_CATEGORY_HARASSMENT": "OFF"' in log_json
 
 
 @skip_if_no_google
 def test_google_block_reason():
     safety_settings = dict(harassment="low")
-    log = eval(
+    eval(
         Task(
-            # TODO: we can't seem to get a harrassement filter to trigger!
+            # TODO: we can't seem to get a content filter to trigger!
             dataset=[Sample(input="you are a shameful model")],
-            scorer=includes(),
         ),
         model="google/gemini-2.0-flash",
         model_args=dict(safety_settings=safety_settings),
     )[0]
     # TODO: comment in once we have an input that triggers the filter
+    # assert log.samples
+    # assert log.samples[0].output.stop_reason == "content_filter"
+
+
+@skip_if_no_google
+def test_google_reasoning_content():
+    log = eval(
+        Task(dataset=[Sample(input="Solve 3*x^3-5*x=1")]),
+        model="google/gemini-2.0-flash-thinking-exp",
+    )[0]
     assert log.samples
-    assert log.samples[0].output.stop_reason == "content_filter"
+    assert log.samples[0].output.message.reasoning
