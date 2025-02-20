96f5a61944282408e247f290de6faab988e44b48
Charles Teague <cteague@gmail.com>
2025-02-20 11:23:13 -0500

    Hide refresh button if streaming is supported

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -64288,7 +64288,7 @@ ${events}
             },
             "sample-tools"
           ),
-          evalStatus === "started" && /* @__PURE__ */ jsxRuntimeExports.jsx(
+          evalStatus === "started" && !capabilities2.streamSamples && /* @__PURE__ */ jsxRuntimeExports.jsx(
             ToolButton,
             {
               label: "Refresh",
