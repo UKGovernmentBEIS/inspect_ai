017699c61957d8581f11cbaaefccf9f56df22b14
Charles Teague <cteague@gmail.com>
2025-02-19 19:29:09 -0500

    Convert Sample List to Virtuoso

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -17064,7 +17064,6 @@ categories: ${categories.join(" ")}`;
       }
     };
     const sortSamples = (sort, samples, samplesDescriptor, score2) => {
-      console.log("SORT SAMPLES");
       const sortedSamples = samples.sort((a, b) => {
         const scoreDescriptor = score2 ? samplesDescriptor.evalDescriptor.scoreDescriptor(score2) : void 0;
         switch (sort) {
@@ -65871,6 +65870,7 @@ ${events}
         if (!logFile) return;
         let isActive = true;
         let pollTimeout;
+        let hadPending = false;
         const clearPendingSummaries = () => {
           if (pendingSampleSummaries.samples.length > 0) {
             setPendingSampleSummaries((prev2) => ({
@@ -65887,7 +65887,11 @@ ${events}
             if (pendingSamples.status === "OK" && pendingSamples.pendingSamples) {
               setPendingSampleSummaries(pendingSamples.pendingSamples);
               reloadSelectedLog();
+              hadPending = true;
             } else if (pendingSamples.status === "NotFound") {
+              if (hadPending) {
+                reloadSelectedLog();
+              }
               clearPendingSummaries();
               isActive = false;
             }
