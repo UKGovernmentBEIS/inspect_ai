f787185cc6fd82d46576bded4c741b29b5c6864d
Charles Teague <cteague@gmail.com>
2025-02-25 20:51:02 -0500

    If there is only one sample, select it

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -66472,6 +66472,11 @@ ${events}
       reactExports.useEffect(() => {
         refreshSelectedSample(selectedSampleIndex);
       }, [selectedSampleIndex, refreshSelectedSample]);
+      reactExports.useEffect(() => {
+        if (sampleSummaries.length === 1) {
+          setSelectedSampleIndex(0);
+        }
+      }, [sampleSummaries]);
       const loadLog = reactExports.useCallback(
         async (logFileName) => {
           try {
