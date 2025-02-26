f6481492284a7da55be729029c0f98ab12714aa2
Eric Patey <eric.patey@gmail.com>
2025-02-26 11:15:09 -0500

    Fix race condition when following navigation to new page. If obtaining the new `PageCrawler` takes too long, the `update` will be done on the old page.

--- src/inspect_ai/tool/_tools/_web_browser/_resources/playwright_crawler.py
+++ src/inspect_ai/tool/_tools/_web_browser/_resources/playwright_crawler.py
@@ -5,6 +5,8 @@ Largely based on  https://github.com/web-arena-x/webarena
 
 from __future__ import annotations
 
+from asyncio.futures import Future
+
 from playwright.async_api import BrowserContext, Page
 
 from playwright_page_crawler import PageCrawler
@@ -28,14 +30,19 @@ class PlaywrightCrawler:
         device_scale_factor: float | None,
     ):
         self._device_scale_factor = device_scale_factor
-        self._page_crawler = page_crawler
+        self._page_future = Future[PageCrawler]()
+        self._page_future.set_result(page_crawler)
         browser_context.on("page", self._on_page)
 
     @property
-    def current_page(self) -> PageCrawler:
-        return self._page_crawler
+    async def current_page(self) -> PageCrawler:
+        return await self._page_future
 
     async def _on_page(self, new_page: Page):
-        self._page_crawler = await PageCrawler.create(
-            new_page, self._device_scale_factor
+        # we know we're switching pages, but it will take time to get the new page crawler, so
+        # reset the future to force new callers to wait.
+        # TODO: A race remains in the case that we get multiple on_pages before the first one sets the result
+        self._page_future = Future()
+        self._page_future.set_result(
+            await PageCrawler.create(new_page, self._device_scale_factor)
         )
