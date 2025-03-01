01f85ddc786ec0660e424d99502938dc98cbc94a
J.J. Allaire <jj.allaire@gmail.com>
2025-03-01 17:27:55 +0000

    propagate for test

--- tests/model/test_is_rate_limit.py
+++ tests/model/test_is_rate_limit.py
@@ -1,3 +1,6 @@
+import logging
+
+from inspect_ai._util.constants import PKG_NAME
 from inspect_ai.model._chat_message import ChatMessage
 from inspect_ai.model._generate_config import GenerateConfig
 from inspect_ai.model._model import Model, ModelAPI
@@ -25,10 +28,16 @@ def test_is_rate_limit_deprecation(caplog):
         def is_rate_limit(self, ex: BaseException) -> bool:
             return True
 
-    # confirm that the deprecated api is called and triggers a warning
-    model = Model(TestAPI(), GenerateConfig())
-    assert model.should_retry(Exception()) is True
-    assert any(
-        "deprecated is_rate_limit() method" in record.message
-        for record in caplog.records
-    ), "Expected warning not found in logs"
+    # add our logging handler to the caplog handler
+    logger = logging.getLogger(PKG_NAME)
+    logger.propagate = True
+    try:
+        # confirm that the deprecated api is called and triggers a warning
+        model = Model(TestAPI(), GenerateConfig())
+        assert model.should_retry(Exception()) is True
+        assert any(
+            "deprecated is_rate_limit() method" in record.message
+            for record in caplog.records
+        ), "Expected warning not found in logs"
+    finally:
+        logger.propagate = False
