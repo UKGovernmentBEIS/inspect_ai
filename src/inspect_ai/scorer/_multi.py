d98698c8c9b0c2794ac0c9e2cb4f3d130124297a
jjallaire <jj.allaire@gmail.com>
2025-03-03 11:23:23 +0000

    tg_collect_or_raise for images and scorer

--- src/inspect_ai/scorer/_multi.py
+++ src/inspect_ai/scorer/_multi.py
@@ -1,5 +1,4 @@
-import asyncio
-
+from inspect_ai._util._async import tg_collect_or_raise
 from inspect_ai.scorer._reducer.registry import create_reducers
 from inspect_ai.solver._task_state import TaskState
 
@@ -19,7 +18,9 @@ def multi_scorer(scorers: list[Scorer], reducer: str | ScoreReducer) -> Scorer:
     reducer = create_reducers(reducer)[0]
 
     async def score(state: TaskState, target: Target) -> Score:
-        scores = await asyncio.gather(*[_scorer(state, target) for _scorer in scorers])
+        scores = await tg_collect_or_raise(
+            [_scorer(state, target) for _scorer in scorers]
+        )
         return reducer(scores)
 
     return score
