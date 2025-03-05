e531cdd0d2961360b167bbe95e8c0ece74f76091
Charles Teague <cteague@gmail.com>
2025-03-05 12:54:20 -0500

    don’t duplicate loading headers

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -18295,13 +18295,6 @@ self.onmessage = function (e) {
                 state.logsActions.setSelectedLogIndex(newIndex);
               }
             }
-            if (refreshedLogs.files.length > 0) {
-              setTimeout(() => {
-                if (!state.logs.headersLoading) {
-                  state.logsActions.loadHeaders();
-                }
-              }, 100);
-            }
           },
           // Select a specific log file
           selectLogFile: async (logUrl) => {
