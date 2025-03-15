7a0cba4add878fd3fdef2f1e7213429839134652
Eric Patey <eric.patey@gmail.com>
2025-03-15 13:39:21 -0400

    tweak

--- src/inspect_ai/tool/_tool_support_helpers.py
+++ src/inspect_ai/tool/_tool_support_helpers.py
@@ -89,10 +89,7 @@ async def exec_sandbox_rpc(
         )
 
     match _parse_json_rpc_response(exec_result.stdout, result_cls):
-        case (
-            JSONRPCError(code=-32601, message=message)
-            | JSONRPCError(code=-32602, message=message)
-        ):
+        case JSONRPCError(code=-32601 | -32602, message=message):
             raise ToolParsingError(message)
         case JSONRPCError(code=-32000, message=message):
             raise ToolError(message)
