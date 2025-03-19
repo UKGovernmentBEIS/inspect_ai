29cf6153076e7eecede084d1a9777b81b762cd82
Eric Patey <eric.patey@gmail.com>
2025-03-19 13:13:22 -0400

    Get rid of :latest

--- src/inspect_ai/util/_sandbox/docker/config.py
+++ src/inspect_ai/util/_sandbox/docker/config.py
@@ -82,7 +82,7 @@ COMPOSE_COMMENT = """# inspect auto-generated docker compose file
 COMPOSE_GENERIC_YAML = f"""{COMPOSE_COMMENT}
 services:
   default:
-    image: "aisiuk/inspect-tool-support:latest"
+    image: "aisiuk/inspect-tool-support"
     command: "tail -f /dev/null"
     init: true
     network_mode: none
