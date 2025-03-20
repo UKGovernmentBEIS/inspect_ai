0576548f2a51e5589484853507835bd1cdd14738
J.J. Allaire <jj.allaire@gmail.com>
2025-03-19 20:00:14 -0700

    currying + tests for as_solver

--- src/inspect_ai/solver/_as_solver.py
+++ src/inspect_ai/solver/_as_solver.py
@@ -1,27 +1,52 @@
-from inspect_ai._util.registry import is_registry_object, registry_info
+from typing import Any
+
+from inspect_ai._util.registry import (
+    is_registry_object,
+    registry_info,
+    registry_unqualified_name,
+)
 from inspect_ai.agent._agent import Agent
 from inspect_ai.agent._execute import agent_execute
 from inspect_ai.solver._solver import Generate, Solver, solver
 from inspect_ai.solver._task_state import TaskState
+from inspect_ai.tool._tool_info import parse_tool_info
 
 
 @solver
-def as_solver(agent: Agent) -> Solver:
+def as_solver(agent: Agent, **kwargs: Any) -> Solver:
     """Convert an agent to a solver.
 
     Note that agents used as solvers will only receive their first parameter
-    (`input`). Any other parameters must provide appropriate defaults.
+    (`input`). Any other parameters must provide appropriate defaults
+    or be explicitly specified in `kwargs`
 
     Args:
        agent: Agent to convert.
+       **kwargs: Additional arguments to
 
     Solver:
        Solver from agent.
     """
+    # agent must be registered (so we can get its name)
+    if not is_registry_object(agent):
+        raise RuntimeError(
+            "Agent passed to as_solver was not created by an @agent decorated function"
+        )
+    agent_name = registry_unqualified_name(agent)
+
+    # check to make sure we have all the parameters we need to run the agent
+    agent_info = parse_tool_info(agent)
+    for name, param in list(agent_info.parameters.properties.items())[1:]:
+        if param.default is None and name not in kwargs:
+            raise ValueError(
+                f"To use the {agent_name} agent as a solver "
+                + f"you must pass a value for the agent's required '{name}' "
+                + "parameter to the as_solver() function."
+            )
 
     async def solve(state: TaskState, generate: Generate) -> TaskState:
         # run agent
-        agent_state = await agent_execute(agent, None, state.messages)
+        agent_state = await agent_execute(agent, None, state.messages, **kwargs)
 
         # append new messages
         message_ids = [message.id for message in state.messages]
@@ -32,6 +57,7 @@ def as_solver(agent: Agent) -> Solver:
         # update output if its not empty
         if not agent_state.output.empty:
             state.output = agent_state.output
+
         return state
 
     # propagate the agent's name to the solver
--- tests/agent/test_agent_as.py
+++ tests/agent/test_agent_as.py
@@ -1,12 +1,16 @@
 import pytest
 
+from inspect_ai import eval
+from inspect_ai._eval.task.task import Task
 from inspect_ai.agent import Agent, AgentState, agent, as_tool
+from inspect_ai.solver._as_solver import as_solver
+from inspect_ai.solver._solver import Solver
 from inspect_ai.tool import ToolDef
 
 
 @agent
 def web_surfer() -> Agent:
-    async def execute(state: AgentState, max_searches: int = 3) -> AgentState:
+    async def execute(state: AgentState, max_searches: int = 5) -> AgentState:
         """Web surfer for conducting web research into a topic.
 
         Args:
@@ -16,6 +20,25 @@ def web_surfer() -> Agent:
         Returns:
             Ouput state (additions to conversation)
         """
+        state.output.completion = str(max_searches)
+        return state
+
+    return execute
+
+
+@agent
+def web_surfer_no_default() -> Agent:
+    async def execute(state: AgentState, max_searches: int) -> AgentState:
+        """Web surfer for conducting web research into a topic.
+
+        Args:
+            state: Input state (conversation)
+            max_searches: Maximum number of web searches to conduct
+
+        Returns:
+            Ouput state (additions to conversation)
+        """
+        state.output.completion = str(max_searches)
         return state
 
     return execute
@@ -65,3 +88,25 @@ def test_agent_as_tool_no_docs_error():
 def test_agent_as_tool_no_param_docs_error():
     with pytest.raises(ValueError, match="provided for parameter"):
         as_tool(web_surfer_no_param_docs())
+
+
+def check_agent_as_solver(agent_solver: Solver):
+    log = eval(Task(solver=agent_solver))[0]
+    assert log.samples
+    assert log.samples[0].output.completion == "5"
+
+
+def test_agent_as_solver():
+    agent_solver = as_solver(web_surfer())
+    check_agent_as_solver(agent_solver)
+
+
+def test_agent_as_solver_with_param():
+    agent_solver = as_solver(web_surfer_no_default(), max_searches=5)
+    check_agent_as_solver(agent_solver)
+
+
+def test_agent_as_solver_no_param():
+    with pytest.raises(ValueError, match="as a solver"):
+        agent_solver = as_solver(web_surfer_no_default())
+        eval(Task(solver=agent_solver))[0]
