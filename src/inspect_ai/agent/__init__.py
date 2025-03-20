8537e0090d9e26fbcea61bbe0bdee25cd1f9e611
J.J. Allaire <jj.allaire@gmail.com>
2025-03-19 20:45:42 -0700

    rename to as_tool

--- src/inspect_ai/agent/__init__.py
+++ src/inspect_ai/agent/__init__.py
@@ -1,6 +1,6 @@
 from ._agent import Agent, AgentState, agent
+from ._as_tool import as_tool
 from ._bridge.bridge import bridge
-from ._convert import as_tool
 from ._handoff import HandoffFilter, handoff
 from ._human.agent import human
 from ._react import react
