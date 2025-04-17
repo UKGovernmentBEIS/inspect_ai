74cb15cb82a7ce78f22605ba70aec0ea3d2927dd
Thomas Broadley <thomas@metr.org>
2025-04-16 17:05:36 -0700

    Hide VS Code window button and label if not in VS Code

--- src/inspect_ai/agent/_human/panel.py
+++ src/inspect_ai/agent/_human/panel.py
@@ -154,9 +154,11 @@ class HumanAgentPanel(InputPanel):
             window_lbl = cast(
                 Label, self.query_one(f"#{self.LOGIN_VSCODE_WINDOW_LABEL_ID}")
             )
-            display = "block" if connection.vscode_command is not None else "none"
-            window_btn.display = display
-            window_lbl.display = display
+            window_btn_and_label_display = (
+                "block" if vscode and connection.vscode_command is not None else "none"
+            )
+            window_btn.display = window_btn_and_label_display
+            window_lbl.display = window_btn_and_label_display
             if connection.vscode_command is not None:
                 window_btn.commands = [
                     VSCodeCommand(
