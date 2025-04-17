dda60c40339bc537de2c998ee1e3340a82ea51aa
Thomas Broadley <thomas@metr.org>
2025-04-16 17:00:35 -0700

    Default param to none

--- examples/human/human.py
+++ examples/human/human.py
@@ -5,7 +5,7 @@ from inspect_ai.agent._human.agent import human_cli
 
 
 @task
-def human(user: Literal["root", "nonroot"] = "nonroot") -> Task:
+def human(user: Literal["root", "nonroot"] | None = None) -> Task:
     return Task(
         solver=human_cli(user=user),
         sandbox=("docker", "compose.yaml"),
