a51cc23d4c9a6083ae5ae295ddaa79bb877df24a
dragonstyle <cteague@gmail.com>
2025-04-24 10:49:41 -0400

    takeover vscode storage serialization
    It uses JSON serialization internally which can’t deal with Nan, so take over serialization and use JSON5 as we do everywhere else.
    
    Fixes #1747

--- src/inspect_ai/_view/www/src/client/storage/index.ts
+++ src/inspect_ai/_view/www/src/client/storage/index.ts
@@ -1,3 +1,4 @@
+import JSON5 from "json5";
 import { PersistedState } from "../../state/store";
 import { getVscodeApi } from "../../utils/vscode";
 import { ClientStorage } from "../api/types";
@@ -7,13 +8,18 @@ const resolveStorage = (): ClientStorage | undefined => {
   if (vscodeApi) {
     return {
       getItem: (_name: string) => {
-        const state = vscodeApi.getState() as PersistedState;
-        return state;
+        const state = vscodeApi.getState() as string;
+        const deserialized = JSON5.parse(state) as {
+          state: PersistedState;
+          version: number;
+        };
+        return deserialized;
       },
       setItem: (_name: string, value: unknown) => {
         // TODO: This is pretty gnarly type hijinks
         const valObj = value as { state: PersistedState; version: number };
-        vscodeApi.setState(valObj);
+        const serialized = JSON5.stringify(valObj);
+        vscodeApi.setState(serialized);
       },
       removeItem: (_name: string) => {
         vscodeApi.setState(null);
