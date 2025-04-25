5fdd9a8d4db5f5d6100c0c7e53f5ed61ef0b1c2b
Andrew Fasano <andrew.fasano@nist.gov>
2025-04-25 20:47:38 +0000

    Tests: test read_file with symlinks

--- tests/util/sandbox/test_sandbox_setup.py
+++ tests/util/sandbox/test_sandbox_setup.py
@@ -13,19 +13,37 @@ from inspect_ai.util._sandbox.docker.config import is_dockerfile
 
 SANDBOX_SETUP_FILE = (Path(__file__).parent / "sandbox_setup.sh").as_posix()
 SANDBOX_SETUP_ERROR_FILE = (Path(__file__).parent / "sandbox_setup_error.sh").as_posix()
+SANDBOX_SETUP_SYMLINK_FILE = (
+    Path(__file__).parent / "sandbox_setup_symlink.sh"
+).as_posix()
 
 with open(SANDBOX_SETUP_FILE, "r") as f:
     sandbox_setup = f.read()
 
+SUCCESS = "FOUND"
+NOT_FOUND = "MISSING"
+
 
 @solver
-def check_foo() -> Solver:
+def check_file(expected_content: str = None) -> Solver:
     async def solve(state: TaskState, generate: Generate) -> TaskState:
+        """
+        Check if a file exists, and if it contains the expected_content.
+
+        Returns SUCCESS if the file exists with expected content. Otherwise
+        returns NOT_FOUND if file is missing or the mismatched set of strings
+        if expected_content doesn't match actual content.
+        """
         try:
-            await sandbox().read_file(state.metadata["file"])
-            completion = "Yes"
+            value = await sandbox().read_file(state.metadata["file"])
+            # Strip before comparing values to avoid trailing newline mismatches
+            if not expected_content or expected_content.strip() == value.strip():
+                completion = SUCCESS
+            else:
+                # Unexpected contents,
+                completion = f"{repr(value)} != {repr(expected_content)}"
         except FileNotFoundError:
-            completion = "No"
+            completion = NOT_FOUND
 
         state.output = ModelOutput.from_content("mockllm/model", completion)
 
@@ -39,22 +57,22 @@ def check_foo() -> Solver:
 def test_docker_sandbox_setup():
     def sample(file: str, target: str, setup: str) -> Sample:
         return Sample(
-            input=f"Does the file '{file}' exist? Answer Yes or No",
+            input=f"Does the file '{file}' exist? Answer {SUCCESS} or {NOT_FOUND}",
             target=target,
             metadata={"file": file},
             setup=setup,
         )
 
     dataset = [
-        sample("foo.txt", "Yes", sandbox_setup),
-        sample("bar.txt", "No", sandbox_setup),
-        sample("foo.txt", "Yes", SANDBOX_SETUP_FILE),
-        sample("bar.txt", "No", SANDBOX_SETUP_FILE),
+        sample("foo.txt", SUCCESS, sandbox_setup),
+        sample("bar.txt", NOT_FOUND, sandbox_setup),
+        sample("foo.txt", SUCCESS, SANDBOX_SETUP_FILE),
+        sample("bar.txt", NOT_FOUND, SANDBOX_SETUP_FILE),
     ]
 
     task = Task(
         dataset=dataset,
-        solver=check_foo(),
+        solver=check_file(),
         scorer=includes(),
         sandbox="docker",
     )
@@ -64,6 +82,46 @@ def test_docker_sandbox_setup():
     assert log.samples
     for sample in log.samples:
         assert sample.scores["includes"].value == CORRECT
+
+
+@skip_if_no_docker
+@pytest.mark.slow
+def test_docker_sandbox_setup_symlink():
+    def sample(file: str, target: str, setup: str) -> Sample:
+        return Sample(
+            input=f"Does the file '{file}' exist? Answer {SUCCESS} or {NOT_FOUND}",
+            target=target,
+            metadata={"file": file},
+            setup=setup,
+        )
+
+    dataset = [
+        sample("link_simple", SUCCESS, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("link_dot_slash", SUCCESS, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("nested/link_up_one", SUCCESS, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("nested/inner/link_up_two", SUCCESS, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("link_absolute", SUCCESS, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("missing_simple", NOT_FOUND, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("missing_dot_slash", NOT_FOUND, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("nested/missing_up_one", NOT_FOUND, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("nested/inner/missing_up_two", NOT_FOUND, SANDBOX_SETUP_SYMLINK_FILE),
+        sample("missing_absolute", NOT_FOUND, SANDBOX_SETUP_SYMLINK_FILE),
+    ]
+
+    task = Task(
+        dataset=dataset,
+        solver=check_file(expected_content="hello world"),
+        scorer=includes(),
+        sandbox="docker",
+    )
+
+    log = eval(task, model="mockllm/model")[0]
+
+    assert log.samples
+    for sample in log.samples:
+        assert sample.scores["includes"].value == CORRECT, (
+            f"Failure for '{sample.metadata['file']}': {sample.scores['includes'].answer}'"
+        )
 
 
 @skip_if_no_docker
