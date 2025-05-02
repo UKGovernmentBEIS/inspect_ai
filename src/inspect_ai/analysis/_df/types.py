1ad96894a91804bccfeee5fd1421dd2171098055
J.J. Allaire <jj.allaire@gmail.com>
2025-05-02 11:26:24 -0400

    precompile paths

--- src/inspect_ai/analysis/_df/types.py
+++ src/inspect_ai/analysis/_df/types.py
@@ -1,8 +1,9 @@
-from dataclasses import KW_ONLY, dataclass, field
+from dataclasses import KW_ONLY, dataclass
 from datetime import date, datetime, time
 from typing import Type, TypeAlias
 
 from jsonpath_ng import JSONPath  # type: ignore
+from jsonpath_ng.ext import parse  # type: ignore
 
 ColumnType: TypeAlias = int | float | bool | str | date | time | datetime | None
 """Valid types for columns.
@@ -11,20 +12,40 @@ Values of `list` and `dict` are read by converting them to JSON `str`.
 """
 
 
-@dataclass
 class Column:
-    path: str | JSONPath
-    """Extract column value using this path.
-
-    Column specifications are expressed as [JSONPath](https://github.com/h2non/jsonpath-ng) expressions.
     """
-    _: KW_ONLY
+    Specification for importing a column into a data frame.
 
-    required: bool = field(default=False)
-    """Is the field required? (error is raised if required fields aren't found)."""
+    Paths to columns within the `EvalLog` are expressed as [JSONPath](https://github.com/h2non/jsonpath-ng) expressions.
+    """
 
-    type: Type[ColumnType] | None = field(default=None)
-    """Field type (import will attempt to coerce to the specified type)."""
+    def __init__(
+        self,
+        path: str | JSONPath,
+        *,
+        required: bool = False,
+        type: Type[ColumnType] | None = None,
+    ) -> None:
+        self._path = path
+        self._required = required
+        self._type = type
+
+    @property
+    def path(self) -> JSONPath:
+        """Path to column in `EvalLog`"""
+        if isinstance(self._path, str):
+            self._path = parse(self._path)
+        return self._path
+
+    @property
+    def required(self) -> bool:
+        """Is the column required? (error is raised if required columns aren't found)."""
+        return self._required
+
+    @property
+    def type(self) -> Type[ColumnType] | None:
+        """Column type (import will attempt to coerce to the specified type)."""
+        return self._type
 
 
 Columns: TypeAlias = dict[str, Column]
