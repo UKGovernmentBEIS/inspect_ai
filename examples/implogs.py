8391ce02075629dcd1d7017f583f67c50e62f0ff
J.J. Allaire <jj.allaire@gmail.com>
2025-05-02 16:17:16 -0400

    update impllogs

--- examples/implogs.py
+++ examples/implogs.py
@@ -1,9 +1,14 @@
 from inspect_ai.analysis import EvalDefault, evals_df
+from inspect_ai.analysis._df.eval import EvalScores
 from inspect_ai.analysis._df.types import Column, Columns
 
 ErrorSpecs: Columns = {"run_id": Column("eval.runid", required=True)}
 
-df = evals_df("logs", columns=EvalDefault)
+df = evals_df(
+    "logs",
+    columns=EvalDefault | EvalScores,
+)
+print(df.columns)
 df = df[
     [
         "eval_id",
@@ -11,6 +16,8 @@ df = df[
         "score_headline_name",
         "score_headline_metric",
         "score_headline_value",
+        "score_match_accuracy",
+        "score_match_stderr",
     ]
 ]
 print(df)
