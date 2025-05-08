aaf1a89c2336f28e1d741768fcc2cca89b45f092
J.J. Allaire <jj.allaire@gmail.com>
2025-05-08 14:32:29 -0400

    data frame => dataframe

--- src/inspect_ai/analysis/beta/_dataframe/evals/table.py
+++ src/inspect_ai/analysis/beta/_dataframe/evals/table.py
@@ -60,7 +60,7 @@ def evals_df(
        logs: One or more paths to log files or log directories.
        columns: Specification for what columns to read from log files.
        recursive: Include recursive contents of directories (defaults to `True`)
-       reverse: Reverse the order of the data frame (by default, items
+       reverse: Reverse the order of the dataframe (by default, items
           are ordered from oldest to newest).
        strict: Raise import errors immediately. Defaults to `True`.
           If `False` then a tuple of `DataFrame` and errors is returned.
