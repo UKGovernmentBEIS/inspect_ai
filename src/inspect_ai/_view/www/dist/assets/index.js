ac0646cb09bb38548c85f539d1314774b76ca5c0
dragonstyle <cteague@gmail.com>
2025-05-13 08:54:02 -0400

    Properly sort errors in scores

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -15851,7 +15851,7 @@ var require_assets = __commonJS({
       return __toString.call(val) === "[object Date]";
     }
     /**
-     * react-router v7.5.1
+     * react-router v7.5.3
      *
      * Copyright (c) Remix Software Inc.
      *
@@ -17249,7 +17249,11 @@ var require_assets = __commonJS({
           }
           return {
             matches,
-            pendingActionResult: [boundaryMatch.route.id, result2]
+            pendingActionResult: [
+              boundaryMatch.route.id,
+              result2,
+              actionMatch.route.id
+            ]
           };
         }
         return {
@@ -19418,7 +19422,9 @@ var require_assets = __commonJS({
       });
       if (pendingError !== void 0 && pendingActionResult) {
         errors2 = { [pendingActionResult[0]]: pendingError };
-        loaderData[pendingActionResult[0]] = void 0;
+        if (pendingActionResult[2]) {
+          loaderData[pendingActionResult[2]] = void 0;
+        }
       }
       return {
         loaderData,
@@ -21109,7 +21115,7 @@ Please change the parent <Route path="${parentPath}"> to <Route path="${parentPa
     var isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined" && typeof window.document.createElement !== "undefined";
     try {
       if (isBrowser) {
-        window.__reactRouterVersion = "7.5.1";
+        window.__reactRouterVersion = "7.5.3";
       }
     } catch (e) {
     }
@@ -21510,7 +21516,7 @@ Please change the parent <Route path="${parentPath}"> to <Route path="${parentPa
       304
     ]);
     /**
-     * react-router v7.5.1
+     * react-router v7.5.3
      *
      * Copyright (c) Remix Software Inc.
      *
@@ -24240,6 +24246,10 @@ Please change the parent <Route path="${parentPath}"> to <Route path="${parentPa
         compare: (a, b) => {
           if (typeof a.value === "number" && typeof b.value === "number") {
             return compareWithNan(a.value, b.value);
+          } else if (typeof a.value === "number" && typeof b.value !== "number") {
+            return -1;
+          } else if (typeof a.value !== "number" && typeof b.value === "number") {
+            return 1;
           } else {
             console.warn("Comparing non-numerics using a numeric score descriptor");
             return 0;
@@ -39151,13 +39161,16 @@ Please change the parent <Route path="${parentPath}"> to <Route path="${parentPa
           }
         },
         compare: (a, b) => {
-          if (typeof a.value !== "string" || typeof b.value !== "string") {
-            throw new Error(
-              "Unexpectedly using the pass fail scorer on non-string values"
-            );
+          if (typeof a.value !== "string" && typeof b.value !== "string") {
+            return 0;
+          } else if (typeof a.value === "string" && typeof b.value !== "string") {
+            return -1;
+          } else if (typeof a.value !== "string" && typeof b.value === "string") {
+            return 1;
+          } else {
+            const sort = order.indexOf(String(a.value || "")) - order.indexOf(String(b.value || ""));
+            return sort;
           }
-          const sort = order.indexOf(a.value || "") - order.indexOf(b.value || "");
-          return sort;
         }
       };
     };
