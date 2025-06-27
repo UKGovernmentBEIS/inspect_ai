cfb639b683b444c5e25dee8332cac7f9a670fd21
Eric Patey <eric.patey@gmail.com>
2025-06-27 14:13:43 -0400

    whoops...tweak

--- src/inspect_ai/_cli/eval.py
+++ src/inspect_ai/_cli/eval.py
@@ -886,8 +886,8 @@ def eval_exec(
     solver_args = parse_cli_config(s, solver_config)
     model_args = parse_cli_config(m, model_config)
     # TODO: Is this the right place to resolve the batch config?
-    if isinstance(kwargs.get("batch", None), str):
-        kwargs["batch"] = BatchConfig.model_validate(resolve_args(kwargs["batch"]))
+    if isinstance((batch := kwargs.get("batch", None)), str):
+        kwargs["batch"] = BatchConfig.model_validate(resolve_args(batch))
 
     # parse model roles
     eval_model_roles = parse_cli_args(model_role, force_str=True)
