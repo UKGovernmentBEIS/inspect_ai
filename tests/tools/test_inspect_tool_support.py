370a78aa20652c71e241edd3662afe01c0e06faf
Andrew Fasano <andrew.fasano@nist.gov>
2025-07-03 17:16:02 +0000

    Add test for text_editor username support

--- tests/tools/test_inspect_tool_support.py
+++ tests/tools/test_inspect_tool_support.py
@@ -226,3 +226,56 @@ def test_bash_session_missing_user(inspect_tool_support_sandbox):
     # Note that the sandbox exec helper doesn't log anything about the user being
     # the cause of this error, so there's unfortunately nothing more precise for us to check
     assert log.status == "error"
+
+
+@pytest.mark.slow
+def test_text_editor_user(inspect_tool_support_sandbox):
+    task = Task(
+        dataset=[
+            Sample(
+                input="Create a file /flag only readable by root. Then read it with the text editor",
+            )
+        ],
+        solver=[
+            use_tools([bash_session(user="root"), text_editor(user="nobody")]),
+            generate(),
+        ],
+        scorer=match(),
+        sandbox=inspect_tool_support_sandbox,
+    )
+    flag = "this_is_it"
+    model = get_model(
+        "mockllm/model",
+        custom_outputs=[
+            ModelOutput.for_tool_call(
+                model="mockllm/model",
+                tool_name="bash_session",
+                tool_arguments={
+                    "action": "type_submit",
+                    "input": f"echo {flag} > /flag && chmod 400 /flag; ls -al /flag && cat /flag",
+                },
+            ),
+            ModelOutput.for_tool_call(
+                model="mockllm/model",
+                tool_name="text_editor",
+                tool_arguments={"command": "view", "path": "/flag"},
+            ),
+            ModelOutput.from_content(model="mockllm/model", content="All done."),
+        ],
+    )
+    log = eval(task, model=model)[0]
+
+    assert log.status == "success"
+    assert log.samples
+    messages = log.samples[0].messages
+
+    bash_tool_call = get_tool_call(messages, "bash_session")
+    bash_response = get_tool_response(messages, bash_tool_call)
+    editor_tool_call = get_tool_call(messages, "text_editor")
+    editor_response = get_tool_response(messages, editor_tool_call)
+
+    assert "-r--------" in bash_response.content  # Expect read only flag
+    assert "root root" in bash_response.content  # Expect flag owned by root
+
+    assert editor_response
+    assert flag not in editor_response.content
