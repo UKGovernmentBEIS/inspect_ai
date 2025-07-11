a5be9ebd58498a827aae1d3e436bee42ed767af3
Eric Patey <eric.patey@gmail.com>
2025-07-11 10:04:22 -0400

    remove test code

--- examples/biology_qa.py
+++ examples/biology_qa.py
@@ -1,6 +1,5 @@
 from inspect_ai import Task, task
 from inspect_ai.dataset import FieldSpec, example_dataset
-from inspect_ai.model._generate_config import BatchConfig, GenerateConfig
 from inspect_ai.scorer import model_graded_qa
 from inspect_ai.solver import generate, use_tools
 from inspect_ai.tool import web_search
@@ -37,7 +36,5 @@ def biology_qa() -> Task:
             ),
             generate(),
         ],
-        # TODO: TESTING ONLY. DON'T MERGE
-        config=GenerateConfig(batch=BatchConfig(max_size=7)),
         scorer=model_graded_qa(),
     )
