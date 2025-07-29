754b9dc6b2f00f5fccf07260c807862b1e6ba4ef
Sami Jawhar <sami@metr.org>
2025-07-29 13:16:29 -0700

    Together Batch API (#2102)

--- src/inspect_ai/model/_providers/together.py
+++ src/inspect_ai/model/_providers/together.py
@@ -1,6 +1,6 @@
 import os
 from json import dumps
-from typing import Any
+from typing import Any, cast
 
 import httpx
 from openai import APIStatusError
@@ -10,12 +10,13 @@ from openai.types.chat import (
 from typing_extensions import override
 
 from inspect_ai._util.constants import DEFAULT_MAX_TOKENS
+from inspect_ai.model._retry import model_retry_config
 from inspect_ai.tool._tool_choice import ToolChoice
 from inspect_ai.tool._tool_info import ToolInfo
 
 from .._chat_message import ChatMessage, ChatMessageAssistant
-from .._generate_config import GenerateConfig
-from .._model import ModelAPI
+from .._generate_config import GenerateConfig, normalized_batch_config
+from .._model import ModelAPI, log_model_retry
 from .._model_output import (
     ChatCompletionChoice,
     Logprob,
@@ -26,6 +27,7 @@ from .._model_output import (
     as_stop_reason,
 )
 from .._openai import chat_message_assistant_from_openai
+from ._together_batch import TogetherBatcher
 from .openai_compatible import OpenAICompatibleAPI
 from .util import (
     chat_api_input,
@@ -91,6 +93,7 @@ class TogetherAIAPI(OpenAICompatibleAPI):
             service="Together",
             service_base_url="https://api.together.xyz/v1",
         )
+        self._batcher: TogetherBatcher | None = None
 
     # Together uses a default of 512 so we bump it up
     @override
@@ -131,6 +134,32 @@ class TogetherAIAPI(OpenAICompatibleAPI):
         self, completion: ChatCompletion, tools: list[ToolInfo]
     ) -> list[ChatCompletionChoice]:
         return chat_choices_from_response_together(completion, tools)
+
+    @override
+    async def _generate_completion(
+        self, request: dict[str, Any], config: GenerateConfig
+    ) -> ChatCompletion:
+        batch_config = normalized_batch_config(config.batch)
+        if not batch_config:
+            return cast(
+                ChatCompletion, await self.client.chat.completions.create(**request)
+            )
+
+        if not self._batcher:
+            self._batcher = TogetherBatcher(
+                self.client,
+                batch_config,
+                # TODO: In the future, we could pass max_retries and timeout
+                # from batch_config falling back to config
+                model_retry_config(
+                    self.model_name,
+                    config.max_retries,
+                    config.timeout,
+                    self.should_retry,
+                    log_model_retry,
+                ),
+            )
+        return await self._batcher.generate(request, config)
 
 
 # Implementation of REST client for Together (currently not used)
