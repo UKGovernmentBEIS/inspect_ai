0c6d453c2814993fcff51b196a3139846b54817c
Daniel Polatajko <danielpolatajko@gmail.com>
2025-09-03 10:07:58 +0100

    Update _openai_web_search.py

--- src/inspect_ai/model/_providers/_openai_web_search.py
+++ src/inspect_ai/model/_providers/_openai_web_search.py
@@ -1,15 +1,13 @@
 from typing import cast
 
-from openai.types.responses import WebSearchPreviewTool, WebSearchPreviewToolParam
+from openai.types.responses import WebSearchTool, WebSearchToolParam
 
 from inspect_ai.tool._tool_info import ToolInfo
 
 COMPATIBLE_MODELS = ["gpt-4o", "gpt-4o-mini", "gpt-4.1", "o3", "o4-mini", "gpt-5"]
 
 
-def maybe_web_search_tool(
-    model_name: str, tool: ToolInfo
-) -> WebSearchPreviewToolParam | None:
+def maybe_web_search_tool(model_name: str, tool: ToolInfo) -> WebSearchToolParam | None:
     return (
         _web_search_tool(tool.options["openai"])
         if (
@@ -22,7 +20,7 @@ def maybe_web_search_tool(
     )
 
 
-def _web_search_tool(maybe_openai_options: object) -> WebSearchPreviewToolParam:
+def _web_search_tool(maybe_openai_options: object) -> WebSearchToolParam:
     if maybe_openai_options is None:
         maybe_openai_options = {}
     elif not isinstance(maybe_openai_options, dict):
@@ -35,4 +33,4 @@ def _web_search_tool(maybe_openai_options: object) -> WebSearchPreviewToolParam:
         else WebSearchTool(type="web_search")
     )
 
-    return cast(WebSearchPreviewToolParam, openai_options.model_dump(exclude_none=True))
+    return cast(WebSearchToolParam, openai_options.model_dump(exclude_none=True))
