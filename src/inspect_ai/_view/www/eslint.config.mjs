938f562599496652ae7c6f69b2b240a8b40f87bb
dragonstyle <cteague@gmail.com>
2025-09-09 10:54:31 -0400

    linting

--- src/inspect_ai/_view/www/eslint.config.mjs
+++ src/inspect_ai/_view/www/eslint.config.mjs
@@ -22,7 +22,7 @@ export default [
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: "./tsconfig.eslint.json",
       },
     },
     plugins: {
