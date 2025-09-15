cc9e4cf4e323a83065c6388c19f37fa981a36f66
Mischa Spiegelmock <me@mish.dev>
2025-09-15 11:51:56 -0700

    prettier

--- src/inspect_ai/_view/www/src/client/api/api-view-server.ts
+++ src/inspect_ai/_view/www/src/client/api/api-view-server.ts
@@ -197,7 +197,7 @@ function createServerFetchApi(
   };
 }
 
-async function open_log_file(): Promise<void> { }
+async function open_log_file(): Promise<void> {}
 
 const clientEvents = (fetchApi: ServerFetchAPI) => async () => {
   const params = new URLSearchParams();
@@ -219,35 +219,35 @@ const evalLogs = (fetchApi: ServerFetchAPI, logDir?: string) => async () => {
 
 const evalLog =
   (fetchApi: ServerFetchAPI) =>
-    async (
-      file: string,
-      headerOnly?: number,
-      _capabilities?: Capabilities,
-    ): Promise<LogContents> => {
-      const result = await fetchApi.fetchString(
-        "GET",
-        `/logs/${encodeURIComponent(file)}?header-only=${headerOnly}`,
-      );
-      return result;
-    };
+  async (
+    file: string,
+    headerOnly?: number,
+    _capabilities?: Capabilities,
+  ): Promise<LogContents> => {
+    const result = await fetchApi.fetchString(
+      "GET",
+      `/logs/${encodeURIComponent(file)}?header-only=${headerOnly}`,
+    );
+    return result;
+  };
 
 const evalLogSize =
   (fetchApi: ServerFetchAPI) =>
-    async (file: string): Promise<number> => {
-      const result = await fetchApi.fetchString(
-        "GET",
-        `/log-size/${encodeURIComponent(file)}`,
-      );
-      return result.parsed;
-    };
+  async (file: string): Promise<number> => {
+    const result = await fetchApi.fetchString(
+      "GET",
+      `/log-size/${encodeURIComponent(file)}`,
+    );
+    return result.parsed;
+  };
 
 const evalLogBytes =
   (fetchApi: ServerFetchAPI) =>
-    async (file: string, start: number, end: number): Promise<Uint8Array> =>
-      fetchApi.fetchBytes(
-        "GET",
-        `/log-bytes/${encodeURIComponent(file)}?start=${start}&end=${end}`,
-      );
+  async (file: string, start: number, end: number): Promise<Uint8Array> =>
+    fetchApi.fetchBytes(
+      "GET",
+      `/log-bytes/${encodeURIComponent(file)}?start=${start}&end=${end}`,
+    );
 
 const evalLogOverviews =
   (fetchApi: ServerFetchAPI) => async (files: string[]) => {
@@ -264,122 +264,122 @@ const evalLogOverviews =
 
 const logMessage =
   (fetchApi: ServerFetchAPI) =>
-    async (log_file: string, message: string): Promise<void> => {
-      const params = new URLSearchParams();
-      params.append("log_file", log_file);
-      params.append("message", message);
+  async (log_file: string, message: string): Promise<void> => {
+    const params = new URLSearchParams();
+    params.append("log_file", log_file);
+    params.append("message", message);
 
-      const request: Request<void> = {
-        headers: {
-          "Content-Type": "text/plain",
-        },
-        parse: async (text: string) => {
-          if (text !== "") {
-            throw new Error(`Unexpected response from log_message: ${text}`);
-          }
-          return;
-        },
-      };
-      await fetchApi.fetchType<void>(
-        "GET",
-        `/log-message?${params.toString()}`,
-        request,
-      );
+    const request: Request<void> = {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      parse: async (text: string) => {
+        if (text !== "") {
+          throw new Error(`Unexpected response from log_message: ${text}`);
+        }
+        return;
+      },
     };
+    await fetchApi.fetchType<void>(
+      "GET",
+      `/log-message?${params.toString()}`,
+      request,
+    );
+  };
 
 const evalPendingSamples =
   (fetchApi: ServerFetchAPI) =>
-    async (log_file: string, etag?: string): Promise<PendingSampleResponse> => {
-      const params = new URLSearchParams();
-      params.append("log", log_file);
+  async (log_file: string, etag?: string): Promise<PendingSampleResponse> => {
+    const params = new URLSearchParams();
+    params.append("log", log_file);
 
-      const headers: Record<string, string> = {};
-      if (etag) {
-        headers["If-None-Match"] = etag;
-      }
+    const headers: Record<string, string> = {};
+    if (etag) {
+      headers["If-None-Match"] = etag;
+    }
 
-      const request: Request<PendingSampleResponse> = {
-        headers,
-        parse: async (text: string) => {
-          const pendingSamples = await asyncJsonParse(text);
+    const request: Request<PendingSampleResponse> = {
+      headers,
+      parse: async (text: string) => {
+        const pendingSamples = await asyncJsonParse(text);
+        return {
+          status: "OK",
+          pendingSamples,
+        };
+      },
+      handleError: (status: number) => {
+        if (status === 404) {
           return {
-            status: "OK",
-            pendingSamples,
+            status: "NotFound",
           };
-        },
-        handleError: (status: number) => {
-          if (status === 404) {
-            return {
-              status: "NotFound",
-            };
-          } else if (status === 304) {
-            return {
-              status: "NotModified",
-            };
-          }
-        },
-      };
-
-      const result = await fetchApi.fetchType<PendingSampleResponse>(
-        "GET",
-        `/pending-samples?${params.toString()}`,
-        request,
-      );
-
-      return result.parsed;
+        } else if (status === 304) {
+          return {
+            status: "NotModified",
+          };
+        }
+      },
     };
+
+    const result = await fetchApi.fetchType<PendingSampleResponse>(
+      "GET",
+      `/pending-samples?${params.toString()}`,
+      request,
+    );
+
+    return result.parsed;
+  };
 
 const evalLogSampleData =
   (fetchApi: ServerFetchAPI) =>
-    async (
-      log_file: string,
-      id: string | number,
-      epoch: number,
-      last_event?: number,
-      last_attachment?: number,
-    ): Promise<SampleDataResponse | undefined> => {
-      const params = new URLSearchParams();
-      params.append("log", log_file);
-      params.append("id", String(id));
-      params.append("epoch", String(epoch));
-      if (last_event !== undefined) {
-        params.append("last-event-id", String(last_event));
-      }
+  async (
+    log_file: string,
+    id: string | number,
+    epoch: number,
+    last_event?: number,
+    last_attachment?: number,
+  ): Promise<SampleDataResponse | undefined> => {
+    const params = new URLSearchParams();
+    params.append("log", log_file);
+    params.append("id", String(id));
+    params.append("epoch", String(epoch));
+    if (last_event !== undefined) {
+      params.append("last-event-id", String(last_event));
+    }
 
-      if (last_attachment !== undefined) {
-        params.append("after-attachment-id", String(last_attachment));
-      }
+    if (last_attachment !== undefined) {
+      params.append("after-attachment-id", String(last_attachment));
+    }
 
-      const request: Request<SampleDataResponse> = {
-        headers: {},
-        parse: async (text: string) => {
-          const sampleData = await asyncJsonParse(text);
+    const request: Request<SampleDataResponse> = {
+      headers: {},
+      parse: async (text: string) => {
+        const sampleData = await asyncJsonParse(text);
+        return {
+          status: "OK",
+          sampleData,
+        };
+      },
+      handleError: (status: number) => {
+        if (status === 404) {
           return {
-            status: "OK",
-            sampleData,
+            status: "NotFound",
           };
-        },
-        handleError: (status: number) => {
-          if (status === 404) {
-            return {
-              status: "NotFound",
-            };
-          } else if (status === 304) {
-            return {
-              status: "NotModified",
-            };
-          }
-        },
-      };
-
-      const result = await fetchApi.fetchType<SampleDataResponse>(
-        "GET",
-        `/pending-sample-data?${params.toString()}`,
-        request,
-      );
-
-      return result.parsed;
+        } else if (status === 304) {
+          return {
+            status: "NotModified",
+          };
+        }
+      },
     };
+
+    const result = await fetchApi.fetchType<SampleDataResponse>(
+      "GET",
+      `/pending-sample-data?${params.toString()}`,
+      request,
+    );
+
+    return result.parsed;
+  };
 
 /**
  * Create a view server API with optional server-side log listing
