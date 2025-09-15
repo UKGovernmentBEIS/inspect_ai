37041526b9b90175463024d900e157bc778d9a29
Mischa Spiegelmock <me@mish.dev>
2025-09-15 12:05:15 -0700

    ok we need a few more exports

--- src/inspect_ai/_view/www/src/index.ts
+++ src/inspect_ai/_view/www/src/index.ts
@@ -2,8 +2,12 @@
 export { App } from "./app/App";
 
 // Client APIs
+export { clientApi } from "./client/api/client-api";
 export { createViewServerApi } from "./client/api/api-view-server.ts";
 export { default as simpleHttpApi } from "./client/api/api-http";
 
 // Client API - Types
 export type { ClientAPI, LogViewAPI, Capabilities } from "./client/api/types";
+
+// State Store
+export { initializeStore } from "./state/store";
