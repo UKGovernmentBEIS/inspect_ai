55d56f01fe6d15fff010250b9a9c6065aef01f21
Mischa Spiegelmock <me@mish.dev>
2025-09-14 22:08:14 -0700

    reverts

--- src/inspect_ai/_view/www/postcss.config.cjs
+++ src/inspect_ai/_view/www/postcss.config.cjs
@@ -2,9 +2,9 @@
 module.exports = {
   plugins: [
     require("postcss-url")({
-      url: "inline",
-      maxSize: Infinity,
-      fallback: "copy",
+      url: "inline", // Inline all assets
+      maxSize: Infinity, // Maximum file size to inline (in kilobytes). Adjust as needed.
+      fallback: "copy", // Copy files to output directory if they are larger than the maxSize
     }),
   ],
 };
