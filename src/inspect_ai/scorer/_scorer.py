f549c1effe5f66099eeda06f95ce1716d7b35673
jeqcho <jeqin_chooi@college.harvard.edu>
2025-09-19 17:00:01 -0400

    improve types for scorer decorator

--- src/inspect_ai/scorer/_scorer.py
+++ src/inspect_ai/scorer/_scorer.py
@@ -1,3 +1,4 @@
+from collections.abc import Mapping, Sequence
 from copy import deepcopy
 from dataclasses import dataclass, field
 from functools import wraps
@@ -121,7 +122,8 @@ def scorer_create(name: str, **kwargs: Any) -> Scorer:
 
 
 def scorer(
-    metrics: list[Metric | dict[str, list[Metric]]] | dict[str, list[Metric]],
+    metrics: Sequence[Metric | Mapping[str, Sequence[Metric]]]
+    | Mapping[str, Sequence[Metric]],
     name: str | None = None,
     **metadata: Any,
 ) -> Callable[[Callable[P, Scorer]], Callable[P, Scorer]]:
