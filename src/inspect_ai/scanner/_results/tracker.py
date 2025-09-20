a8f1930c2d3ca3de54801735b962810050cc7677
J.J. Allaire <jj.allaire@gmail.com>
2025-09-20 12:37:24 -0400

    more setup for resumability

--- src/inspect_ai/scanner/_results/tracker.py
+++ src/inspect_ai/scanner/_results/tracker.py
@@ -1,31 +1,51 @@
-from typing import Protocol
+from dataclasses import dataclass
+from typing import Any, Protocol, cast
 
-from inspect_ai.scanner._transcript.types import TranscriptInfo
+from inspect_ai.scanner._transcript.transcripts import Transcripts
 
 from .._scanner.result import Result
+from .._scanner.scanner import Scanner
+from .._transcript.types import TranscriptInfo
 
 
-class ResultReporter(Protocol):
+class Reporter(Protocol):
     async def __call__(self, result: Result) -> None: ...
 
 
-class ResultsTracker(Protocol):
+class Tracker(Protocol):
     async def __call__(
         self, transcript: TranscriptInfo, scanner: str, context_id: str | None
-    ) -> ResultReporter | None: ...
+    ) -> Reporter | None: ...
 
 
-def results_tracker(scans_dir: str, scan_id: str, scan_name: str) -> ResultsTracker:
+def results_tracker(
+    scans_dir: str,
+    scan_id: str,
+    scan_name: str,
+    transcripts: Transcripts,
+    scanners: dict[str, Scanner[Any]],
+) -> Tracker:
     # create the results directory
 
     # create the metadata file for the scan (transcript query + scanners w/ args to reconstruct them)
 
     async def tracker(
         transcript: TranscriptInfo, scanner: str, context_id: str | None
-    ) -> ResultReporter | None:
+    ) -> Reporter | None:
         async def report(result: Result) -> None:
             pass
 
         return report
 
     return tracker
+
+
+@dataclass
+class ScanOptions:
+    name: str
+    transcripts: Transcripts
+    scanners: dict[str, Scanner[Any]]
+
+
+def scan_options_from_tracker(scans_dir: str, scan_id: str) -> ScanOptions:
+    return cast(ScanOptions, None)
--- src/inspect_ai/scanner/_scan.py
+++ src/inspect_ai/scanner/_scan.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 import os
 import re
+from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, Sequence
 
+from shortuuid import uuid
+
 from inspect_ai._util._async import run_coroutine
+from inspect_ai._util.registry import registry_info
 
 from ._scanner.scanner import Scanner
 from ._transcript.transcripts import Transcripts
@@ -13,15 +17,27 @@ if TYPE_CHECKING:
     import pandas as pd
 
 
+@dataclass
+class ScanResults:
+    scan_id: str
+    scan_name: str
+    scanners: dict[str, "pd.DataFrame"]
+
+
 def scan(
     transcripts: Transcripts,
     scanners: Sequence[Scanner[Any] | tuple[str, Scanner[Any]]],
-    name: str | None = None,
+    scan_id: str | None = None,
+    scan_name: str | None = None,
     scans_dir: str | None = None,
-) -> dict[str, "pd.DataFrame"]:
+) -> ScanResults:
     return run_coroutine(
         scan_async(
-            transcripts=transcripts, scanners=scanners, name=name, scans_dir=scans_dir
+            transcripts=transcripts,
+            scanners=scanners,
+            scan_id=scan_id,
+            scan_name=scan_name,
+            scans_dir=scans_dir,
         )
     )
 
@@ -29,20 +45,35 @@ def scan(
 async def scan_async(
     transcripts: Transcripts,
     scanners: Sequence[Scanner[Any] | tuple[str, Scanner[Any]]],
-    name: str | None = None,
+    scan_id: str | None = None,
+    scan_name: str | None = None,
     scans_dir: str | None = None,
-) -> dict[str, "pd.DataFrame"]:
+) -> ScanResults:
+    # resolve id
+    scan_id = scan_id or uuid()
+
     # validate and resolve name
-    if name is not None:
-        if not re.match(r"^[a-zA-Z0-9_-]+$", name):
+    if scan_name is not None:
+        if not re.match(r"^[a-zA-Z0-9_-]+$", scan_name):
             raise ValueError(
                 "scan 'name' may use only letters, numbers, underscores, and dashes"
             )
-    name = name or "scan"
+    scan_name = scan_name or "scan"
 
     # resolve scans_dir
     scans_dir = scans_dir or os.getenv("INSPECT_SCANS_DIR", "./scans")
 
-    # confirm each scanner has a unique name
+    # resolve scanners and confirm unique names
+    named_scanners: dict[str, Scanner[Any]] = {}
+    for scanner in scanners:
+        if isinstance(scanner, tuple):
+            name, scanner = scanner
+        else:
+            name = registry_info(scanner).name
+        if name in named_scanners:
+            raise ValueError(
+                f"Scanners must have unique names (found duplicate name '{name}'). Use a tuple of str,Scanner to explicitly name a scanner."
+            )
+        named_scanners[name] = scanner
 
-    return {}
+    return ScanResults(scan_id=scan_id, scan_name=scan_name, scanners={})
