413a775b41be5ae57b45d82640ad7e8446760907
anthonyduong <anthonyduong9@gmail.com>
2025-09-22 07:21:07 -0700

    rename _edit.py to _score.py

--- src/inspect_ai/__init__.py
+++ src/inspect_ai/__init__.py
@@ -12,7 +12,7 @@ from inspect_ai._eval.task.tasks import Tasks
 from inspect_ai._util.constants import PKG_NAME
 from inspect_ai._view.view import view
 from inspect_ai.agent._human.agent import human_cli
-from inspect_ai.log._edit import edit_score, recompute_metrics
+from inspect_ai.log._score import edit_score, recompute_metrics
 from inspect_ai.solver._human_agent import human_agent
 
 __version__ = importlib_version(PKG_NAME)
