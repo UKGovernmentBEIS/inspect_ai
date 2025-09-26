1805d681fd8210b97a5fde8e435f0fce798369b7
anthonyduong <anthonyduong9@gmail.com>
2025-09-26 14:17:13 -0700

    add number of messages to log header

--- src/inspect_ai/_view/www/src/@types/log.d.ts
+++ src/inspect_ai/_view/www/src/@types/log.d.ts
@@ -1143,6 +1143,7 @@ export interface EvalStats {
   started_at: StartedAt;
   completed_at: CompletedAt;
   model_usage: ModelUsage;
+  sample_message_counts: SampleMessageCounts;
 }
 export interface ModelUsage {
   [k: string]: ModelUsage1;
@@ -1157,6 +1158,9 @@ export interface ModelUsage1 {
   input_tokens_cache_write: InputTokensCacheWrite;
   input_tokens_cache_read: InputTokensCacheRead;
   reasoning_tokens: ReasoningTokens1;
+}
+export interface SampleMessageCounts {
+  [k: string]: number;
 }
 /**
  * Eval error details.
