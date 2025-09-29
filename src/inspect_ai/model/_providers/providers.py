887a1031729437fe601579901b5dd11597b5150c
dragonstyle <cteague@gmail.com>
2025-09-29 17:20:20 -0400

    Provide correct tool version for Claude Sonnet 4.5
    - Also correct the claude 4 regex to properly match claude 4 versions
    - This in turn requires update to the latest version of the anthropic client.
    
    update guard

--- src/inspect_ai/model/_providers/providers.py
+++ src/inspect_ai/model/_providers/providers.py
@@ -324,7 +324,7 @@ def validate_openai_client(feature: str) -> None:
 
 def validate_anthropic_client(feature: str) -> None:
     PACKAGE = "anthropic"
-    MIN_VERSION = "0.52.0"
+    MIN_VERSION = "0.69.0"
 
     # verify we have the package
     try:
