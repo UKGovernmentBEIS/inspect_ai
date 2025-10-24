240cc1353e0c9c857d158a4aac36ad3343726707
J.J. Allaire <jj.allaire@gmail.com>
2025-10-24 15:35:22 -0400

    move resolve_model_roles to util

--- src/inspect_ai/_eval/task/task.py
+++ src/inspect_ai/_eval/task/task.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from logging import getLogger
-from typing import Any, Awaitable, Callable, Mapping, Sequence, cast
+from typing import Any, Awaitable, Callable, Sequence, cast
 
 from pydantic import BaseModel
 from typing_extensions import TypedDict, Unpack
@@ -21,6 +21,7 @@ from inspect_ai.dataset import Dataset, MemoryDataset, Sample
 from inspect_ai.log import EvalLog
 from inspect_ai.model import GenerateConfig
 from inspect_ai.model._model import Model, get_model
+from inspect_ai.model._util import resolve_model_roles
 from inspect_ai.scorer import Metric, Scorer
 from inspect_ai.scorer._reducer import ScoreReducers, create_reducers
 from inspect_ai.solver import Plan, Solver, generate
@@ -403,21 +404,6 @@ def resolve_model(model: str | Model | None) -> Model | None:
         return get_model(model)
     else:
         return model
-
-
-def resolve_model_roles(
-    model_roles: Mapping[str, str | Model] | None,
-) -> dict[str, Model] | None:
-    if model_roles is not None:
-        resolved_model_roles = {
-            k: get_model(v, memoize=False) if isinstance(v, str) else v
-            for k, v in model_roles.items()
-        }
-        for k, v in resolved_model_roles.items():
-            v._set_role(k)
-        return resolved_model_roles
-    else:
-        return None
 
 
 def resolve_scorer(scorer: Scorer | list[Scorer] | None) -> list[Scorer] | None:
