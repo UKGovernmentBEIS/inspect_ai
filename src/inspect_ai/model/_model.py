6b40c4abf39a103ca78579982934c2da59634c15
Rasmus Faber-Espensen <rfaber@gmail.com>
2025-10-27 09:35:43 +0100

    Also retry on auth failure

--- src/inspect_ai/model/_model.py
+++ src/inspect_ai/model/_model.py
@@ -814,6 +814,11 @@ class Model:
                 report_http_retry()
                 return True
 
+            retry = self.api.is_auth_failure(ex)
+            if retry:
+                report_http_retry()
+                return True
+
             # see if the API implements legacy is_rate_limit() method
             is_rate_limit = getattr(self.api, "is_rate_limit", None)
             if is_rate_limit:
