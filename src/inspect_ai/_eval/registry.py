7eb5e0e68a282bec0aa755e75baa0f4197e31605
Nathan Habib <nathan.habib19@gmail.com>
2025-12-09 16:50:39 +0100

    make it work for hf/repo

--- src/inspect_ai/_eval/registry.py
+++ src/inspect_ai/_eval/registry.py
@@ -4,6 +4,7 @@ from functools import wraps
 from pathlib import Path
 from typing import Any, Callable, TypeVar, cast, overload
 
+from inspect_ai._eval.task.epochs import Epochs
 from inspect_ai._util.error import PrerequisiteError
 from inspect_ai._util.package import get_installed_package_name
 from inspect_ai._util.registry import (
@@ -82,6 +83,84 @@ def task_create(name: str, **kwargs: Any) -> Task:
             logger.warning(f"param '{param}' not used by task '{name}'")
 
     return registry_create("task", name, **task_args)
+
+
+def task_create_from_hf(repo_id: str, **kwargs: Any) -> list[Task]:
+    """Build a Task from a full config definition (solvers, scorers, dataset, etc.)."""
+    from importlib import import_module
+
+    import yaml
+    from huggingface_hub import hf_hub_download
+
+    from inspect_ai._util.from_hub import load_dataset
+
+    repo_id = repo_id.replace("hf/", "")
+    revision = kwargs.get("revision", "main")
+    yaml_path = Path(
+        hf_hub_download(
+            repo_id=repo_id,
+            filename="eval.yaml",
+            repo_type="dataset",
+            revision=revision,
+        )
+    )
+
+    with open(yaml_path, "r") as f:
+        global_config = yaml.safe_load(f)
+
+    task_configs = global_config["tasks"]
+
+    tasks = []
+    for task_config in task_configs:
+        # Build dataset
+        dataset = load_dataset(repo_id, revision, task_config=task_config)
+
+        # Build solvers
+        solvers = []
+        solver_configs = task_config.get("solvers", [])
+        solver_module = import_module("inspect_ai.solver")
+        for solver_config in solver_configs:
+            solver_name = solver_config.get("name")
+
+            if not hasattr(solver_module, solver_name):
+                raise ValueError(f"Unknown solver: {solver_name}")
+
+            solver_fn = getattr(solver_module, solver_name)
+            solvers.append(solver_fn(**solver_config.get("args", {})))
+
+        # Build scorers
+        scorers = []
+        scorer_configs = task_config.get("scorers", [])
+        scorer_module = import_module("inspect_ai.scorer")
+        for scorer_config in scorer_configs:
+            scorer_name = scorer_config.get("name")
+
+            if not hasattr(scorer_module, scorer_name):
+                raise ValueError(f"Unknown scorer: {scorer_name}")
+
+            scorer_fn = getattr(scorer_module, scorer_name)
+            scorers.append(scorer_fn(**scorer_config.get("args", {})))
+
+        # Extract other task parameters
+        task_name = task_config.get("name") or task_config.get("task")
+        epochs = task_config.get("epochs", 1)
+        epochs_reducer = task_config.get("epochs_reducer", None)
+
+        # Build and return task
+        task = Task(
+            dataset=dataset,
+            solver=solvers,
+            scorer=scorers,
+            name=task_name,
+            epochs=Epochs(epochs, epochs_reducer),
+        )
+
+        # Set file attributes
+        setattr(task, TASK_FILE_ATTR, yaml_path.absolute().as_posix())
+        setattr(task, TASK_RUN_DIR_ATTR, yaml_path.parent.resolve().as_posix())
+        tasks.append(task)
+
+    return tasks
 
 
 @overload
