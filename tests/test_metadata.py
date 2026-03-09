05e6bffe9776f0d2c907d7d057d07ed9c18a1c5c
Ransom Richardson <ransom@meridianlabs.ai>
2026-03-09 13:36:06 -0400

    Add tests for tag merging

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,6 +1,7 @@
 from inspect_ai import Task, eval
 
 TEST_METADATA = {"foo": "bar "}
+TEST_TAGS = ["tag1", "tag2"]
 
 
 def test_task_metadata():
@@ -17,3 +18,20 @@ def test_task_and_eval_metadata():
     TEST_METADATA_2 = {"bar": "foo"}
     log = eval(Task(metadata=TEST_METADATA_2), metadata=TEST_METADATA)[0]
     assert log.eval.metadata == TEST_METADATA | TEST_METADATA_2
+
+
+def test_task_tags():
+    log = eval(Task(tags=TEST_TAGS))[0]
+    assert sorted(log.eval.tags) == sorted(TEST_TAGS)
+
+
+def test_eval_tags():
+    log = eval(Task(), tags=TEST_TAGS)[0]
+    assert sorted(log.eval.tags) == sorted(TEST_TAGS)
+
+
+def test_task_and_eval_tags():
+    task_tags = ["tag1", "tag3"]
+    eval_tags = ["tag2", "tag3"]
+    log = eval(Task(tags=task_tags), tags=eval_tags)[0]
+    assert sorted(log.eval.tags) == ["tag1", "tag2", "tag3"]
